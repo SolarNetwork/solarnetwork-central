0737bc5bb016b77e23d4c724ca2cc0d025d90918
Matt Magoffin <git+matt@msqr.us>
2012-12-11 13:48:11 +1300

    Tweak @RequestMapping config to make closer to method implementation.

--- net.solarnetwork.central.in.web/src/net/solarnetwork/central/in/web/BulkDataCollector.java
+++ net.solarnetwork.central.in.web/src/net/solarnetwork/central/in/web/BulkDataCollector.java
@@ -32,12 +32,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-
 import net.solarnetwork.central.RepeatableTaskException;
 import net.solarnetwork.central.dao.SolarNodeDao;
 import net.solarnetwork.central.datum.domain.Datum;
@@ -48,7 +46,6 @@ import net.solarnetwork.central.instructor.domain.InstructionState;
 import net.solarnetwork.central.security.AuthenticatedNode;
 import net.solarnetwork.central.security.SecurityException;
 import net.solarnetwork.domain.NodeControlPropertyType;
-
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
@@ -68,22 +65,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Controller for accepting bulk upload XML documents.
  * 
- * <p>This controller expects an {@link AuthenticatedNode} to be available
- * on each request for securely processing node data.</p>
+ * <p>
+ * This controller expects an {@link AuthenticatedNode} to be available on each
+ * request for securely processing node data.
+ * </p>
  * 
  * @author matt
  * @version $Revision$
  */
 @Controller
-@RequestMapping({"/bulkCollector.do", "/u/bulkCollector.do"})
 public class BulkDataCollector extends AbstractDataCollector {
-	
+
 	/** The InstructionStatus element name. */
 	public static final String INSTRUCTION_STATUS_ELEMENT_NAME = "InstructionStatus";
 
 	/** The NodeControlInfo element name. */
 	public static final String NODE_CONTROL_INFO_ELEMENT_NAME = "NodeControlInfo";
-	
+
 	private static final String NODE_ID_GLOBAL_PARAM = "nodeId";
 
 	/**
@@ -92,16 +90,17 @@ public class BulkDataCollector extends AbstractDataCollector {
 	public BulkDataCollector() {
 		super();
 	}
-	
+
 	/**
 	 * Constructor.
 	 * 
-	 * @param dataCollectorBiz the {@link DataCollectorBiz} to use
-	 * @param solarNodeDao the {@link SolarNodeDao} to use
+	 * @param dataCollectorBiz
+	 *        the {@link DataCollectorBiz} to use
+	 * @param solarNodeDao
+	 *        the {@link SolarNodeDao} to use
 	 */
 	@Autowired
-	public BulkDataCollector(DataCollectorBiz dataCollectorBiz, 
-			SolarNodeDao solarNodeDao) {
+	public BulkDataCollector(DataCollectorBiz dataCollectorBiz, SolarNodeDao solarNodeDao) {
 		setDataCollectorBiz(dataCollectorBiz);
 		setSolarNodeDao(solarNodeDao);
 	}
@@ -109,19 +108,23 @@ public class BulkDataCollector extends AbstractDataCollector {
 	/**
 	 * Post new generation data.
 	 * 
-	 * <p>If {@code encoding} contains {@code gzip} the InputStream itself is assumed
-	 * to be compressed with GZip and encoded as Base64. Otherwise the InputStream is 
-	 * assumed to be regular text (not compressed).</p>
+	 * <p>
+	 * If {@code encoding} contains {@code gzip} the InputStream itself is
+	 * assumed to be compressed with GZip and encoded as Base64. Otherwise the
+	 * InputStream is assumed to be regular text (not compressed).
+	 * </p>
 	 * 
-	 * @param datum the ConsumptionDatum to post
-	 * @param model the model
+	 * @param datum
+	 *        the ConsumptionDatum to post
+	 * @param model
+	 *        the model
 	 * @return the result model
-	 * @throws IOException if any IO error occurs
+	 * @throws IOException
+	 *         if any IO error occurs
 	 */
-	@RequestMapping(method = RequestMethod.POST)
-	public String postData(@RequestHeader("Content-Encoding") String encoding,
-			InputStream in, Model model) 
-	throws IOException {
+	@RequestMapping(value = "/bulkCollector.do", method = RequestMethod.POST)
+	public String postData(@RequestHeader("Content-Encoding") String encoding, InputStream in,
+			Model model) throws IOException {
 		AuthenticatedNode authNode = getAuthenticatedNode(false);
 
 		InputStream input = in;
@@ -139,40 +142,40 @@ public class BulkDataCollector extends AbstractDataCollector {
 		xmlInputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.FALSE);
 		try {
 			XMLStreamReader reader = xmlInputFactory.createXMLStreamReader(input);
-			
+
 			// once we pass the root element, assume all elements are datum records
 			boolean rootFound = false;
 			while ( reader.hasNext() ) {
-				switch ( reader.next() ) {
-				case XMLStreamReader.START_ELEMENT:
-					if ( rootFound ) {
-						Object o = handleElement(reader, globalAttributes);
-						if ( o instanceof Datum ) {
-							parsedDatum.add((Datum)o);
-						} else if ( o instanceof Instruction ) {
-							results.add(o);
-						}
-					} else {
-						int count = reader.getAttributeCount();
-						for ( int i = 0; i < count; i++ ) {
-							globalAttributes.put(reader.getAttributeLocalName(i),
-									reader.getAttributeValue(i));
-						}
-						if ( authNode != null ) {
-							// set nodeId to authenticated node ID
-							Long nodeId = authNode.getNodeId();
-							if ( nodeId != null  ) {
-								globalAttributes.put(NODE_ID_GLOBAL_PARAM, authNode.getNodeId());
+				switch (reader.next()) {
+					case XMLStreamReader.START_ELEMENT:
+						if ( rootFound ) {
+							Object o = handleElement(reader, globalAttributes);
+							if ( o instanceof Datum ) {
+								parsedDatum.add((Datum) o);
+							} else if ( o instanceof Instruction ) {
+								results.add(o);
 							}
+						} else {
+							int count = reader.getAttributeCount();
+							for ( int i = 0; i < count; i++ ) {
+								globalAttributes.put(reader.getAttributeLocalName(i),
+										reader.getAttributeValue(i));
+							}
+							if ( authNode != null ) {
+								// set nodeId to authenticated node ID
+								Long nodeId = authNode.getNodeId();
+								if ( nodeId != null ) {
+									globalAttributes.put(NODE_ID_GLOBAL_PARAM, authNode.getNodeId());
+								}
+							}
+							rootFound = true;
 						}
-						rootFound = true;
-					}
 				}
 			}
-		} catch (XMLStreamException e) {
+		} catch ( XMLStreamException e ) {
 			throw new RuntimeException(e);
 		}
-		
+
 		try {
 			List<Datum> postedDatum = getDataCollectorBiz().postDatum(parsedDatum);
 			results.addAll(postedDatum);
@@ -183,38 +186,43 @@ public class BulkDataCollector extends AbstractDataCollector {
 				while ( root.getCause() != null ) {
 					root = root.getCause();
 				}
-				log.debug("RepeatableTaskException caused by: " +root.getMessage());
+				log.debug("RepeatableTaskException caused by: " + root.getMessage());
 			}
 		}
-		
+
 		if ( authNode != null ) {
 			defaultHandleNodeInstructions(authNode.getNodeId(), model);
 		}
-		
+
 		return getViewName();
 	}
-	
+
 	@ExceptionHandler(SecurityException.class)
 	public void handleSecurityException(SecurityException e, HttpServletResponse res) {
 		if ( log.isWarnEnabled() ) {
-			log.warn("Security exception: " +e.getMessage());
+			log.warn("Security exception: " + e.getMessage());
 		}
 		res.setStatus(HttpServletResponse.SC_FORBIDDEN);
 	}
-	
+
 	/**
 	 * Parse InstructionStatus XML.
 	 * 
-	 * <p>The XML looks like this:</p>
+	 * <p>
+	 * The XML looks like this:
+	 * </p>
 	 * 
-	 * <pre>&lt;InstructionStatus instructionId="123" status="Received"/></pre>
+	 * <pre>
+	 * &lt;InstructionStatus instructionId="123" status="Received"/>
+	 * </pre>
 	 * 
-	 * @param reader the stream reader
-	 * @param attributes global attributes
+	 * @param reader
+	 *        the stream reader
+	 * @param attributes
+	 *        global attributes
 	 * @return
 	 */
-	private Object handleInstructionStatusElement(XMLStreamReader reader, 
-			Map<String, Object> attributes) {
+	private Object handleInstructionStatusElement(XMLStreamReader reader, Map<String, Object> attributes) {
 		String instructionId = reader.getAttributeValue(null, "instructionId");
 		String status = reader.getAttributeValue(null, "status");
 		Instruction result = null;
@@ -229,9 +237,9 @@ public class BulkDataCollector extends AbstractDataCollector {
 		}
 		return result;
 	}
-	
-	private Object handleElement(XMLStreamReader reader, Map<String, Object> attributes) 
-	throws XMLStreamException {
+
+	private Object handleElement(XMLStreamReader reader, Map<String, Object> attributes)
+			throws XMLStreamException {
 		// we are on a START_ELEMENT event here, so create bean from datum
 		// class and all attributes into JavaBean style properties
 		String className = reader.getLocalName();
@@ -241,40 +249,40 @@ public class BulkDataCollector extends AbstractDataCollector {
 		} else if ( className.equals(NODE_CONTROL_INFO_ELEMENT_NAME) ) {
 			return handleNodeControlInfoElement(reader, attributes);
 		}
-		className = "net.solarnetwork.central.datum.domain."+className;
+		className = "net.solarnetwork.central.datum.domain." + className;
 		Datum datum = null;
 		try {
 			@SuppressWarnings("unchecked")
-			Class<? extends Datum> datumClass = (Class<? extends Datum>)Class.forName(
-					className, true, Datum.class.getClassLoader());
+			Class<? extends Datum> datumClass = (Class<? extends Datum>) Class.forName(className, true,
+					Datum.class.getClassLoader());
 			datum = datumClass.newInstance();
-		} catch (ClassNotFoundException e) {
+		} catch ( ClassNotFoundException e ) {
 			if ( log.isWarnEnabled() ) {
-				log.warn("Unable to load Datum class " +className +" specified in XML");
+				log.warn("Unable to load Datum class " + className + " specified in XML");
 			}
 			return null;
-		} catch (InstantiationException e) {
+		} catch ( InstantiationException e ) {
 			if ( log.isWarnEnabled() ) {
-				log.warn("Unable to instantiate Datum class " +className +" specified in XML: "
-						+e.getMessage());
+				log.warn("Unable to instantiate Datum class " + className + " specified in XML: "
+						+ e.getMessage());
 			}
 			return null;
-		} catch (IllegalAccessException e) {
+		} catch ( IllegalAccessException e ) {
 			if ( log.isWarnEnabled() ) {
-				log.warn("Unable to access Datum class " +className +" specified in XML: "
-						+e.getMessage());
+				log.warn("Unable to access Datum class " + className + " specified in XML: "
+						+ e.getMessage());
 			}
 			return null;
 		}
 		BeanWrapper bean = PropertyAccessorFactory.forBeanPropertyAccess(datum);
-		bean.registerCustomEditor(DateTime.class, (PropertyEditor)DATE_TIME_EDITOR.clone());
-		bean.registerCustomEditor(LocalDate.class, (PropertyEditor)LOCAL_DATE_EDITOR.clone());
-		bean.registerCustomEditor(LocalTime.class, (PropertyEditor)LOCAL_TIME_EDITOR.clone());
-		
+		bean.registerCustomEditor(DateTime.class, (PropertyEditor) DATE_TIME_EDITOR.clone());
+		bean.registerCustomEditor(LocalDate.class, (PropertyEditor) LOCAL_DATE_EDITOR.clone());
+		bean.registerCustomEditor(LocalTime.class, (PropertyEditor) LOCAL_TIME_EDITOR.clone());
+
 		// apply global attributes
 		MutablePropertyValues pvs = new MutablePropertyValues(attributes);
 		bean.setPropertyValues(pvs, true, true);
-		
+
 		int propertyCount = reader.getAttributeCount();
 		for ( int i = 0; i < propertyCount; i++ ) {
 			String attrName = reader.getAttributeLocalName(i);
@@ -287,28 +295,26 @@ public class BulkDataCollector extends AbstractDataCollector {
 				bean.setPropertyValue(attrName, attrValue);
 			} catch ( InvalidPropertyException e ) {
 				if ( log.isDebugEnabled() ) {
-					log.debug("Unable to set property [" +attrName +"] to ["
-							+attrValue +"] on instance of " 
-							+reader.getLocalName());
+					log.debug("Unable to set property [" + attrName + "] to [" + attrValue
+							+ "] on instance of " + reader.getLocalName());
 				}
-			} catch( PropertyAccessException e ) {
+			} catch ( PropertyAccessException e ) {
 				if ( log.isDebugEnabled() ) {
-					log.debug("Unable to set property [" +attrName +"] to ["
-							+attrValue +"] on instance of " 
-							+reader.getLocalName());
+					log.debug("Unable to set property [" + attrName + "] to [" + attrValue
+							+ "] on instance of " + reader.getLocalName());
 				}
 			}
 		}
-		
+
 		if ( log.isTraceEnabled() ) {
-			log.trace("Parsed datum " +datum +" from XML");
+			log.trace("Parsed datum " + datum + " from XML");
 		}
-		
+
 		return datum;
 	}
 
-	private Object handleNodeControlInfoElement(XMLStreamReader reader,
-			Map<String, Object> attributes) throws XMLStreamException {
+	private Object handleNodeControlInfoElement(XMLStreamReader reader, Map<String, Object> attributes)
+			throws XMLStreamException {
 		HardwareControlDatum datum = null;
 		String controlId = reader.getAttributeValue(null, "controlId");
 		String propertyName = reader.getAttributeValue(null, "propertyName");
@@ -318,30 +324,30 @@ public class BulkDataCollector extends AbstractDataCollector {
 			datum = new HardwareControlDatum();
 
 			NodeControlPropertyType t = NodeControlPropertyType.valueOf(type);
-			switch ( t ) {
-			case Boolean:
-				if ( value.length() > 0 && (value.equals("1")
-						|| value.equalsIgnoreCase("yes") 
-						|| value.equalsIgnoreCase("true")) ) {
-					datum.setIntegerValue(Integer.valueOf(1));
-				} else {
-					datum.setIntegerValue(Integer.valueOf(0));
-				}
-				break;
-				
-			case Integer:
-				datum.setIntegerValue(Integer.valueOf(value));
-				break;
-				
-			case Float:
-			case Percent:
-				datum.setFloatValue(Float.valueOf(value));
-				break;
-				
+			switch (t) {
+				case Boolean:
+					if ( value.length() > 0
+							&& (value.equals("1") || value.equalsIgnoreCase("yes") || value
+									.equalsIgnoreCase("true")) ) {
+						datum.setIntegerValue(Integer.valueOf(1));
+					} else {
+						datum.setIntegerValue(Integer.valueOf(0));
+					}
+					break;
+
+				case Integer:
+					datum.setIntegerValue(Integer.valueOf(value));
+					break;
+
+				case Float:
+				case Percent:
+					datum.setFloatValue(Float.valueOf(value));
+					break;
+
 			}
 			String sourceId = controlId;
 			if ( propertyName != null ) {
-				sourceId += ";" +propertyName;
+				sourceId += ";" + propertyName;
 			}
 			datum.setSourceId(sourceId);
 		}
