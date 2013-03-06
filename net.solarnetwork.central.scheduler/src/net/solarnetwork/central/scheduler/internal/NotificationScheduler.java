6488ee5be7a05ebdc00b237772e799e5fd7e2b48
Matt Magoffin <git+matt@msqr.us>
2013-03-06 15:32:20 +1300

    Formatting.

--- net.solarnetwork.central.scheduler/src/net/solarnetwork/central/scheduler/internal/NotificationScheduler.java
+++ net.solarnetwork.central.scheduler/src/net/solarnetwork/central/scheduler/internal/NotificationScheduler.java
@@ -18,18 +18,14 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
  * 02111-1307 USA
  * ==================================================================
- * $Id$
- * ==================================================================
  */
 
 package net.solarnetwork.central.scheduler.internal;
 
 import java.util.Date;
 import java.util.Map;
-
 import net.solarnetwork.central.scheduler.EventHandlerSupport;
 import net.solarnetwork.central.scheduler.SchedulerConstants;
-
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
 import org.quartz.JobDataMap;
@@ -42,68 +38,69 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * OSGi {@link EventHandler} that schedules  job requests.
+ * OSGi {@link EventHandler} that schedules job requests.
  * 
  * @author matt
- * @version $Revision$
+ * @version 1.0
  */
 public class NotificationScheduler extends EventHandlerSupport {
 
 	private static final String NOTIFICATION_JOB_NAME = "NotificationJob";
 	private static final String NOTIFICATION_JOB_GROUP = "Notification";
-	
+
 	private final Logger log = LoggerFactory.getLogger(getClass());
-	
-	private Scheduler scheduler;
-	
+
+	private final Scheduler scheduler;
+
 	/**
 	 * Constructor.
 	 * 
-	 * @param scheduler the scheduler
+	 * @param scheduler
+	 *        the scheduler
 	 */
 	public NotificationScheduler(Scheduler scheduler) {
 		this.scheduler = scheduler;
 	}
-	
+
 	@Override
 	protected void handleEventInternal(Event event) throws SchedulerException {
-		
-		final String jobId = (String)event.getProperty(SchedulerConstants.JOB_ID);
+
+		final String jobId = (String) event.getProperty(SchedulerConstants.JOB_ID);
 		if ( jobId == null ) {
 			log.debug("Ignoring OSGi event without {}", SchedulerConstants.JOB_ID);
 			return;
 		}
-		
-		final String jobGroup = (String)event.getProperty(SchedulerConstants.JOB_GROUP);
-		
-		final Long jobDate = (Long)event.getProperty(SchedulerConstants.JOB_DATE);
+
+		final String jobGroup = (String) event.getProperty(SchedulerConstants.JOB_GROUP);
+
+		final Long jobDate = (Long) event.getProperty(SchedulerConstants.JOB_DATE);
 		if ( jobDate == null ) {
 			log.debug("Ignoring OSGi event without {}", SchedulerConstants.JOB_DATE);
 			return;
 		}
-		
-		final String jobTopic = (String)event.getProperty(SchedulerConstants.JOB_TOPIC);
+
+		final String jobTopic = (String) event.getProperty(SchedulerConstants.JOB_TOPIC);
 		if ( jobTopic == null ) {
 			log.debug("Ignoring OSGi event without {}", SchedulerConstants.JOB_TOPIC);
 			return;
 		}
-		
+
 		@SuppressWarnings("unchecked")
-		final Map<String, ?>  jobProps = (Map<String, ?>)event.getProperty(
-				SchedulerConstants.JOB_PROPERTIES);
-		
+		final Map<String, ?> jobProps = (Map<String, ?>) event
+				.getProperty(SchedulerConstants.JOB_PROPERTIES);
+
 		JobDetail job = scheduler.getJobDetail(NOTIFICATION_JOB_NAME, NOTIFICATION_JOB_GROUP);
 		if ( job == null ) {
-			job = new JobDetail(NOTIFICATION_JOB_NAME, NOTIFICATION_JOB_GROUP, 
-					NotificationJob.class, false, true, false);
+			job = new JobDetail(NOTIFICATION_JOB_NAME, NOTIFICATION_JOB_GROUP, NotificationJob.class,
+					false, true, false);
 			scheduler.addJob(job, false);
 		}
-		
+
 		final Date triggerDate = new Date(jobDate);
 		final SimpleTrigger trigger = new SimpleTrigger(jobId, jobGroup, triggerDate);
 		trigger.setJobName(NOTIFICATION_JOB_NAME);
 		trigger.setJobGroup(NOTIFICATION_JOB_GROUP);
-		
+
 		// set up trigger properties, copying all job request properties
 		JobDataMap jobMap = new JobDataMap();
 		for ( String propName : event.getPropertyNames() ) {
@@ -113,19 +110,18 @@ public class NotificationScheduler extends EventHandlerSupport {
 			jobMap.putAll(jobProps);
 		}
 		trigger.setJobDataMap(jobMap);
-		
+
 		Trigger t = scheduler.getTrigger(jobId, jobGroup);
 		if ( t != null ) {
 			// job already scheduled, check for same time to re-schedule
 			Date d = t.getStartTime();
 			if ( d.getTime() != jobDate ) {
 				log.debug("Re-scheduling job {}.{} for {}",
-						new Object[] {jobGroup, jobId, triggerDate});
+						new Object[] { jobGroup, jobId, triggerDate });
 				scheduler.rescheduleJob(jobId, jobGroup, trigger);
 			}
 		} else {
-			log.debug("Scheduling job {}.{} for {}",
-					new Object[] {jobGroup, jobId, triggerDate});
+			log.debug("Scheduling job {}.{} for {}", new Object[] { jobGroup, jobId, triggerDate });
 			scheduler.scheduleJob(trigger);
 		}
 	}
