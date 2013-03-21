3f36843dc1a85cd800ab47a729ff7f0048a0c45c
Matt Magoffin <git+matt@msqr.us>
2013-03-22 10:13:17 +1300

    Add solaruser.user_auth_token_node to associate specific nodes with auth tokens.

--- net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-users.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-users.sql
@@ -71,6 +71,18 @@ CREATE TABLE solaruser.user_auth_token (
 		ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
+CREATE TABLE solaruser.user_auth_token_node (
+	auth_token CHARACTER(20) NOT NULL, 
+	node_id BIGINT NOT NULL,
+	CONSTRAINT user_auth_token_node_pkey PRIMARY KEY (auth_token, node_id),
+	CONSTRAINT user_auth_token_node_token_fk FOREIGN KEY (auth_token)
+		REFERENCES solaruser.user_auth_token(auth_token) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE CASCADE,
+	CONSTRAINT user_auth_token_node_node_fk FOREIGN KEY (node_id)
+		REFERENCES solarnet.sn_node (node_id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
 CREATE VIEW solaruser.user_auth_token_login  AS
 	SELECT
 		t.auth_token AS username,
@@ -162,40 +174,3 @@ CREATE TABLE solaruser.user_node_cert (
 		ON UPDATE NO ACTION ON DELETE NO ACTION,
 	CONSTRAINT user_node_cert_unq UNIQUE (user_id, conf_key)
 );
-
-/* === USER NODE AUTH TOKEN ================================================
- * Holds user node authentication tokens. View node_auth_token_login used
- * to support node authentication.
- */
-
-CREATE TABLE solaruser.user_node_auth_token (
-	auth_token		CHARACTER(20) NOT NULL,
-	created			TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	node_id			BIGINT NOT NULL,
-	auth_secret		CHARACTER VARYING(32) NOT NULL,
-	status			CHAR(1) NOT NULL,
-	CONSTRAINT user_node_auth_token_pkey PRIMARY KEY (auth_token),
-	CONSTRAINT user_node_auth_token_node_fk FOREIGN KEY (node_id)
-		REFERENCES solaruser.user_node (node_id) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE CASCADE
-);
-
-DROP VIEW IF EXISTS solaruser.node_auth_token_login;
-CREATE VIEW solaruser.node_auth_token_login  AS
-	SELECT
-		n.node_id AS node_id,
-		t.auth_token AS auth_token,
-		t.auth_secret AS password,
-		n.user_id AS user_id,
-		u.email AS username,
-		u.disp_name AS display_name,
-		ut.auth_token AS user_auth_token,
-		ut.auth_secret AS user_auth_secret
-	FROM solaruser.user_node n
-	INNER JOIN solaruser.user_user u ON u.id = n.user_id
-	LEFT OUTER JOIN solaruser.user_node_auth_token t 
-		ON t.node_id = n.node_id AND t.status = 'v'::bpchar
-	LEFT OUTER JOIN solaruser.user_auth_token ut 
-		ON ut.user_id = n.user_id AND t.status = 'v'::bpchar
-	WHERE 
-		u.enabled = TRUE;
--- net.solarnetwork.central.datum/defs/sql/postgres/updates/NET-29-node-auth-tokens.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/updates/NET-29-node-auth-tokens.sql
@@ -1,2 +1,14 @@
 ALTER TABLE solaruser.user_node
    ADD COLUMN private boolean NOT NULL DEFAULT FALSE;
+
+CREATE TABLE solaruser.user_auth_token_node (
+	auth_token CHARACTER(20) NOT NULL, 
+	node_id BIGINT NOT NULL,
+	CONSTRAINT user_auth_token_node_pkey PRIMARY KEY (auth_token, node_id),
+	CONSTRAINT user_auth_token_node_token_fk FOREIGN KEY (auth_token)
+		REFERENCES solaruser.user_auth_token(auth_token) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE CASCADE,
+	CONSTRAINT user_auth_token_node_node_fk FOREIGN KEY (node_id)
+		REFERENCES solarnet.sn_node (node_id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE CASCADE
+);
