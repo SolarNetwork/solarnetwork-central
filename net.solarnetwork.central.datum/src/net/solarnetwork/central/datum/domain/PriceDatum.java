8fceecd19b7263294f2e9da7c7028eb943a7f56d
Matt Magoffin <git+matt@msqr.us>
2013-09-29 20:02:59 +1300

    Add TODO comment re using BigDecimal for price value.

--- net.solarnetwork.central.datum/src/net/solarnetwork/central/datum/domain/PriceDatum.java
+++ net.solarnetwork.central.datum/src/net/solarnetwork/central/datum/domain/PriceDatum.java
@@ -20,30 +20,29 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
  * 02111-1307 USA
  * ===================================================================
- * $Id$
- * ===================================================================
  */
 
 package net.solarnetwork.central.datum.domain;
 
-
 /**
  * Domain object for a unit of data collected from a power price monitor.
- *
- * <p>Note a {@code PriceDatum} is not directly related to a {@code SolarNode},
- * and the {@code nodeId} value may actually be <em>null</em>. This class
- * implements both {@link NodeDatum} and {@link LocationDatum} for ease of use,
- * although strictly speaking it is only a {@link LocationDatum}.</p>
+ * 
+ * <p>
+ * Note a {@code PriceDatum} is not directly related to a {@code SolarNode}, and
+ * the {@code nodeId} value may actually be <em>null</em>. This class implements
+ * both {@link NodeDatum} and {@link LocationDatum} for ease of use, although
+ * strictly speaking it is only a {@link LocationDatum}.
+ * </p>
  * 
  * @author matt
- * @version $Revision$ $Date$
+ * @version 1.0
  */
 public class PriceDatum extends BaseNodeDatum implements LocationDatum {
 
 	private static final long serialVersionUID = 4601794526965944988L;
 
 	private Long locationId;
-	private Float price = null;
+	private Float price = null; // TODO: should be BigDecimal
 
 	/**
 	 * Default constructor.
@@ -51,24 +50,24 @@ public class PriceDatum extends BaseNodeDatum implements LocationDatum {
 	public PriceDatum() {
 		super();
 	}
-	
+
 	@Override
 	public String toString() {
-		return "PriceDatum{nodeId=" +getNodeId()
-			+",locationId=" +getLocationId()
-			+",price=" +this.price
-			+'}';
+		return "PriceDatum{nodeId=" + getNodeId() + ",locationId=" + getLocationId() + ",price="
+				+ this.price + '}';
 	}
-	
+
 	/**
 	 * @return the locationId
 	 */
+	@Override
 	public Long getLocationId() {
 		return locationId;
 	}
 
 	/**
-	 * @param locationId the locationId to set
+	 * @param locationId
+	 *        the locationId to set
 	 */
 	public void setLocationId(Long locationId) {
 		this.locationId = locationId;
@@ -80,12 +79,13 @@ public class PriceDatum extends BaseNodeDatum implements LocationDatum {
 	public Float getPrice() {
 		return price;
 	}
-	
+
 	/**
-	 * @param price the price to set
+	 * @param price
+	 *        the price to set
 	 */
 	public void setPrice(Float price) {
 		this.price = price;
 	}
-	
+
 }
