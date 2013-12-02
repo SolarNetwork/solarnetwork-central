08348aa623e0b8f7a0e6a4b074e0a2f9667246d6
Matt Magoffin <git+matt@msqr.us>
2013-12-03 08:45:39 +1300

    Fix comments.

--- net.solarnetwork.central.query.biz.dao/src/net/solarnetwork/central/query/biz/dao/DaoQueryBiz.java
+++ net.solarnetwork.central.query.biz.dao/src/net/solarnetwork/central/query/biz/dao/DaoQueryBiz.java
@@ -20,8 +20,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
  * 02111-1307 USA
  * ===================================================================
- * $Id$
- * ===================================================================
  */
 
 package net.solarnetwork.central.query.biz.dao;
@@ -69,7 +67,7 @@ import org.springframework.transaction.annotation.Transactional;
  * Implementation of {@link QueryBiz}.
  * 
  * @author matt
- * @version $Revision$ $Date$
+ * @version 1.1
  */
 public class DaoQueryBiz implements QueryBiz {
 
