ff584a3355124b4903c93ca12cfa5fef0904db69
Matt Magoffin <git+matt@msqr.us>
2014-08-23 20:22:33 +1200

    To allow passing as NULL to functions, remove NOT NULL from declaration.

--- net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-common.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-common.sql
@@ -1,13 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS solarcommon;
 
 CREATE DOMAIN solarcommon.node_id
-  AS bigint
-  NOT NULL;
+  AS bigint;
 
 CREATE DOMAIN solarcommon.source_id
   AS character varying(32)
-  DEFAULT ''
-  NOT NULL;
+  DEFAULT '';
 
 CREATE DOMAIN solarcommon.ts
   AS timestamp with time zone
