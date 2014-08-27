0113b365aece712ad07cc3b68e75a7fbcf574880
Matt Magoffin <git+matt@msqr.us>
2014-08-28 06:34:13 +1200

    Implement /range API endpoints for general node data.

---
+++ net.solarnetwork.central.query.web/src/net/solarnetwork/central/query/web/domain/GeneralReportableIntervalCommand.java
@@ -0,0 +1,72 @@
+/* ==================================================================
+ * GeneralReportableIntervalCommand.java - Aug 28, 2014 6:06:46 AM
+ * 
+ * Copyright 2007-2014 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.query.web.domain;
+
+import org.joda.time.DateTime;
+
+/**
+ * Command for general reportable interval queries.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class GeneralReportableIntervalCommand {
+
+	private Long nodeId;
+	private String sourceId;
+	private DateTime start;
+	private DateTime end;
+
+	public Long getNodeId() {
+		return nodeId;
+	}
+
+	public void setNodeId(Long nodeId) {
+		this.nodeId = nodeId;
+	}
+
+	public String getSourceId() {
+		return sourceId;
+	}
+
+	public void setSourceId(String sourceId) {
+		this.sourceId = sourceId;
+	}
+
+	public DateTime getStart() {
+		return start;
+	}
+
+	public void setStart(DateTime start) {
+		this.start = start;
+	}
+
+	public DateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(DateTime end) {
+		this.end = end;
+	}
+
+}
