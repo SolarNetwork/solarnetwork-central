17a7a39b35f3382f5f4199153142a7b96eda170d
Matt Magoffin <git+matt@msqr.us>
2014-09-10 07:25:54 +1200

    Add finder functions for datum that doesn't use multiple queries (i.e. replacement for solaragg.find_datum_samples_for_time_slot).

--- net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-generic-datum-agg-functions.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-generic-datum-agg-functions.sql
@@ -96,6 +96,88 @@ ORDER BY d.ts
 $BODY$
   LANGUAGE sql STABLE;
 
+CREATE OR REPLACE FUNCTION solaragg.find_datum_for_time_slot(
+	IN node bigint, 
+	IN source text[], 
+	IN start_ts timestamp with time zone, 
+	IN span interval, 
+	IN tolerance interval DEFAULT interval '20 minutes')
+  RETURNS TABLE(ts timestamp with time zone, source_id text, tsms bigint, percent real, tdiffms integer, jdata json) AS
+$BODY$
+SELECT * FROM (
+	SELECT 
+		d.ts,
+		d.source_id,
+		CAST(EXTRACT(EPOCH FROM d.ts) * 1000 AS BIGINT) as tsms,
+		CASE 
+			WHEN lead(d.ts) over win < start_ts OR lag(d.ts) over win > (start_ts + span)
+				THEN -1::real
+			WHEN d.ts < start_ts OR lag(d.ts) over win > (start_ts + span)
+				THEN 0::real
+			WHEN d.ts > (start_ts + span) AND lag(d.ts) over win IS NULL
+				THEN 0::real
+			WHEN d.ts > (start_ts + span)
+				THEN (1.0::real - EXTRACT('epoch' FROM (d.ts - (start_ts + span))) / EXTRACT('epoch' FROM (d.ts - lag(d.ts) over win)))::real
+			WHEN lag(d.ts) over win < start_ts
+				THEN (EXTRACT('epoch' FROM (d.ts - start_ts)) / EXTRACT('epoch' FROM (d.ts - lag(d.ts) over win)))::real
+			ELSE 1::real
+		END AS percent,
+		COALESCE(CAST(EXTRACT(EPOCH FROM d.ts - lag(d.ts) over win) * 1000 AS INTEGER), 0) as tdiff,
+		d.jdata as jdata
+	FROM solardatum.da_datum d
+	WHERE d.node_id = node
+		AND d.source_id = ANY(source)
+		AND d.ts >= start_ts - tolerance
+		AND d.ts <= start_ts + span + tolerance
+	WINDOW win AS (PARTITION BY d.source_id ORDER BY d.ts)
+	ORDER BY d.ts
+) AS sub
+WHERE 
+	sub.percent > -1
+$BODY$
+  LANGUAGE sql STABLE;
+
+CREATE OR REPLACE FUNCTION solaragg.find_datum_for_minute_time_slots(
+	IN node bigint, 
+	IN source text[], 
+	IN start_ts timestamp with time zone, 
+	IN span interval, 
+	IN slotsecs integer DEFAULT 600,
+	IN tolerance interval DEFAULT '00:20:00'::interval)
+  RETURNS TABLE(ts timestamp with time zone, ts_start timestamp with time zone, source_id text, tsms bigint, percent real, tdiffms integer, jdata json) AS
+$BODY$
+SELECT * FROM (
+	SELECT 
+		d.ts,
+		solaragg.minute_time_slot(d.ts) as ts_start,
+		d.source_id,
+		CAST(EXTRACT(EPOCH FROM solaragg.minute_time_slot(d.ts)) * 1000 AS BIGINT) as tsms,
+		CASE 
+			WHEN lead(d.ts) over win < start_ts OR d.ts > (start_ts + span)
+				THEN -1::real
+			WHEN d.ts < start_ts OR lag(d.ts) over win > (start_ts + span)
+				THEN 0::real
+			WHEN d.ts > (start_ts + span) AND lag(d.ts) over win IS NULL
+				THEN 0::real
+			WHEN solaragg.minute_time_slot(lag(d.ts) over win) < solaragg.minute_time_slot(d.ts)
+				THEN (1::real - EXTRACT('epoch' FROM solaragg.minute_time_slot(d.ts) - lag(d.ts) over win) / EXTRACT('epoch' FROM d.ts - lag(d.ts) over win))::real
+			ELSE 1::real
+		END AS percent,
+		COALESCE(CAST(EXTRACT(EPOCH FROM d.ts - lag(d.ts) over win) * 1000 AS INTEGER), 0) as tdiff,
+		d.jdata as jdata
+	FROM solardatum.da_datum d
+	WHERE d.node_id = node
+		AND d.source_id = ANY(source)
+		AND d.ts >= start_ts - tolerance
+		AND d.ts <= start_ts + span + tolerance
+	WINDOW win AS (PARTITION BY d.source_id ORDER BY d.ts)
+	ORDER BY d.ts, d.source_id
+) AS sub
+WHERE 
+	sub.percent > -1
+$BODY$
+  LANGUAGE sql STABLE;
+
 CREATE OR REPLACE FUNCTION solaragg.calc_datum_time_slot(
 	IN node bigint, 
 	IN source text, 
