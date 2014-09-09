90798cfcc964f766a3e507ed119ccad946017071
Matt Magoffin <git+matt@msqr.us>
2014-09-10 11:20:31 +1200

    Tidy up loose ends; add SQL function to return reporting-ready minute-aggregate data.

--- net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-generic-datum-agg-functions.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-generic-datum-agg-functions.sql
@@ -1,12 +1,34 @@
 CREATE OR REPLACE FUNCTION solaragg.minute_time_slot(ts timestamp with time zone, sec integer default 600)
-  RETURNS timestamp with time zone AS
+  RETURNS timestamp with time zone 
+  LANGUAGE sql 
+  IMMUTABLE AS
 $BODY$
 	SELECT date_trunc('hour', ts) + (
 		ceil(extract('epoch' from ts) - extract('epoch' from date_trunc('hour', ts))) 
 		- ceil(extract('epoch' from ts))::bigint % sec
 	) * interval '1 second'
+$BODY$;
+
+/**
+ * Return a valid minute-level time slot seconds value for an arbitrary input value.
+ * This function is meant to validate and correct inappropriate input for minute level
+ * time slot second values, which must be between 60 and 1800 and evenly divide into 1800.
+ * 
+ * @param secs the seconds to validate
+ * @returns integer seconds value, possibly different than the input seconds value
+ */
+CREATE OR REPLACE FUNCTION solaragg.slot_seconds(secs integer default 600)
+  RETURNS integer 
+  LANGUAGE sql 
+  IMMUTABLE AS
 $BODY$
-  LANGUAGE sql IMMUTABLE;
+	SELECT 
+	CASE 
+		WHEN secs < 60 OR secs > 1800 OR 1800 % secs <> 0 THEN 600 
+	ELSE
+		secs
+	END
+$BODY$;
 
 CREATE OR REPLACE FUNCTION solardatum.trigger_agg_stale_datum()
   RETURNS trigger AS
@@ -31,70 +53,6 @@ $BODY$BEGIN
 	END CASE;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE;
-
-CREATE OR REPLACE FUNCTION solaragg.find_datum_samples_for_time_slot(
-	IN node bigint, 
-	IN source text, 
-	IN start_ts timestamp with time zone, 
-	IN span interval, 
-	IN spill interval DEFAULT interval '20 minutes')
-  RETURNS TABLE(ts timestamp with time zone, tsms bigint, percent real, tdiffms integer, jdata json) AS
-$BODY$
-WITH prevd AS (
-	/*
-	Find the previous datum, but only look up to 'spill' interval prior. This query also handles the situation 
-	where there is no data earlier than the start of the specified time span, e.g. at the very start of the data.
-	*/
-	SELECT d.ts, 
-		ABS(EXTRACT('epoch' FROM (d.ts - start_ts))) as dt, 
-		CASE WHEN d.ts < start_ts THEN 0 ELSE 1 END as earlier
-	FROM solardatum.da_datum d
-	WHERE 
-		d.node_id = node
-		and d.source_id = source
-		and d.ts >= (start_ts - spill)
-		and d.ts < (start_ts + span)
-		ORDER BY earlier ASC, dt ASC
-		LIMIT 1),
-nextd AS (
-	/*
-	Find the next datum, but only look up to 'spill' interval after this query also handles the situation
-	where there is no data later than the end of the specified time span, e.g. at the very end of the data.
-	*/
-	SELECT d.ts, 
-		ABS(EXTRACT('epoch' FROM (d.ts - (start_ts + span)))) as dt, 
-		CASE WHEN d.ts < (start_ts + span) THEN 0 ELSE 1 END as earlier
-	FROM solardatum.da_datum d
-	WHERE 
-		d.node_id = node
-		and d.source_id = source
-		and d.ts >= start_ts
-		and d.ts < (start_ts + span +spill)
-		ORDER BY earlier DESC, dt ASC
-		LIMIT 1)
-SELECT 
-	d.ts,
-	CAST(EXTRACT(EPOCH FROM d.ts) * 1000 AS BIGINT) as tsms,
-		CASE 
-			WHEN d.ts < start_ts THEN 0::real
-			WHEN d.ts > (start_ts + span) AND lag(d.ts) over win IS NULL THEN 0::real
-			WHEN d.ts > (start_ts + span)
-				THEN (1.0::real - EXTRACT('epoch' FROM (d.ts - (start_ts + span))) / EXTRACT('epoch' FROM (d.ts - lag(d.ts) over win)))::real
-			WHEN lag(d.ts) over win < start_ts
-				THEN (EXTRACT('epoch' FROM (d.ts - start_ts)) / EXTRACT('epoch' FROM (d.ts - lag(d.ts) over win)))::real
-			ELSE 1.0
-		END AS percent,
-	COALESCE(CAST(EXTRACT(EPOCH FROM d.ts - lag(d.ts) over win) * 1000 AS INTEGER), 0) as tdiff,
-	d.jdata as jdata
-FROM solardatum.da_datum d
-WHERE d.node_id = node
-	AND d.source_id = source
-	AND d.ts >= (select ts FROM prevd)
-	AND d.ts <= (select ts FROM nextd)
-WINDOW win AS (order by d.ts)
-ORDER BY d.ts
-$BODY$
-  LANGUAGE sql STABLE;
 
 CREATE OR REPLACE FUNCTION solaragg.find_datum_for_time_slot(
 	IN node bigint, 
@@ -178,53 +136,35 @@ WHERE
 $BODY$
   LANGUAGE sql STABLE;
 
-CREATE OR REPLACE FUNCTION solaragg.calc_datum_time_slot(
+CREATE OR REPLACE FUNCTION solaragg.calc_datum_time_slots(
 	IN node bigint, 
-	IN source text, 
+	IN sources text[], 
 	IN start_ts timestamp with time zone, 
 	IN span interval, 
-	IN spill interval DEFAULT interval '20 minutes')
-  RETURNS json  LANGUAGE plv8 AS
+	IN slotsecs integer DEFAULT 600,
+	IN tolerance interval DEFAULT interval '20 minutes')
+  RETURNS TABLE(ts_start timestamp with time zone, source_id text, jdata json)  LANGUAGE plv8 AS
 $BODY$
 'use strict';
-var stmt = plv8.prepare('SELECT tsms, percent, tdiffms, jdata FROM solaragg.find_datum_samples_for_time_slot($1, $2, $3, $4, $5)', 
-				['bigint', 'text', 'timestamp with time zone', 'interval', 'interval']),
-	cur = stmt.cursor([node, source, start_ts, span, spill]),
-	iobj = {},
-	iobjCounts = {},
-	aobj = {},
-	sobj = {},
-	robj,
-	acc,
-	prevAcc,
-	accAvg = {},
-	inst,
-	prevInst,
-	rec,
-	prevRec,
-	obj,
-	prop,
-	propHour,
-	val,
-	runningAvgDiff,
+var runningAvgDiff,
 	runningAvgMax = 5,
 	toleranceMs,
 	hourFill = {'watts' : 'wattHours'};
 
 toleranceMs = (function() {
-	// calculate the number of milliseconds in the spill interval, which plv8 gives us as a string which we look for HH:MM:SS format
-	var hms = spill.match(/(\d{2}):(\d{2}):(\d{2})$/);
+	// calculate the number of milliseconds in the tolerance interval, which plv8 gives us as a string which we look for HH:MM:SS format
+	var hms = tolerance.match(/(\d{2}):(\d{2}):(\d{2})$/);
 	if ( hms && hms.length === 4 ) {
 		return ((hms[1] * 60 * 60 * 1000) + (hms[2] * 60 * 1000) + (hms[3] * 1000));
 	}
 	return 0;
 }());
 
-function calculateAccumulatingValue(val, prevVal, prop, ms) {
+function calculateAccumulatingValue(r, val, prevVal, prop, ms) {
 	var diff = (val - prevVal),
 		diffT = Math.abs(diff / (ms / 60000)),
 		offsetT = 0,
-		avgObj = accAvg[prop];
+		avgObj = r.accAvg[prop];
 	if ( avgObj && avgObj.average > 0 ) {
 		offsetT = (diffT / avgObj.average) * Math.pow(avgObj.samples.length / runningAvgMax, 2) 
 			* (ms > 60000 ? 1 : Math.pow(ms/60000, 2));
@@ -234,11 +174,11 @@ function calculateAccumulatingValue(val, prevVal, prop, ms) {
 		plv8.elog(NOTICE, 'Diff', diffT.toFixed(2), '; running average', avgObj.average, JSON.stringify(avgObj.samples));
 		return 0;
 	}
-	maintainAccumulatingRunningAverageDifference(prop, diffT)
+	maintainAccumulatingRunningAverageDifference(r.accAvg, prop, diffT)
 	return diff;
 }
 
-function maintainAccumulatingRunningAverageDifference(prop, diff) {
+function maintainAccumulatingRunningAverageDifference(accAvg, prop, diff) {
 	var i = 0,
 		avg = 0,
 		avgObj = accAvg[prop];
@@ -263,70 +203,207 @@ function maintainAccumulatingRunningAverageDifference(prop, diff) {
 	}
 }
 
-while ( rec = cur.fetch() ) {
-	if ( !rec.jdata ) {
-		continue;
+function finishResultObject(r) {
+	var prop,
+		robj,
+		ri,
+		ra;
+	if ( r.tsms < start_ts.getTime() /*|| r.tsms > start_ts.getTime() +*/  ) {
+		// not included in output because before time start
+		return;
 	}
-	acc = rec.jdata.a;
+	robj = {
+		ts_start : new Date(r.tsms),
+		source_id : r.source_id,
+		jdata : {}
+	};
+	ri = sn.math.util.calculateAverages(r.iobj, r.iobjCounts);
+	ra = r.aobj;
+	
+	for ( prop in ri ) {
+		robj.jdata.i = ri;
+		break;
+	}
+	for ( prop in ra ) {
+		robj.jdata.a = sn.util.merge({}, ra); // call merge() to pick up sn.math.util.fixPrecision
+		break;
+	}
+
+	if ( r.prevRec && r.prevRec.percent > 0 ) {
+		// merge last record s obj into results, but not overwriting any existing properties
+		if ( r.prevRec.jdata.s ) {
+			for ( prop in r.prevRec.jdata.s ) {
+				robj.jdata.s = r.prevRec.jdata.s;
+				break;
+			}
+		}
+		if ( Array.isArray(r.prevRec.jdata.t) && r.prevRec.jdata.t.length > 0 ) {
+			robj.jdata.t = r.prevRec.jdata.t;
+		}
+	}
+	plv8.return_next(robj);
+}
+
+function handleAccumulatingResult(rec, result) {
+	var acc = rec.jdata.a,
+		prevAcc = result.prevAcc,
+		aobj = result.aobj,
+		prop;
 	if ( acc && prevAcc && rec.tdiffms <= toleranceMs ) {
 		// accumulating data
 		for ( prop in acc ) {
 			if ( prevAcc[prop] !== undefined ) {				
-				sn.math.util.addto(prop, calculateAccumulatingValue(acc[prop], prevAcc[prop], prop, rec.tdiffms), aobj, rec.percent);
+				sn.math.util.addto(prop, calculateAccumulatingValue(result, acc[prop], prevAcc[prop], prop, rec.tdiffms), aobj, rec.percent);
 			}
 		}
 	}
-	inst = rec.jdata.i;
-	if ( inst ) {
+}
+
+function handleInstantaneousResult(rec, result, onlyHourFill) {
+	var inst = rec.jdata.i,
+		prevInst = result.prevInst,
+		iobj = result.iobj,
+		iobjCounts = result.iobjCounts,
+		prop,
+		propHour;
+	if ( inst && rec.percent > 0 && rec.tdiffms <= toleranceMs ) {
 		// instant data
 		for ( prop in inst ) {
-			if ( rec.tdiffms > toleranceMs ) {
-				continue;
+			if ( onlyHourFill !== true ) {
+				// only add instantaneous average values for 100% records; we may have to use percent to hour-fill below
+				sn.math.util.addto(prop, inst[prop], iobj, 1, iobjCounts);
 			}
-			sn.math.util.addto(prop, inst[prop], iobj, rec.percent, iobjCounts);
-			if ( prevRec && hourFill[prop] ) {
+			if ( result.prevRec && hourFill[prop] ) {
 				// calculate hour value, if not already defined for given property
 				propHour = hourFill[prop];
-				if ( !(acc && acc[propHour]) && prevInst && prevInst[prop] !== undefined ) {
-					sn.math.util.addto(propHour, sn.math.util.calculateAverageOverHours(inst[prop], prevInst[prop], rec.tdiffms), aobj, rec.percent);
+				if ( !(rec.jdata.a && rec.jdata.a[propHour]) && prevInst && prevInst[prop] !== undefined ) {
+					sn.math.util.addto(propHour, sn.math.util.calculateAverageOverHours(inst[prop], prevInst[prop], rec.tdiffms), result.aobj, rec.percent);
 				}
 			}
 		}
 	}
-	prevRec = rec;
-	prevAcc = acc;
-	prevInst = inst;
-}
-cur.close();
-stmt.free();
-
-robj = {};
-
-iobj = sn.math.util.calculateAverages(iobj, iobjCounts);
-for ( prop in iobj ) {
-	robj['i'] = iobj;
-	break;
-}
-for ( prop in aobj ) {
-	robj['a'] = sn.util.merge({}, aobj); // call merge() to pick up sn.math.util.fixPrecision
-	break;
 }
 
-if ( prevRec && prevRec.percent > 0 ) {
-	// merge last record s obj into results, but not overwriting any existing properties
-	if ( prevRec.jdata.s ) {
-		for ( prop in prevRec.jdata.s ) {
-			robj['s'] = prevRec.jdata.s;
+function handleFractionalAccumulatingResult(rec, result) {
+	var fracRec = {
+		source_id 	: rec.source_id,
+		tsms		: result.prevRec.tsms,
+		percent		: (1 - rec.percent),
+		tdiffms		: rec.tdiffms,
+		jdata		: rec.jdata
+	};
+	handleAccumulatingResult(fracRec, result);
+	handleInstantaneousResult(fracRec, result, true);
+}
+
+(function() {
+	var results = {}, // { ts_start : 123, source_id : 'A', aobj : {}, iobj : {}, iobjCounts : {}, sobj : {} ...}
+		sourceId,
+		result,
+		rec,
+		prop,
+		stmt,
+		cur,
+		slotMode = (slotsecs > 0 && slotsecs < 3600);
+	
+	if ( slotMode ) {
+		stmt = plv8.prepare('SELECT source_id, tsms, percent, tdiffms, jdata FROM solaragg.find_datum_for_minute_time_slots($1, $2, $3, $4, $5, $6)', 
+				['bigint', 'text[]', 'timestamp with time zone', 'interval', 'integer', 'interval']);
+		cur = stmt.cursor([node, sources, start_ts, span, slotsecs, tolerance]);
+	} else {
+		stmt = plv8.prepare('SELECT source_id, tsms, percent, tdiffms, jdata FROM solaragg.find_datum_for_time_slot($1, $2, $3, $4, $5)', 
+				['bigint', 'text[]', 'timestamp with time zone', 'interval', 'interval']);
+		cur = stmt.cursor([node, sources, start_ts, span, tolerance]);
+	}
+
+	while ( rec = cur.fetch() ) {
+		if ( !rec.jdata ) {
+			continue;
 		}
+		sourceId = rec.source_id;
+		result = results[sourceId];
+		if ( result === undefined ) {
+			result = { 
+				tsms : (slotMode ? rec.tsms : start_ts.getTime()), 
+				source_id : sourceId, 
+				aobj : {}, 
+				iobj : {}, 
+				iobjCounts : {}, 
+				sobj: {}, 
+				accAvg : {}
+			};
+			results[sourceId] = result;
+		} else if ( slotMode && rec.tsms !== result.tsms ) {
+			if ( rec.percent < 1 && result.prevRec && result.prevRec.tsms >= start_ts.getTime() ) {
+				// add 1-rec.percent to the previous time slot results
+				handleFractionalAccumulatingResult(rec, result);
+			}
+			finishResultObject(result);
+			result.tsms = rec.tsms;
+			result.aobj = {};
+			result.iobj = {};
+			result.iobjCounts = {};
+			result.sobj = {};
+		}
+	
+		handleAccumulatingResult(rec, result);
+		handleInstantaneousResult(rec, result);
+	
+		result.prevRec = rec;
+		result.prevAcc = rec.jdata.a;
+		result.prevInst = rec.jdata.i;
 	}
-	if ( Array.isArray(prevRec.jdata.t) && prevRec.jdata.t.length > 0 ) {
-		robj['t'] = prevRec.jdata.t;
-	}
-}
+	cur.close();
+	stmt.free();
 
-return robj;
+	for ( prop in results ) {
+		finishResultObject(results[prop]);
+	}
+}());
 $BODY$ STABLE;
 
+/**
+ * Dynamically calculate minute-level time slot aggregate values for a node and set of source IDs.
+ * 
+ * @param node				node ID
+ * @param source			array of source IDs
+ * @param start_ts			the start timestamp
+ * @param end_ts			the end timestamp
+ * @param slotsecs			the number of seconds per time slot, e.g. 600 == 10 minutes.
+ */
+CREATE OR REPLACE FUNCTION solaragg.find_agg_datum_minute(
+	IN node bigint, 
+	IN source text[], 
+	IN start_ts timestamp with time zone, 
+	IN end_ts timestamp with time zone, 
+	IN slotsecs integer DEFAULT 600,
+	IN tolerance interval DEFAULT '00:20:00'::interval)
+  RETURNS TABLE(
+	node_id solarcommon.node_id, 
+	ts_start timestamp with time zone, 
+	local_date timestamp without time zone, 
+	source_id solarcommon.source_id,
+	jdata json)
+  LANGUAGE sql 
+  STABLE AS
+$BODY$
+SELECT 
+	n.node_id::solarcommon.node_Id,
+	d.ts_start,
+	d.ts_start AT TIME ZONE COALESCE(l.time_zone, 'UTC') AS local_date, 
+	d.source_id::solarcommon.source_id,
+	d.jdata
+ FROM solaragg.calc_datum_time_slots(
+	node,
+	source,
+	solaragg.minute_time_slot(start_ts, solaragg.slot_seconds(slotsecs)),
+	(end_ts - solaragg.minute_time_slot(start_ts, solaragg.slot_seconds(slotsecs))),
+	solaragg.slot_seconds(slotsecs),
+	tolerance
+) AS d
+JOIN solarnet.sn_node n ON n.node_id = node
+LEFT OUTER JOIN solarnet.sn_loc l ON l.id = n.loc_id
+$BODY$;
 
 CREATE OR REPLACE FUNCTION solaragg.process_one_agg_stale_datum(kind char)
   RETURNS integer AS
@@ -367,7 +444,8 @@ BEGIN
 			node_tz := 'UTC';
 		END IF;
 
-		SELECT solaragg.calc_datum_time_slot(stale.node_id, stale.source_id, stale.ts_start, agg_span, interval '20 minutes')
+		SELECT jdata FROM solaragg.calc_datum_time_slots(stale.node_id, ARRAY[stale.source_id::text], 
+			stale.ts_start, agg_span, 0, interval '20 minutes')
 		INTO agg_json;
 		IF agg_json IS NULL THEN
 			CASE kind
