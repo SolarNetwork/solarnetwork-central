44954a96f4ed4cc5b5624233947f1c16e09f7ab1
Matt Magoffin <git+matt@msqr.us>
2014-10-03 10:27:45 +1300

    Add GeneralNodeDatumMetadata entity and DAO API.

---
+++ net.solarnetwork.central.datum.test/src/net/solarnetwork/central/datum/test/GeneralNodeDatumMetadataTests.java
@@ -0,0 +1,108 @@
+/* ==================================================================
+ * GeneralNodeDatumTests.java - Aug 22, 2014 3:15:33 PM
+ * 
+ * Copyright 2007-2014 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.datum.test;
+
+import java.util.HashMap;
+import java.util.Map;
+import net.solarnetwork.central.datum.domain.GeneralNodeDatumMetadata;
+import net.solarnetwork.domain.GeneralDatumMetadata;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cases for the {@link GeneralNodeDatumMetadata} class.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class GeneralNodeDatumMetadataTests {
+
+	private static final Long TEST_NODE_ID = -1L;
+	private static final String TEST_SOURCE_ID = "test.source";
+
+	private ObjectMapper objectMapper;
+
+	@Before
+	public void setup() {
+		objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion(Inclusion.NON_NULL);
+		objectMapper.setDeserializationConfig(objectMapper.getDeserializationConfig().with(
+				DeserializationConfig.Feature.USE_BIG_DECIMAL_FOR_FLOATS));
+
+	}
+
+	private GeneralNodeDatumMetadata getTestInstance() {
+		GeneralNodeDatumMetadata datum = new GeneralNodeDatumMetadata();
+		datum.setCreated(new DateTime(2014, 8, 22, 12, 0, 0, 0));
+		datum.setNodeId(TEST_NODE_ID);
+		datum.setUpdated(datum.getCreated());
+		datum.setSourceId(TEST_SOURCE_ID);
+
+		GeneralDatumMetadata samples = new GeneralDatumMetadata();
+		datum.setMeta(samples);
+
+		// some sample data
+		Map<String, Object> info = new HashMap<String, Object>(2);
+		info.put("unit", "C");
+		samples.setInfo(info);
+
+		return datum;
+	}
+
+	@Test
+	public void serializeJson() throws Exception {
+		String json = objectMapper.writeValueAsString(getTestInstance());
+		Assert.assertEquals(
+				"{\"created\":1408665600000,\"updated\":1408665600000,\"nodeId\":-1,\"sourceId\":\"test.source\","
+						+ "\"m\":{\"unit\":\"C\"}}", json);
+	}
+
+	@Test
+	public void serializeJsonWithTags() throws Exception {
+		GeneralNodeDatumMetadata datum = getTestInstance();
+		datum.getMeta().addTag("test");
+		String json = objectMapper.writeValueAsString(datum);
+		Assert.assertEquals(
+				"{\"created\":1408665600000,\"updated\":1408665600000,\"nodeId\":-1,\"sourceId\":\"test.source\","
+						+ "\"m\":{\"unit\":\"C\"},\"t\":[\"test\"]}", json);
+	}
+
+	@Test
+	public void deserializeJson() throws Exception {
+		String json = "{\"created\":1408665600000,\"sourceId\":\"Main\",\"meta\":{\"m\":{\"ploc\":2502287},\"t\":[\"foo\"]}}}";
+		GeneralNodeDatumMetadata datum = objectMapper.readValue(json, GeneralNodeDatumMetadata.class);
+		Assert.assertNotNull(datum);
+		Assert.assertNotNull(datum.getCreated());
+		Assert.assertEquals(1408665600000L, datum.getCreated().getMillis());
+		Assert.assertEquals("Main", datum.getSourceId());
+		Assert.assertNotNull(datum.getMeta());
+		Assert.assertEquals(Long.valueOf(2502287), datum.getMeta().getInfoLong("ploc"));
+		Assert.assertTrue("Has tag", datum.getMeta().hasTag("foo"));
+	}
+
+}
---
+++ net.solarnetwork.central.datum/src/net/solarnetwork/central/datum/domain/NodeSourcePK.java
@@ -0,0 +1,153 @@
+/* ==================================================================
+ * NodeSourcePK.java - Oct 3, 2014 6:47:25 AM
+ * 
+ * Copyright 2007-2014 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.datum.domain;
+
+import java.io.Serializable;
+
+/**
+ * Primary key based on a node ID and source ID.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class NodeSourcePK implements Serializable, Cloneable, Comparable<NodeSourcePK> {
+
+	private static final long serialVersionUID = -4263480807507680532L;
+
+	private Long nodeId;
+	private String sourceId;
+
+	/**
+	 * Compare two {@code NodeSourcePK} objects. Keys are ordered based on:
+	 * 
+	 * <ol>
+	 * <li>nodeId</li>
+	 * <li>sourceId</li>
+	 * </ol>
+	 * 
+	 * <em>Null</em> values will be sorted before non-<em>null</em> values.
+	 */
+	@Override
+	public int compareTo(NodeSourcePK o) {
+		if ( o == null ) {
+			return 1;
+		}
+		if ( o.nodeId == null ) {
+			return 1;
+		} else if ( nodeId == null ) {
+			return -1;
+		}
+		int comparison = nodeId.compareTo(o.nodeId);
+		if ( comparison != 0 ) {
+			return comparison;
+		}
+		if ( o.sourceId == null ) {
+			return 1;
+		} else if ( sourceId == null ) {
+			return -1;
+		}
+		return sourceId.compareToIgnoreCase(o.sourceId);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("NodeSourcePK{");
+		if ( nodeId != null ) {
+			builder.append("nodeId=");
+			builder.append(nodeId);
+			builder.append(", ");
+		}
+		if ( sourceId != null ) {
+			builder.append("sourceId=");
+			builder.append(sourceId);
+		}
+		builder.append("}");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nodeId == null) ? 0 : nodeId.hashCode());
+		result = prime * result + ((sourceId == null) ? 0 : sourceId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( getClass() != obj.getClass() ) {
+			return false;
+		}
+		NodeSourcePK other = (NodeSourcePK) obj;
+		if ( nodeId == null ) {
+			if ( other.nodeId != null ) {
+				return false;
+			}
+		} else if ( !nodeId.equals(other.nodeId) ) {
+			return false;
+		}
+		if ( sourceId == null ) {
+			if ( other.sourceId != null ) {
+				return false;
+			}
+		} else if ( !sourceId.equals(other.sourceId) ) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	protected Object clone() {
+		try {
+			return super.clone();
+		} catch ( CloneNotSupportedException e ) {
+			// shouldn't get here
+			throw new RuntimeException(e);
+		}
+	}
+
+	public Long getNodeId() {
+		return nodeId;
+	}
+
+	public void setNodeId(Long nodeId) {
+		this.nodeId = nodeId;
+	}
+
+	public String getSourceId() {
+		return sourceId;
+	}
+
+	public void setSourceId(String sourceId) {
+		this.sourceId = sourceId;
+	}
+
+}
