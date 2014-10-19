527874312beaa24b499a99b394700e1af7092307
Matt Magoffin <git+matt@msqr.us>
2014-10-20 07:19:28 +1300

    Return location.id from locationIds if that is available.

--- net.solarnetwork.central.datum/src/net/solarnetwork/central/datum/domain/DatumFilterCommand.java
+++ net.solarnetwork.central.datum/src/net/solarnetwork/central/datum/domain/DatumFilterCommand.java
@@ -351,7 +351,13 @@ public class DatumFilterCommand implements LocationDatumFilter, NodeDatumFilter,
 
 	@Override
 	public Long[] getLocationIds() {
-		return locationIds;
+		if ( locationIds != null ) {
+			return locationIds;
+		}
+		if ( location != null && location.getId() != null ) {
+			return new Long[] { location.getId() };
+		}
+		return null;
 	}
 
 	public void setLocationIds(Long[] locationIds) {
