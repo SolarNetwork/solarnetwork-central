b2f9de8a276e8b3c5167933a2430454bebaef96a
Matt Magoffin <git+matt@msqr.us>
2015-05-15 19:55:29 +1200

    Expand RepeatableTaskException to accept a generic "id" property, to aid with passing "last processed ID" back to catching code.

--- net.solarnetwork.central.common/src/net/solarnetwork/central/RepeatableTaskException.java
+++ net.solarnetwork.central.common/src/net/solarnetwork/central/RepeatableTaskException.java
@@ -18,37 +18,83 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
  * 02111-1307 USA
  * ==================================================================
- * $Id$
- * ==================================================================
  */
 
 package net.solarnetwork.central;
 
+import java.io.Serializable;
+
 /**
- * An exception when some task that can be repeated safely has failed
- * to signal to the caller to retry the task.
+ * An exception when some task that can be repeated safely has failed to signal
+ * to the caller to retry the task.
  * 
  * @author matt
- * @version $Revision$
+ * @version 1.1
  */
 public class RepeatableTaskException extends RuntimeException {
 
 	private static final long serialVersionUID = 948738004538481858L;
 
+	private final Serializable id;
+
+	/**
+	 * Default constructor.
+	 */
 	public RepeatableTaskException() {
 		super();
+		this.id = null;
+	}
+
+	/**
+	 * Construct with an ID value.
+	 * 
+	 * @param id
+	 *        An ID associated with this repeatable task.
+	 * @since 1.1
+	 */
+	public RepeatableTaskException(Serializable id) {
+		super();
+		this.id = id;
 	}
 
 	public RepeatableTaskException(String msg, Throwable t) {
 		super(msg, t);
+		this.id = null;
+	}
+
+	/**
+	 * Construct with values.
+	 * 
+	 * @param msg
+	 *        A message.
+	 * @param t
+	 *        A nested exception.
+	 * @param id
+	 *        An ID associated with this repeatable task.
+	 * @since 1.1
+	 */
+	public RepeatableTaskException(String msg, Throwable t, Serializable id) {
+		super(msg, t);
+		this.id = id;
 	}
 
 	public RepeatableTaskException(String msg) {
 		super(msg);
+		this.id = null;
 	}
 
 	public RepeatableTaskException(Throwable t) {
 		super(t);
+		this.id = null;
+	}
+
+	/**
+	 * Get the ID associated with the repeatable task.
+	 * 
+	 * @return An ID, or <em>null</em> if none available.
+	 */
+	public Serializable getId() {
+		return id;
 	}
 
 }
