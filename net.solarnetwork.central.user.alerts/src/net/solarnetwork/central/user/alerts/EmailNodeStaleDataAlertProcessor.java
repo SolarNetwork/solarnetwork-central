c20857e6e4510420dc556d6d888150f8db609982
Matt Magoffin <git+matt@msqr.us>
2015-05-18 11:28:20 +1200

    Work on node stale data alert business logic (sending mail).

--- net.solarnetwork.central.user.alerts/src/net/solarnetwork/central/user/alerts/EmailNodeStaleDataAlertProcessor.java
+++ net.solarnetwork.central.user.alerts/src/net/solarnetwork/central/user/alerts/EmailNodeStaleDataAlertProcessor.java
@@ -22,17 +22,35 @@
 
 package net.solarnetwork.central.user.alerts;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import net.solarnetwork.central.RepeatableTaskException;
+import net.solarnetwork.central.dao.SolarNodeDao;
 import net.solarnetwork.central.datum.dao.GeneralNodeDatumDao;
+import net.solarnetwork.central.datum.domain.DatumFilterCommand;
+import net.solarnetwork.central.datum.domain.GeneralNodeDatumFilterMatch;
+import net.solarnetwork.central.domain.FilterResults;
+import net.solarnetwork.central.domain.SolarNode;
 import net.solarnetwork.central.mail.MailService;
-import net.solarnetwork.central.mail.MessageTemplateDataSource;
 import net.solarnetwork.central.mail.support.BasicMailAddress;
+import net.solarnetwork.central.mail.support.ClasspathResourceMessageTemplateDataSource;
 import net.solarnetwork.central.user.dao.UserAlertDao;
 import net.solarnetwork.central.user.dao.UserDao;
 import net.solarnetwork.central.user.domain.User;
 import net.solarnetwork.central.user.domain.UserAlert;
+import net.solarnetwork.central.user.domain.UserAlertOptions;
 import net.solarnetwork.central.user.domain.UserAlertType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
 
 /**
  * Process stale data alerts for nodes.
@@ -42,17 +60,29 @@ import net.solarnetwork.central.user.domain.UserAlertType;
  */
 public class EmailNodeStaleDataAlertProcessor implements UserAlertBatchProcessor {
 
+	/** The default value for {@link #getBatchSize()}. */
 	public static final Integer DEFAULT_BATCH_SIZE = 50;
 
+	/** The default value for {@link #get}. */
+	public static final String DEFAULT_MAIL_TEMPLATE_RESOURCE = "net/solarnetwork/central/user/alerts/user-alert-NodeStaleData.txt";
+
+	private final SolarNodeDao solarNodeDao;
 	private final UserDao userDao;
 	private final UserAlertDao userAlertDao;
 	private final GeneralNodeDatumDao generalNodeDatumDao;
 	private final MailService mailService;
 	private Integer batchSize = DEFAULT_BATCH_SIZE;
+	private final MessageSource messageSource;
+	private String mailTemplateResource = DEFAULT_MAIL_TEMPLATE_RESOURCE;
+	private DateTimeFormatter timestampFormat = DateTimeFormat.forPattern("d MMM yyyy HH:mm z");
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * Construct with properties.
 	 * 
+	 * @param solarNodeDao
+	 *        The {@link SolarNodeDao} to use.
 	 * @param userDao
 	 *        The {@link UserDao} to use.
 	 * @param userAlertDao
@@ -61,40 +91,159 @@ public class EmailNodeStaleDataAlertProcessor implements UserAlertBatchProcessor
 	 *        The {@link GeneralNodeDatumDao} to use.
 	 * @param mailService
 	 *        The {@link MailService} to use.
+	 * @param messageSource
+	 *        The {@link MessageSource} to use.
 	 */
-	public EmailNodeStaleDataAlertProcessor(UserDao userDao, UserAlertDao userAlertDao,
-			GeneralNodeDatumDao generalNodeDatumDao, MailService mailService) {
+	public EmailNodeStaleDataAlertProcessor(SolarNodeDao solarNodeDao, UserDao userDao,
+			UserAlertDao userAlertDao, GeneralNodeDatumDao generalNodeDatumDao, MailService mailService,
+			MessageSource messageSource) {
 		super();
+		this.solarNodeDao = solarNodeDao;
 		this.userDao = userDao;
 		this.userAlertDao = userAlertDao;
 		this.generalNodeDatumDao = generalNodeDatumDao;
 		this.mailService = mailService;
+		this.messageSource = messageSource;
 	}
 
 	@Override
 	public Long processAlerts(Long lastProcessedAlertId) {
 		List<UserAlert> alerts = userAlertDao.findAlertsToProcess(UserAlertType.NodeStaleData,
 				lastProcessedAlertId, batchSize);
-		Long lastAlertId = null;
+		Long lastAlertId = lastProcessedAlertId;
 		try {
-			for ( UserAlert alert : alerts ) {
-				boolean sendEmail = false;
-				// TODO: process alert, send email, etc
-				if ( sendEmail ) {
-					User user = userDao.get(alert.getUserId());
-					if ( user != null ) {
-						// TODO: construct mail data source
-						BasicMailAddress addr = new BasicMailAddress(user.getName(), user.getEmail());
-						MessageTemplateDataSource msg = null; // TODO
-						mailService.sendMail(addr, msg);
+			// get our set of unique node IDs, to query for the latest data; 
+			// maintain result listing order via LinkedHashMap, so our startingId logic works later on
+			Map<Long, List<UserAlert>> alertNodeMapping = getAlertNodeMapping(alerts);
+			if ( alerts.size() > 0 ) {
+				Map<Long, List<GeneralNodeDatumFilterMatch>> latestNodeDataMapping = getLatestNodeData(alertNodeMapping);
+
+				final long now = System.currentTimeMillis();
+
+				// now we can re-iterate over alerts, processing those with stale data accordingly
+				for ( Map.Entry<Long, List<UserAlert>> me : alertNodeMapping.entrySet() ) {
+					List<GeneralNodeDatumFilterMatch> latestNodeData = latestNodeDataMapping.get(me
+							.getKey());
+					for ( UserAlert alert : me.getValue() ) {
+						Map<String, Object> alertOptions = alert.getOptions();
+						if ( alertOptions == null ) {
+							continue;
+						}
+
+						// extract options
+						Number age;
+						String[] sourceIds;
+						try {
+							age = (Number) alertOptions.get(UserAlertOptions.AGE_THRESHOLD);
+							sourceIds = (String[]) alertOptions.get(UserAlertOptions.SOURCE_IDS);
+						} catch ( ClassCastException e ) {
+							log.warn("Unexpected option data type in alert {}: {}", alert,
+									e.getMessage());
+							continue;
+						}
+
+						if ( age == null ) {
+							log.debug("Skipping alert {} that does not include {} option", alert,
+									UserAlertOptions.AGE_THRESHOLD);
+							continue;
+						}
+
+						if ( sourceIds != null ) {
+							// sort so we can to binarySearch later
+							Arrays.sort(sourceIds);
+						}
+
+						// look for first stale data matching age + source criteria
+						GeneralNodeDatumFilterMatch stale = null;
+						for ( GeneralNodeDatumFilterMatch datum : latestNodeData ) {
+							if ( datum.getId().getCreated().getMillis()
+									+ (long) (age.doubleValue() * 1000) < now
+									&& (sourceIds == null || Arrays.binarySearch(sourceIds, datum
+											.getId().getSourceId()) >= 0) ) {
+								stale = datum;
+								break;
+							}
+						}
+
+						if ( stale != null ) {
+							sendAlertMail(alert, stale);
+						}
+						lastAlertId = alert.getId();
 					}
 				}
-				lastAlertId = alert.getId();
 			}
 		} catch ( RuntimeException e ) {
 			throw new RepeatableTaskException("Error processing user alerts", e, lastAlertId);
 		}
 		return lastAlertId;
+	}
+
+	private void sendAlertMail(UserAlert alert, GeneralNodeDatumFilterMatch stale) {
+		User user = userDao.get(alert.getUserId());
+		SolarNode node = solarNodeDao.get(alert.getNodeId());
+		if ( user != null ) {
+			BasicMailAddress addr = new BasicMailAddress(user.getName(), user.getEmail());
+			Locale locale = Locale.US; // TODO: get Locale from User entity
+			Map<String, Object> model = new HashMap<String, Object>(4);
+			model.put("alert", alert);
+			model.put("user", user);
+			model.put("datum", stale);
+
+			// add a formatted datum date to model
+			DateTimeFormatter dateFormat = timestampFormat.withLocale(locale);
+			if ( node != null && node.getTimeZone() != null ) {
+				dateFormat = dateFormat.withZone(DateTimeZone.forTimeZone(node.getTimeZone()));
+			}
+			model.put("datumDate", dateFormat.print(stale.getId().getCreated()));
+
+			log.debug("Sending NodeStaleData alert to {} with model {}", user.getEmail(), model);
+
+			String subject = messageSource.getMessage("user.alert.NodeStaleData.mail.subject",
+					new Object[] { alert.getNodeId() }, locale);
+			ClasspathResourceMessageTemplateDataSource msg = new ClasspathResourceMessageTemplateDataSource(
+					locale, subject, mailTemplateResource, model);
+			mailService.sendMail(addr, msg);
+		}
+	}
+
+	private Map<Long, List<UserAlert>> getAlertNodeMapping(List<UserAlert> alerts) {
+		Map<Long, List<UserAlert>> alertNodeMapping = new LinkedHashMap<Long, List<UserAlert>>(
+				alerts.size());
+		for ( UserAlert alert : alerts ) {
+			if ( alert.getNodeId() == null ) {
+				log.debug("Skipping NodeStaleData alert with null nodeId: {}", alert);
+				continue;
+			}
+			List<UserAlert> nodeList = alertNodeMapping.get(alert.getNodeId());
+			if ( nodeList == null ) {
+				nodeList = new ArrayList<UserAlert>(4);
+				alertNodeMapping.put(alert.getNodeId(), nodeList);
+			}
+			nodeList.add(alert);
+		}
+		return alertNodeMapping;
+	}
+
+	private Map<Long, List<GeneralNodeDatumFilterMatch>> getLatestNodeData(
+			Map<Long, List<UserAlert>> alertNodeMapping) {
+		Long[] nodeIds = alertNodeMapping.keySet().toArray(new Long[alertNodeMapping.keySet().size()]);
+		DatumFilterCommand filter = new DatumFilterCommand();
+		filter.setNodeIds(nodeIds);
+		filter.setMostRecent(true);
+		FilterResults<GeneralNodeDatumFilterMatch> latestNodeData = generalNodeDatumDao.findFiltered(
+				filter, null, null, null);
+		Map<Long, List<GeneralNodeDatumFilterMatch>> latestNodeDataMapping = new HashMap<Long, List<GeneralNodeDatumFilterMatch>>(
+				latestNodeData.getReturnedResultCount());
+		for ( GeneralNodeDatumFilterMatch match : latestNodeData.getResults() ) {
+			List<GeneralNodeDatumFilterMatch> nodeData = latestNodeDataMapping.get(match.getId()
+					.getNodeId());
+			if ( nodeData == null ) {
+				nodeData = new ArrayList<GeneralNodeDatumFilterMatch>(4);
+				latestNodeDataMapping.put(match.getId().getNodeId(), nodeData);
+			}
+			nodeData.add(match);
+		}
+		return latestNodeDataMapping;
 	}
 
 	public Integer getBatchSize() {
@@ -103,6 +252,22 @@ public class EmailNodeStaleDataAlertProcessor implements UserAlertBatchProcessor
 
 	public void setBatchSize(Integer batchSize) {
 		this.batchSize = batchSize;
+	}
+
+	public String getMailTemplateResource() {
+		return mailTemplateResource;
+	}
+
+	public void setMailTemplateResource(String mailTemplateResource) {
+		this.mailTemplateResource = mailTemplateResource;
+	}
+
+	public DateTimeFormatter getTimestampFormat() {
+		return timestampFormat;
+	}
+
+	public void setTimestampFormat(DateTimeFormatter timestampFormat) {
+		this.timestampFormat = timestampFormat;
 	}
 
 }
