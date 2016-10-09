cc9d1007d517ed71b45efd2bbb9397abcc244ef4
Matt Magoffin <matt@solarnetwork.net>
2016-10-09 14:52:13 +1300

    Start work on RDBMS level support of security policy for auth tokens.

--- net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-users.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-users.sql
@@ -65,21 +65,10 @@ CREATE TABLE solaruser.user_auth_token (
 	auth_secret		CHARACTER VARYING(32) NOT NULL,
 	status			solaruser.user_auth_token_status NOT NULL,
 	token_type		solaruser.user_auth_token_type NOT NULL,
+	jpolicy			json,
 	CONSTRAINT user_auth_token_pkey PRIMARY KEY (auth_token),
 	CONSTRAINT user_auth_token_user_fk FOREIGN KEY (user_id)
 		REFERENCES solaruser.user_user (id) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE CASCADE
-);
-
-CREATE TABLE solaruser.user_auth_token_node (
-	auth_token CHARACTER(20) NOT NULL, 
-	node_id BIGINT NOT NULL,
-	CONSTRAINT user_auth_token_node_pkey PRIMARY KEY (auth_token, node_id),
-	CONSTRAINT user_auth_token_node_token_fk FOREIGN KEY (auth_token)
-		REFERENCES solaruser.user_auth_token(auth_token) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE CASCADE,
-	CONSTRAINT user_auth_token_node_node_fk FOREIGN KEY (node_id)
-		REFERENCES solarnet.sn_node (node_id) MATCH SIMPLE
 		ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
@@ -91,9 +80,7 @@ CREATE VIEW solaruser.user_auth_token_login  AS
 		u.id AS user_id,
 		u.disp_name AS display_name,
 		CAST(t.token_type AS character varying) AS token_type,
-		ARRAY(SELECT n.node_id 
-			FROM solaruser.user_auth_token_node n 
-			WHERE n.auth_token = t.auth_token) AS node_ids
+		t.policy AS policy,
 	FROM solaruser.user_auth_token t
 	INNER JOIN solaruser.user_user u ON u.id = t.user_id
 	WHERE 
