bdaace760a2a6c0a67db7b697be7bee9f1fe9ff5
Matt Magoffin <matt@solarnetwork.net>
2017-02-27 17:26:28 +1300

    Integrate new SQL into main init scripts.

--- net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-generic-datum.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-generic-datum.sql
@@ -332,3 +332,59 @@ CREATE TRIGGER populate_updated
   ON solardatum.da_meta
   FOR EACH ROW
   EXECUTE PROCEDURE solardatum.populate_updated();
+
+
+
+
+/**
+ * Find source IDs matching a datum metadata search filter.
+ *
+ * Search filters are specified using LDAP filter syntax, e.g. <code>(/m/foo=bar)</code>.
+ *
+ * @param nodes				array of node IDs
+ * @param criteria			the search filter
+ *
+ * @returns All matching source IDs.
+ */
+CREATE OR REPLACE FUNCTION solardatum.find_sources_for_meta(
+    IN nodes bigint[],
+    IN criteria text
+  )
+  RETURNS TABLE(node_id solarcommon.node_id, source_id solarcommon.source_id)
+  LANGUAGE plv8 ROWS 100 STABLE AS
+$BODY$
+'use strict';
+
+var objectPathMatcher = require('util/objectPathMatcher').default,
+	searchFilter = require('util/searchFilter').default;
+
+var filter = searchFilter(criteria),
+	stmt,
+	curs,
+	rec,
+	meta,
+	matcher,
+	resultRec = {};
+
+if ( !filter.rootNode ) {
+	plv8.elog(NOTICE, 'Malformed search filter:', criteria);
+	return;
+}
+
+stmt = plv8.prepare('SELECT node_id, source_id, jdata FROM solardatum.da_meta WHERE node_id = ANY($1)', ['bigint[]']);
+curs = stmt.cursor([nodes]);
+
+while ( rec = curs.fetch() ) {
+	meta = rec.jdata;
+	matcher = objectPathMatcher(meta);
+	if ( matcher.matchesFilter(filter) ) {
+		resultRec.node_id = rec.node_id;
+		resultRec.source_id = rec.source_id;
+		plv8.return_next(resultRec);
+	}
+}
+
+curs.close();
+stmt.free();
+
+$BODY$;
