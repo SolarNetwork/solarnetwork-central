3ada29e58b0654f360a945a5777a1c2cb0c1b5c2
Matt Magoffin <matt@solarnetwork.net>
2017-05-26 11:15:55 +1200

    Push PasswordEncoder API down to Common module; retain this API for backwards compatibility.

--- net.solarnetwork.central.common.security/src/net/solarnetwork/central/security/PasswordEncoder.java
+++ net.solarnetwork.central.common.security/src/net/solarnetwork/central/security/PasswordEncoder.java
@@ -30,44 +30,8 @@ package net.solarnetwork.central.security;
  * </p>
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
-public interface PasswordEncoder {
-
-	/**
-	 * Return <em>true</em> if a password is already encrypted or not.
-	 * 
-	 * <p>
-	 * This assumes the password has been encoded in such a way that it can be
-	 * recognized as an ecrypted password, for example with a
-	 * <code>{SSHA}</code> prefix.
-	 * </p>
-	 * 
-	 * @param rawPassword
-	 *        the password
-	 * @return boolean
-	 */
-	boolean isPasswordEncrypted(CharSequence password);
-
-	/**
-	 * Encode a raw password.
-	 * 
-	 * @return the encrypted password string
-	 */
-	String encode(CharSequence rawPassword);
-
-	/**
-	 * Verify the encoded password obtained from storage matches the submitted
-	 * raw password after it too is encoded. Returns true if the passwords
-	 * match, false if they do not. The stored password itself is never decoded.
-	 * 
-	 * @param rawPassword
-	 *        the raw password to encode and match
-	 * @param encodedPassword
-	 *        the encoded password from storage to compare with
-	 * @return true if the raw password, after encoding, matches the encoded
-	 *         password from storage
-	 */
-	boolean matches(CharSequence rawPassword, String encodedPassword);
+public interface PasswordEncoder extends net.solarnetwork.support.PasswordEncoder {
 
 }
