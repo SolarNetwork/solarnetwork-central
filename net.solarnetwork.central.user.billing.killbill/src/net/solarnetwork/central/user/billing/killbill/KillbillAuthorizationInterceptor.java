b63502904fa2752ced3dc82d6aafecf0640849ec
Matt Magoffin <matt@solarnetwork.net>
2017-08-23 10:37:07 +1200

    Add the X-Killbill-CreatedBy HTTP header for non-GET requests.

--- net.solarnetwork.central.user.billing.killbill/src/net/solarnetwork/central/user/billing/killbill/KillbillAuthorizationInterceptor.java
+++ net.solarnetwork.central.user.billing.killbill/src/net/solarnetwork/central/user/billing/killbill/KillbillAuthorizationInterceptor.java
@@ -24,11 +24,17 @@ package net.solarnetwork.central.user.billing.killbill;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.util.Base64Utils;
+import net.solarnetwork.central.security.SecurityException;
+import net.solarnetwork.central.security.SecurityUtils;
 
 /**
  * Authorization interceptor for Killbill.
@@ -46,9 +52,18 @@ public class KillbillAuthorizationInterceptor implements ClientHttpRequestInterc
 	/** The Killbill API secret HTTP header name. */
 	public static final String API_SECRET_HEADER_NAME = "X-Killbill-ApiSecret";
 
+	/** The Killbill "created by" HTTP header name. */
+	public static final String CREATED_BY_HEADER_NAME = "X-Killbill-CreatedBy";
+
+	/** The default "created by" HTTP header value. */
+	public static final String DEFAULT_CREATED_BY = "SolarUser";
+
 	private final String auth;
 	private final String apiKey;
 	private final String apiSecret;
+	private String defaultCreatedBy = DEFAULT_CREATED_BY;
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * Create a new interceptor which adds a BASIC authorization header for the
@@ -78,7 +93,33 @@ public class KillbillAuthorizationInterceptor implements ClientHttpRequestInterc
 		request.getHeaders().add("Authorization", "Basic " + this.auth);
 		request.getHeaders().set(API_KEY_HEADER_NAME, this.apiKey);
 		request.getHeaders().set(API_SECRET_HEADER_NAME, this.apiSecret);
+
+		// for POST/PUT methods include the CreatedBy header
+		if ( !HttpMethod.GET.equals(request.getMethod()) ) {
+			String createdBy = this.defaultCreatedBy;
+			try {
+				Authentication auth = SecurityUtils.getCurrentAuthentication();
+				if ( auth != null ) {
+					createdBy = auth.getName();
+				}
+			} catch ( SecurityException e ) {
+				log.debug("User not available for CreatedBy header");
+			}
+			request.getHeaders().set(CREATED_BY_HEADER_NAME, createdBy);
+		}
+
 		return execution.execute(request, body);
+	}
+
+	/**
+	 * The default value to use for the {@literal X-Killbill-CreatedBy} HTTP
+	 * header.
+	 * 
+	 * @param defaultCreatedBy
+	 *        the header value to set
+	 */
+	public void setDefaultCreatedBy(String defaultCreatedBy) {
+		this.defaultCreatedBy = defaultCreatedBy;
 	}
 
 }
