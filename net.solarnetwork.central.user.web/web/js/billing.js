feac329ba08538c56574166b18856088bd560093
Matt Magoffin <matt@solarnetwork.net>
2017-08-28 11:46:37 +1200

    Testing: get unpaid invoice list.

--- net.solarnetwork.central.user.web/web/js/billing.js
+++ net.solarnetwork.central.user.web/web/js/billing.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 	'use strict';
 	
 	$('#outstanding-invoices').each(function() {
+		/*
 		$.getJSON(SolarReg.solarUserURL('/sec/billing/systemInfo'), function(json) {
 			console.log('Got billing info: %o', json);
+		});
+		*/
+		$.getJSON(SolarReg.solarUserURL('/sec/billing/invoices/list?unpaid=true'), function(json) {
+			console.log('Got unpaid invoices: %o', json);
 		});
 		return false; // break on each()
 	});
