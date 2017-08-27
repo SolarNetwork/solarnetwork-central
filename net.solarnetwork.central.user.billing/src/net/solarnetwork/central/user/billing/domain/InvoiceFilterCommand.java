f818e7d674d53109ddfa1bc0619f88210d4b7927
Matt Magoffin <matt@solarnetwork.net>
2017-08-28 09:47:27 +1200

    Add missing equals/hashCode implementations.

--- net.solarnetwork.central.user.billing/src/net/solarnetwork/central/user/billing/domain/InvoiceFilterCommand.java
+++ net.solarnetwork.central.user.billing/src/net/solarnetwork/central/user/billing/domain/InvoiceFilterCommand.java
@@ -44,6 +44,60 @@ public class InvoiceFilterCommand extends FilterSupport implements InvoiceFilter
 	private Integer max;
 	private Boolean unpaid;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((max == null) ? 0 : max.hashCode());
+		result = prime * result + ((offset == null) ? 0 : offset.hashCode());
+		result = prime * result + ((sorts == null) ? 0 : sorts.hashCode());
+		result = prime * result + ((unpaid == null) ? 0 : unpaid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( !super.equals(obj) ) {
+			return false;
+		}
+		if ( !(obj instanceof InvoiceFilterCommand) ) {
+			return false;
+		}
+		InvoiceFilterCommand other = (InvoiceFilterCommand) obj;
+		if ( max == null ) {
+			if ( other.max != null ) {
+				return false;
+			}
+		} else if ( !max.equals(other.max) ) {
+			return false;
+		}
+		if ( offset == null ) {
+			if ( other.offset != null ) {
+				return false;
+			}
+		} else if ( !offset.equals(other.offset) ) {
+			return false;
+		}
+		if ( sorts == null ) {
+			if ( other.sorts != null ) {
+				return false;
+			}
+		} else if ( !sorts.equals(other.sorts) ) {
+			return false;
+		}
+		if ( unpaid == null ) {
+			if ( other.unpaid != null ) {
+				return false;
+			}
+		} else if ( !unpaid.equals(other.unpaid) ) {
+			return false;
+		}
+		return true;
+	}
+
 	/**
 	 * Get the mutable sort descriptors.
 	 * 
