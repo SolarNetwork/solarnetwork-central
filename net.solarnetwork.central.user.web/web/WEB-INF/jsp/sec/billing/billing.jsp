e8d9a0521fb325a3a52dab773c70ab2780612b7a
Matt Magoffin <matt@solarnetwork.net>
2017-08-29 19:42:43 +1200

    Display unpaid/paid invoices, with pagination for paid.

--- net.solarnetwork.central.user.web/web/WEB-INF/jsp/sec/billing/billing.jsp
+++ net.solarnetwork.central.user.web/web/WEB-INF/jsp/sec/billing/billing.jsp
@@ -16,10 +16,12 @@
 	<table class="table unpaid" id="outstanding-invoice-list-table">
 		<thead>
 			<tr>
+				<th><fmt:message key="billing.invoice.number.label"/></th>
 				<th><fmt:message key="billing.invoice.date.label"/></th>
 				<th><fmt:message key="billing.invoice.balance.label"/></th>
 			</tr>
 			<tr class="template">
+				<td data-tprop="invoiceNumber"></td>
 				<td data-tprop="localizedDate"></td>
 				<td><span class="label label-danger" data-tprop="localizedBalance"></span></td>
 			</tr>
@@ -38,15 +40,24 @@
 			<fmt:param value="0"/>
 		</fmt:message>
 	</p>
+	<nav aria-label="<fmt:message key='billing.invoice.pagination.label'/>" id="invoice-list-pagination" class="hidden">
+		<ul class="pagination">
+			<li><a href="#" aria-label="<fmt:message key='billing.invoice.pagination.prev.label'/>"><span aria-hidden="true">&laquo;</span></a></li>
+			<li class="template"><a href="#"><span data-tprop="pageNumber">1</span> <span class="sr-only hidden"><fmt:message key='billing.invoice.pagination.current.label'/></span></a></li>
+			<li><a href="#" aria-label="<fmt:message key='billing.invoice.pagination.next.label'/>"><span aria-hidden="true">&raquo;</span></a></li>
+		</ul>
+	</nav>
 	<table class="table" id="invoice-list-table">
 		<thead>
 			<tr>
+				<th><fmt:message key="billing.invoice.number.label"/></th>
 				<th><fmt:message key="billing.invoice.date.label"/></th>
-				<th><fmt:message key="billing.invoice.balance.label"/></th>
+				<th><fmt:message key="billing.invoice.amount.label"/></th>
 			</tr>
 			<tr class="template">
+				<td data-tprop="invoiceNumber"></td>
 				<td data-tprop="localizedDate"></td>
-				<td><span class="label label-danger" data-tprop="localizedBalance"></span></td>
+				<td><span class="label label-success" data-tprop="localizedAmount"></span></td>
 			</tr>
 		</thead>
 		<tbody>
--- net.solarnetwork.central.user.web/web/js/billing.js
+++ net.solarnetwork.central.user.web/web/js/billing.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	'use strict';
 	
+	var invoicePagination = {
+		page: 0,
+		total: 0,
+		pageSize: 10,
+	};
+	
 	function renderInvoiceTableRows(tbody, templateRow, results) {
 		var i, len, tr, invoice, prop, cell;
 		tbody.empty();
@@ -21,24 +27,100 @@ $(document).ready(function() {
 		}
 	}
 	
+	function renderInvoiceTable(table, pagination, displayCount, json) {
+		var haveRows = json && json.data && json.data.results.length > 0;
+		var total = (json.data ? json.data.totalResults : returned);
+		if ( haveRows ) {
+			var table = $(table);
+			var templateRow = table.find('tr.template');
+			var tbody = table.find('tbody');
+			renderInvoiceTableRows(tbody, templateRow, json.data.results);
+		}
+		if ( pagination ) {
+			var offset = (json.data ? json.data.startingOffset : 0);
+			var returned = (haveRows ? json.data.results.length : 0);
+			var pageCount = Math.ceil(total / invoicePagination.pageSize);
+			var page = (offset / invoicePagination.pageSize);
+			var haveMore = (offset + returned < total);
+			
+			var pageNav = $(pagination);
+			var paginationListItems = pageNav.find('li');
+			paginationListItems.not(':first').not(':last').not('.template').remove();
+			
+			var prevItem = paginationListItems.first();
+			prevItem.toggleClass('disabled', !(offset > 0));
+			
+			var nextItem = paginationListItems.last();
+			nextItem.toggleClass('disabled', !haveMore);
+
+			var templateItem = paginationListItems.filter('.template');
+			
+			var i, len, pageItem;
+			for ( i = 0, len = pageCount; i < len; i += 1 ) {
+				pageItem = templateItem.clone(true).removeClass('template');
+				if ( i === page ) {
+					pageItem.addClass('active');
+				}
+				pageItem.find("[data-tprop='pageNumber']").text(i+1);
+				pageItem.find('a').attr('href', '#'+i);
+				pageItem.insertBefore(nextItem);
+			}
+			
+			invoicePagination.total = total;
+			invoicePagination.page = page;
+			
+			pageNav.toggleClass('hidden', pageCount < 2);
+		}
+		$(displayCount).text(total);
+		return haveRows;
+	}
+	
+	function loadInvoicePage(pageNum) {
+		console.log('Want page %d', pageNum);
+		$.getJSON(SolarReg.solarUserURL('/sec/billing/invoices/list?unpaid=false&offset=' 
+				+(pageNum * invoicePagination.pageSize)
+				+'&max=' +invoicePagination.pageSize), function(json) {
+			console.log('Got invoices: %o', json);
+			var havePaid = renderInvoiceTable('#invoice-list-table', '#invoice-list-pagination', 
+					'.invoiceListCount', json);
+			$('#invoices').toggleClass('hidden', !havePaid);
+		});
+	}
+	
 	$('#outstanding-invoices').each(function() {
+		
+		// setup pagination links
+		var paginationListItems = $('#invoice-list-pagination li');
+		paginationListItems.first().find('a').on('click', function(event) {
+			event.preventDefault();
+			loadInvoicePage(invoicePagination.page - 1);
+		});
+		paginationListItems.filter('.template').find('a').on('click', function(event) {
+			event.preventDefault();
+			var page = +this.hash.substring(1);
+			loadInvoicePage(page);
+		});
+		paginationListItems.last().find('a').on('click', function(event) {
+			event.preventDefault();
+			loadInvoicePage(invoicePagination.page + 1);
+		});
+		
 		/*
 		$.getJSON(SolarReg.solarUserURL('/sec/billing/systemInfo'), function(json) {
 			console.log('Got billing info: %o', json);
 		});
 		*/
+		// get unpaid invoices
 		$.getJSON(SolarReg.solarUserURL('/sec/billing/invoices/list?unpaid=true'), function(json) {
 			console.log('Got unpaid invoices: %o', json);
-			var section = $('#outstanding-invoices');
-			var haveUnpaid = json && json.data && json.data.results.length > 0;
-			if ( haveUnpaid ) {
-				var table = $('#outstanding-invoice-list-table');
-				var templateRow = table.find('tr.template');
-				var tbody = table.find('tbody');
-				renderInvoiceTableRows(tbody, templateRow, json.data.results);
-			}
-			section.toggleClass('hidden', !haveUnpaid);
+			var haveUnpaid = renderInvoiceTable('#outstanding-invoice-list-table', null, 
+					'.outstandingInvoiceListCount', json);
+			$('#outstanding-invoices').toggleClass('hidden', !haveUnpaid);
 		});
+		
+		// get all invoices
+		loadInvoicePage(0);
+
 		return false; // break on each()
 	});
 	
