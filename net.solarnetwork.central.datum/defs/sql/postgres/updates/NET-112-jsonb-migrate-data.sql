6b6e29a87275e781e0331a9abf2c4ad933c60829
Matt Magoffin <matt@solarnetwork.net>
2017-12-06 09:56:35 +1300

    Fix data migration.

--- net.solarnetwork.central.datum/defs/sql/postgres/updates/NET-112-jsonb-migrate-data.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/updates/NET-112-jsonb-migrate-data.sql
@@ -1,25 +1,33 @@
 UPDATE solardatum.da_datum
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
 UPDATE solaragg.agg_datum_hourly
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
 UPDATE solaragg.agg_datum_daily
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
 UPDATE solaragg.agg_datum_monthly
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
 
 UPDATE solardatum.da_loc_datum
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
 UPDATE solaragg.agg_loc_datum_hourly
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
 UPDATE solaragg.agg_loc_datum_daily
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
 UPDATE solaragg.agg_loc_datum_monthly
-	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s', jdata_t = jdata->'t';
+	SET jdata_i = jdata->'i', jdata_a = jdata->'a', jdata_s = jdata->'s',
+		jdata_t = solarnet.json_array_to_text_arrary(jdata->'t');
 
