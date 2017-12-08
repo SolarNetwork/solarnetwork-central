114a2979595133f404f85f8950193791d53a8c34
Matt Magoffin <matt@solarnetwork.net>
2017-12-08 18:19:35 +1300

    Trailing whitespace removed.

--- net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-common.sql
+++ net.solarnetwork.central.datum/defs/sql/postgres/postgres-init-common.sql
@@ -5,7 +5,7 @@ RETURNS tsquery AS $$
 SELECT to_tsquery(config,
 	regexp_replace(
 			regexp_replace(
-				regexp_replace(qtext, E'[^\\w ]', '', 'g'), 
+				regexp_replace(qtext, E'[^\\w ]', '', 'g'),
 			E'\\M', ':*', 'g'),
 		E'\\s+',' & ','g')
 );
@@ -13,7 +13,7 @@ $$ LANGUAGE SQL STRICT IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION solarcommon.plainto_prefix_tsquery(qtext TEXT)
 RETURNS tsquery AS $$
-SELECT solarcommon.plainto_prefix_tsquery(get_current_ts_config(), qtext); 
+SELECT solarcommon.plainto_prefix_tsquery(get_current_ts_config(), qtext);
 $$ LANGUAGE SQL STRICT IMMUTABLE;
 
 /**
