0ded54bf305724d34c8b747ddc5ae96bc646ccae
Matt Magoffin <matt@solarnetwork.net>
2018-04-13 17:21:26 +1200

    Only provide setting specifiers for service info properties, i.e. that aren't known in advance.

--- net.solarnetwork.central.datum/src/net/solarnetwork/central/datum/support/BaseDatumExportOutputFormatService.java
+++ net.solarnetwork.central.datum/src/net/solarnetwork/central/datum/support/BaseDatumExportOutputFormatService.java
@@ -22,13 +22,11 @@
 
 package net.solarnetwork.central.datum.support;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import net.solarnetwork.central.datum.biz.DatumExportOutputFormatService;
-import net.solarnetwork.central.datum.domain.export.OutputCompressionType;
 import net.solarnetwork.settings.SettingSpecifier;
 import net.solarnetwork.settings.support.BaseSettingsSpecifierLocalizedServiceInfoProvider;
-import net.solarnetwork.settings.support.BasicTextFieldSettingSpecifier;
 
 /**
  * Base class to support implementations of
@@ -54,10 +52,7 @@ public abstract class BaseDatumExportOutputFormatService
 
 	@Override
 	public List<SettingSpecifier> getSettingSpecifiers() {
-		List<SettingSpecifier> result = new ArrayList<>(2);
-		result.add(new BasicTextFieldSettingSpecifier("compressionTypeKey",
-				String.valueOf(OutputCompressionType.None.getKey())));
-		return result;
+		return Collections.emptyList();
 	}
 
 }
