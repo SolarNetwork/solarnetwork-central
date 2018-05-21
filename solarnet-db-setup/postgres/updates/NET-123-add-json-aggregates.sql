f00371a7a28eedd09ec5e06aefdb692bc4ea51eb
Matt Magoffin <matt@solarnetwork.net>
2018-05-21 16:48:02 +1200

    NET-123: add JSON object sum aggregate.

--- solarnet-db-setup/postgres/updates/NET-123-add-json-aggregates.sql
+++ solarnet-db-setup/postgres/updates/NET-123-add-json-aggregates.sql
@@ -1,16 +1,42 @@
-CREATE OR REPLACE FUNCTION solarnet.sn_jsonb_sum_sfunc(agg_state jsonb, el jsonb)
+CREATE OR REPLACE FUNCTION solarcommon.jsonb_sum_sfunc(agg_state jsonb, el jsonb)
 RETURNS jsonb LANGUAGE plv8 IMMUTABLE AS $$
-	plv8.elog(NOTICE, 'agg_state = ', agg_state, ', el = ', el)
 	return (agg_state === undefined ? el : agg_state + el);
 $$;
 
-CREATE OR REPLACE FUNCTION solarnet.sn_jsonb_sum_finalfunc(agg_state jsonb) 
-RETURNS DOUBLE PRECISION LANGUAGE plv8 IMMUTABLE AS $$
+CREATE OR REPLACE FUNCTION solarcommon.jsonb_sum_finalfunc(agg_state jsonb)
+RETURNS jsonb LANGUAGE plv8 IMMUTABLE AS $$
     return agg_state;
 $$;
 
-CREATE AGGREGATE solarnet.sn_jsonb_sum(jsonb) (
-    sfunc = solarnet.sn_jsonb_sum_sfunc,
+CREATE AGGREGATE solarcommon.jsonb_sum(jsonb) (
+    sfunc = solarcommon.jsonb_sum_sfunc,
     stype = jsonb,
-    finalfunc = solarnet.sn_jsonb_sum_finalfunc
+    finalfunc = solarcommon.jsonb_sum_finalfunc
+);
+
+CREATE OR REPLACE FUNCTION solarcommon.jsonb_sum_object_sfunc(agg_state jsonb, el jsonb)
+RETURNS jsonb LANGUAGE plv8 IMMUTABLE AS $$
+	'use strict';
+	var addTo,
+		prop;
+	if ( !agg_state ) {
+		agg_state = el;
+	} else if ( el ) {
+		addTo = require('util/addTo').default;
+		for ( prop in el ) {
+			addTo(prop, el[prop], agg_state);
+		}
+	}
+	return agg_state;
+$$;
+
+CREATE OR REPLACE FUNCTION solarcommon.jsonb_sum_object_finalfunc(agg_state jsonb)
+RETURNS jsonb LANGUAGE plv8 IMMUTABLE AS $$
+    return agg_state;
+$$;
+
+CREATE AGGREGATE solarcommon.jsonb_sum_object(jsonb) (
+    sfunc = solarcommon.jsonb_sum_object_sfunc,
+    stype = jsonb,
+    finalfunc = solarcommon.jsonb_sum_object_finalfunc
 );
