0c97594f9e088e395814f96e6061b45cd6f2a5ce
Matt Magoffin <matt@solarnetwork.net>
2018-07-10 11:18:20 +1200

    NET-148: updates to expire procedure to handle all levels of expiration.

--- solarnet-db-setup/postgres/updates/NET-148-user-datum-expire.sql
+++ solarnet-db-setup/postgres/updates/NET-148-user-datum-expire.sql
@@ -6,6 +6,7 @@ CREATE TABLE solaruser.user_expire_data_conf (
 	user_id			BIGINT NOT NULL,
 	cname			CHARACTER VARYING(64) NOT NULL,
 	sident			CHARACTER VARYING(128) NOT NULL,
+	expire_days		integer NOT NULL,
 	sprops			jsonb,
 	filter			jsonb,
 	CONSTRAINT user_expire_data_conf_pkey PRIMARY KEY (id),
@@ -17,7 +18,7 @@ CREATE TABLE solaruser.user_expire_data_conf (
 /* Add index on user_id so we can show all entities for user. */
 CREATE INDEX user_expire_data_conf_user_idx ON solaruser.user_expire_data_conf (user_id);
 
-CREATE OR REPLACE FUNCTION solaruser.expire_datum_for_policy(userid bigint, jpolicy jsonb, older_than timestamptz)
+CREATE OR REPLACE FUNCTION solaruser.expire_datum_for_policy(userid bigint, jpolicy jsonb, age interval)
   RETURNS bigint LANGUAGE plpgsql VOLATILE AS
 $BODY$
 DECLARE
@@ -26,7 +27,7 @@ DECLARE
 	node_ids bigint[];
 	have_source_ids boolean := jpolicy->'sourceIds' IS NULL;
 	source_id_regexs text[];
-	agg_kind text := jpolicy->>'aggregationKey';
+	agg_key text := jpolicy->>'aggregationKey';
 BEGIN
 	-- filter node IDs to only those owned by user
 	SELECT ARRAY(SELECT node_id
@@ -44,48 +45,149 @@ BEGIN
 	INTO source_id_regexs;
 
 	-- delete raw data
+	WITH nlt AS (
+		SELECT
+			node_id,
+			(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+		FROM solarnet.node_local_time
+		WHERE node_id = ANY (node_ids)
+	)
 	DELETE FROM solardatum.da_datum d
-	WHERE d.node_id = ANY (node_ids)
-		AND d.ts < older_than
+	USING nlt
+	WHERE d.node_id = nlt.node_id
+		AND d.ts < nlt.older_than
 		AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
 	GET DIAGNOSTICS total_count = ROW_COUNT;
 
 	-- delete any triggered stale rows
+	WITH nlt AS (
+		SELECT
+			node_id,
+			(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+		FROM solarnet.node_local_time
+		WHERE node_id = ANY (node_ids)
+	)
 	DELETE FROM solaragg.agg_stale_datum d
-	WHERE d.node_id = ANY (node_ids)
-		AND d.ts_start < older_than
+	USING nlt
+	WHERE d.node_id = nlt.node_id
+		AND d.ts_start < nlt.older_than
 		AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs))
 		AND d.agg_kind = 'h';
 
-	IF agg_kind IN ('h', 'd', 'm') THEN
+	-- update daily audit datum counts
+	WITH nlt AS (
+		SELECT
+			node_id,
+			(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+		FROM solarnet.node_local_time
+		WHERE node_id = ANY (node_ids)
+	)
+	UPDATE solaragg.aud_datum_daily d
+	SET datum_count = 0
+	FROM nlt
+	WHERE d.node_id = nlt.node_id
+		AND d.ts_start < nlt.older_than
+		AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
+
+	IF agg_key IN ('h', 'd', 'M') THEN
 		-- delete hourly data
+		WITH nlt AS (
+			SELECT
+				node_id,
+				(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+			FROM solarnet.node_local_time
+			WHERE node_id = ANY (node_ids)
+		)
 		DELETE FROM solaragg.agg_datum_hourly d
-		WHERE d.node_id = ANY (node_ids)
+		USING nlt
+		WHERE d.node_id = nlt.node_id
 			AND d.ts_start < older_than
 			AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
 		GET DIAGNOSTICS one_count = ROW_COUNT;
 		total_count := total_count + one_count;
+
+		-- update daily audit datum counts
+		WITH nlt AS (
+			SELECT
+				node_id,
+				(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+			FROM solarnet.node_local_time
+			WHERE node_id = ANY (node_ids)
+		)
+		UPDATE solaragg.aud_datum_daily d
+		SET datum_hourly_count = 0
+		FROM nlt
+		WHERE d.node_id = nlt.node_id
+			AND d.ts_start < nlt.older_than
+			AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
 	END IF;
 
-	IF agg_kind IN ('d', 'm') THEN
-		-- delete hourly data
+	IF agg_key IN ('d', 'M') THEN
+		-- delete daily data
+		WITH nlt AS (
+			SELECT
+				node_id,
+				(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+			FROM solarnet.node_local_time
+			WHERE node_id = ANY (node_ids)
+		)
 		DELETE FROM solaragg.agg_datum_daily d
-		WHERE d.node_id = ANY (node_ids)
+		USING nlt
+		WHERE d.node_id = nlt.node_id
+			AND d.ts_start < older_than
+			AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
+		GET DIAGNOSTICS one_count = ROW_COUNT;
+		total_count := total_count + one_count;
+
+		-- update daily audit datum counts
+		WITH nlt AS (
+			SELECT
+				node_id,
+				(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+			FROM solarnet.node_local_time
+			WHERE node_id = ANY (node_ids)
+		)
+		UPDATE solaragg.aud_datum_daily d
+		SET datum_daily_pres = FALSE
+		FROM nlt
+		WHERE d.node_id = nlt.node_id
+			AND d.ts_start < nlt.older_than
+			AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
+	END IF;
+
+	IF agg_key = 'M' THEN
+		-- delete monthly data (round down to whole months only)
+		WITH nlt AS (
+			SELECT
+				node_id,
+				(date_trunc('month', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+			FROM solarnet.node_local_time
+			WHERE node_id = ANY (node_ids)
+		)
+		DELETE FROM solaragg.agg_datum_monthly d
+		USING nlt
+		WHERE d.node_id = nlt.node_id
 			AND d.ts_start < older_than
 			AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
 		GET DIAGNOSTICS one_count = ROW_COUNT;
 		total_count := total_count + one_count;
 	END IF;
 
-	IF agg_kind = 'm' THEN
-		-- delete hourly data
-		DELETE FROM solaragg.agg_datum_monthly d
-		WHERE d.node_id = ANY (node_ids)
-			AND d.ts_start < older_than
-			AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs));
-		GET DIAGNOSTICS one_count = ROW_COUNT;
-		total_count := total_count + one_count;
-	END IF;
+	-- mark all monthly audit data as stale for recalculation
+	INSERT INTO solaragg.aud_datum_daily_stale (node_id, ts_start, source_id, aud_kind)
+	WITH nlt AS (
+		SELECT
+			node_id,
+			(date_trunc('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone) - age) AT TIME ZONE time_zone AS older_than
+		FROM solarnet.node_local_time
+		WHERE node_id = ANY (node_ids)
+	)
+	SELECT d.node_id, d.ts_start, d.source_id, 'm'
+	FROM solaragg.aud_datum_monthly d
+	INNER JOIN nlt ON nlt.node_id = d.node_id
+	WHERE d.ts_start < nlt.older_than
+		AND (have_source_ids OR d.source_id ~ ANY(source_id_regexs))
+	ON CONFLICT DO NOTHING;
 
 	RETURN total_count;
 END;
