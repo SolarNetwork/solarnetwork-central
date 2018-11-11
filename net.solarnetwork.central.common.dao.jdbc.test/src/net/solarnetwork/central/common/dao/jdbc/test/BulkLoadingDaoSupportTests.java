6c20adc3db874c13df77ff554a74abb380cc2365
Matt Magoffin <matt@solarnetwork.net>
2018-11-12 12:12:04 +1300

    NET-165: add BulkLoadingDaoSupport class that implements generic parts of the bulk loading API.

---
+++ net.solarnetwork.central.common.dao.jdbc.test/src/net/solarnetwork/central/common/dao/jdbc/test/BulkLoadingDaoSupportTests.java
@@ -0,0 +1,275 @@
+/* ==================================================================
+ * BulkLoadingDaoSupportTests.java - 12/11/2018 7:34:09 AM
+ * 
+ * Copyright 2018 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.common.dao.jdbc.test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import javax.sql.DataSource;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.PlatformTransactionManager;
+import net.solarnetwork.central.common.dao.jdbc.BulkLoadingDaoSupport;
+import net.solarnetwork.central.dao.BulkLoadingDao.LoadingContext;
+import net.solarnetwork.central.dao.BulkLoadingDao.LoadingExceptionHandler;
+import net.solarnetwork.central.dao.BulkLoadingDao.LoadingOptions;
+import net.solarnetwork.central.dao.BulkLoadingDao.LoadingTransactionMode;
+import net.solarnetwork.central.domain.BaseEntity;
+import net.solarnetwork.central.support.SimpleBulkLoadingOptions;
+import net.solarnetwork.central.test.AbstractCentralTest;
+
+/**
+ * Test cases for the {@link BulkLoadingDaoSupport} class.
+ * 
+ * <p>
+ * This class manually manages transactions and creates/drops a
+ * {@literal solarnet.sntest_bulk_load_test} table for testing with.
+ * </p>
+ * 
+ * @author matt
+ * @version 1.0
+ */
+@ContextConfiguration(locations = { "classpath:/net/solarnetwork/central/test/test-tx-context.xml" })
+public class BulkLoadingDaoSupportTests extends AbstractCentralTest {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	private JdbcTemplate jdbcTemplate;
+	private PlatformTransactionManager txManager;
+	private BulkLoadingDaoSupport support;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	@Autowired
+	public void setTransactionManager(PlatformTransactionManager txManager) {
+		this.txManager = txManager;
+	}
+
+	@Before
+	public void setup() {
+		jdbcTemplate.execute(new ConnectionCallback<Void>() {
+
+			@Override
+			public Void doInConnection(Connection con) throws SQLException, DataAccessException {
+				con.createStatement()
+						.executeUpdate("drop table if exists solarnet.sntest_bulk_load_test");
+				con.createStatement().executeUpdate(
+						"create table solarnet.sntest_bulk_load_test (id bigint not null primary key,created timestamp,thingy character varying(64))");
+				return null;
+			}
+		});
+
+		support = new BulkLoadingDaoSupport(log);
+		support.setDataSource(jdbcTemplate.getDataSource());
+		support.setJdbcCall(
+				"insert into solarnet.sntest_bulk_load_test (id,created,thingy) values (?,?,?)");
+		support.setTransactionManager(this.txManager);
+	}
+
+	@After
+	public void cleanup() {
+		jdbcTemplate.execute(new ConnectionCallback<Void>() {
+
+			@Override
+			public Void doInConnection(Connection con) throws SQLException, DataAccessException {
+				con.createStatement()
+						.executeUpdate("drop table if exists solarnet.sntest_bulk_load_test");
+				return null;
+			}
+		});
+	}
+
+	private static class BulkLoadThingy extends BaseEntity {
+
+		private static final long serialVersionUID = 4184939958330559933L;
+
+		private BulkLoadThingy(long id) {
+			super();
+			setId(id);
+		}
+	}
+
+	private class TestBulkLoadingContext
+			extends BulkLoadingDaoSupport.BulkLoadingContext<BulkLoadThingy, Long> {
+
+		private final Timestamp creation = new Timestamp(System.currentTimeMillis());
+
+		private int commitCount = 0;
+
+		private TestBulkLoadingContext(LoadingOptions options,
+				LoadingExceptionHandler<BulkLoadThingy, Long> exceptionHandler) throws SQLException {
+			support.super(options, exceptionHandler);
+		}
+
+		@Override
+		protected void doLoad(BulkLoadThingy entity, PreparedStatement stmt, long index)
+				throws SQLException {
+			stmt.setLong(1, index);
+			stmt.setTimestamp(2, creation);
+			stmt.setString(3, "Thing " + index);
+			stmt.executeUpdate();
+		}
+
+		@Override
+		public void commit() {
+			super.commit();
+			commitCount++;
+		}
+
+	}
+
+	private int countOfBulkLoadThingy() {
+		return jdbcTemplate.queryForObject("select count(*) from solarnet.sntest_bulk_load_test",
+				Integer.class);
+	}
+
+	@Test
+	public void loadInSingleTransaction() throws SQLException {
+		int rowCount = 50;
+		LoadingOptions options = new SimpleBulkLoadingOptions("Test", null,
+				LoadingTransactionMode.SingleTransaction, null);
+		try (TestBulkLoadingContext ctx = new TestBulkLoadingContext(options,
+				new LoadingExceptionHandler<BulkLoadingDaoSupportTests.BulkLoadThingy, Long>() {
+
+					@Override
+					public void handleLoadingException(Throwable t,
+							LoadingContext<BulkLoadThingy, Long> context) {
+						throw new RuntimeException(t);
+					}
+				})) {
+			for ( long i = 0; i < rowCount; i++ ) {
+				ctx.load(new BulkLoadThingy(i));
+			}
+			ctx.commit();
+			assertThat("Commit count", ctx.commitCount, equalTo(1));
+		}
+		assertThat("Inserted row count", countOfBulkLoadThingy(), equalTo(rowCount));
+	}
+
+	@Test
+	public void loadInSingleTransactionRollback() throws SQLException {
+		int rowCount = 50;
+		LoadingOptions options = new SimpleBulkLoadingOptions("Test", null,
+				LoadingTransactionMode.SingleTransaction, null);
+		try (TestBulkLoadingContext ctx = new TestBulkLoadingContext(options,
+				new LoadingExceptionHandler<BulkLoadingDaoSupportTests.BulkLoadThingy, Long>() {
+
+					@Override
+					public void handleLoadingException(Throwable t,
+							LoadingContext<BulkLoadThingy, Long> context) {
+						throw new RuntimeException(t);
+					}
+				})) {
+			for ( long i = 0; i < rowCount; i++ ) {
+				ctx.load(new BulkLoadThingy(i));
+			}
+			ctx.rollback();
+			assertThat("Commit count", ctx.commitCount, equalTo(0));
+		}
+		assertThat("Inserted row count", countOfBulkLoadThingy(), equalTo(0));
+	}
+
+	@Test
+	public void loadInSingleTransactionImplicitRollback() throws SQLException {
+		int rowCount = 50;
+		LoadingOptions options = new SimpleBulkLoadingOptions("Test", null,
+				LoadingTransactionMode.SingleTransaction, null);
+		try (TestBulkLoadingContext ctx = new TestBulkLoadingContext(options,
+				new LoadingExceptionHandler<BulkLoadingDaoSupportTests.BulkLoadThingy, Long>() {
+
+					@Override
+					public void handleLoadingException(Throwable t,
+							LoadingContext<BulkLoadThingy, Long> context) {
+						throw new RuntimeException(t);
+					}
+				})) {
+			for ( long i = 0; i < rowCount; i++ ) {
+				ctx.load(new BulkLoadThingy(i));
+			}
+			// no explicit rollback here...
+			assertThat("Commit count", ctx.commitCount, equalTo(0));
+		}
+		assertThat("Inserted row count", countOfBulkLoadThingy(), equalTo(0));
+	}
+
+	@Test
+	public void loadInBatchTransactions() throws SQLException {
+		int rowCount = 50;
+		LoadingOptions options = new SimpleBulkLoadingOptions("Test", 10,
+				LoadingTransactionMode.BatchTransactions, null);
+		try (TestBulkLoadingContext ctx = new TestBulkLoadingContext(options,
+				new LoadingExceptionHandler<BulkLoadingDaoSupportTests.BulkLoadThingy, Long>() {
+
+					@Override
+					public void handleLoadingException(Throwable t,
+							LoadingContext<BulkLoadThingy, Long> context) {
+						throw new RuntimeException(t);
+					}
+				})) {
+			for ( long i = 0; i < rowCount; i++ ) {
+				ctx.load(new BulkLoadThingy(i));
+			}
+			ctx.commit();
+			assertThat("Commit count", ctx.commitCount, equalTo(5));
+		}
+		assertThat("Inserted row count", countOfBulkLoadThingy(), equalTo(rowCount));
+	}
+
+	@Test
+	public void loadInBatchTransactionsRollbackAndStop() throws SQLException {
+		int rowCount = 50;
+		LoadingOptions options = new SimpleBulkLoadingOptions("Test", 10,
+				LoadingTransactionMode.BatchTransactions, null);
+		try (TestBulkLoadingContext ctx = new TestBulkLoadingContext(options,
+				new LoadingExceptionHandler<BulkLoadingDaoSupportTests.BulkLoadThingy, Long>() {
+
+					@Override
+					public void handleLoadingException(Throwable t,
+							LoadingContext<BulkLoadThingy, Long> context) {
+						throw new RuntimeException(t);
+					}
+				})) {
+			for ( long i = 0; i < rowCount; i++ ) {
+				ctx.load(new BulkLoadThingy(i));
+			}
+			ctx.rollback();
+			assertThat("Commit count", ctx.commitCount, equalTo(4));
+		}
+		assertThat("Inserted row count", countOfBulkLoadThingy(),
+				equalTo(rowCount - options.getBatchSize()));
+	}
+}
---
+++ net.solarnetwork.central.common.dao.jdbc/src/net/solarnetwork/central/common/dao/jdbc/BulkLoadingDaoSupport.java
@@ -0,0 +1,333 @@
+/* ==================================================================
+ * BulkLoadingDaoSupport.java - 12/11/2018 6:06:32 AM
+ * 
+ * Copyright 2018 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.common.dao.jdbc;
+
+import static net.solarnetwork.central.dao.BulkLoadingDao.LoadingTransactionMode.BatchTransactions;
+import static net.solarnetwork.central.dao.BulkLoadingDao.LoadingTransactionMode.NoTransaction;
+import static net.solarnetwork.central.dao.BulkLoadingDao.LoadingTransactionMode.SingleTransaction;
+import static net.solarnetwork.central.dao.BulkLoadingDao.LoadingTransactionMode.TransactionCheckpoints;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import net.solarnetwork.central.dao.BulkLoadingDao;
+import net.solarnetwork.central.dao.BulkLoadingDao.LoadingExceptionHandler;
+import net.solarnetwork.central.dao.BulkLoadingDao.LoadingOptions;
+import net.solarnetwork.central.domain.Entity;
+
+/**
+ * Helper class for {@link net.solarnetwork.central.dao.BulkLoadingDao}
+ * implementations that uses a JDBC prepared statement for bulk loading
+ * operations.
+ * 
+ * <p>
+ * To use this class, create an instance and configure the {@link DataSource},
+ * {@link PlatformTransactionManager}, and {@code jdbcCall} to use, which can be
+ * a simple prepared statement or a callable statement. Then create a subclass
+ * of {@link BulkLoadingDaoSupport.BulkLoadingContext} and implement the
+ * {@link BulkLoadingDaoSupport.BulkLoadingContext.doLoad(T, PreparedStatement,
+ * long)} method to set the prepared statement parameters and execute the update
+ * for each entity to be loaded.
+ * </p>
+ * 
+ * @author matt
+ * @version 1.0
+ */
+public class BulkLoadingDaoSupport {
+
+	/** The default batch size. */
+	public static final int DEFAULT_BATCH_SIZE = 100;
+
+	private final Logger log;
+	private PlatformTransactionManager txManager;
+	private DataSource dataSource;
+	private String jdbcCall;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param log
+	 *        the logger to use
+	 */
+	public BulkLoadingDaoSupport(Logger log) {
+		super();
+		this.log = log;
+	}
+
+	/**
+	 * Abstract implementation of a bulk loading context.
+	 * 
+	 * <p>
+	 * This class implements the majority of a bulk loading context. Extending
+	 * classes must override {@link #doLoad(Entity, long)} to perform the actual
+	 * loading of an entity.
+	 * </p>
+	 * 
+	 * @param <T>
+	 *        the entity type
+	 * @param <PK>
+	 *        the primary key type
+	 */
+	public abstract class BulkLoadingContext<T extends Entity<PK>, PK extends Serializable>
+			implements BulkLoadingDao.LoadingContext<T, PK> {
+
+		private final LoadingOptions options;
+		private final LoadingExceptionHandler<T, PK> exceptionHandler;
+		private final int batchSize;
+		private long numLoaded;
+		private final TransactionStatus transaction;
+		private Connection con;
+		private PreparedStatement stmt;
+		private TransactionStatus batchTransaction;
+		private Object transactionCheckpoint;
+
+		public BulkLoadingContext(LoadingOptions options,
+				LoadingExceptionHandler<T, PK> exceptionHandler) throws SQLException {
+			super();
+			if ( options == null ) {
+				throw new IllegalArgumentException("The LoadingOptions argument cannot be null");
+			}
+			this.options = options;
+			this.exceptionHandler = exceptionHandler;
+			this.numLoaded = 0;
+			if ( options.getTransactionMode() == SingleTransaction
+					|| options.getTransactionMode() == TransactionCheckpoints ) {
+				log.debug("Starting new bulk load {} overall transaction", options.getName());
+				DefaultTransactionDefinition txDef = new DefaultTransactionDefinition();
+				txDef.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+				transaction = txManager.getTransaction(txDef);
+			} else {
+				transaction = null;
+			}
+			if ( options.getBatchSize() != null && options.getBatchSize() > 0 ) {
+				this.batchSize = options.getBatchSize();
+			} else {
+				this.batchSize = DEFAULT_BATCH_SIZE;
+			}
+		}
+
+		@Override
+		public LoadingOptions getOptions() {
+			return options;
+		}
+
+		@Override
+		public long getLoadedCount() {
+			return numLoaded;
+		}
+
+		private Connection getConnection() throws SQLException {
+			Connection c = this.con;
+			if ( c != null ) {
+				return c;
+			}
+			c = DataSourceUtils.getConnection(dataSource);
+			c.setAutoCommit(options.getTransactionMode() == NoTransaction);
+			this.con = c;
+			return c;
+		}
+
+		private PreparedStatement getPreparedStatement() throws SQLException {
+			if ( this.stmt != null ) {
+				return this.stmt;
+			}
+			PreparedStatement ps = createJdbcCall(getConnection());
+			this.stmt = ps;
+			return ps;
+
+		}
+
+		@Override
+		public final void load(T entity) {
+			try {
+				if ( options.getTransactionMode() == BatchTransactions ) {
+					if ( numLoaded % batchSize == 0 ) {
+						if ( batchTransaction != null ) {
+							commit();
+						}
+						log.debug("Starting new bulk load {} batch transaction @ row {}",
+								options.getName(), numLoaded);
+						DefaultTransactionDefinition txDef = new DefaultTransactionDefinition();
+						txDef.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+						batchTransaction = txManager.getTransaction(txDef);
+					}
+				}
+				doLoad(entity, getPreparedStatement(), numLoaded);
+				numLoaded++;
+			} catch ( Exception e ) {
+				if ( exceptionHandler != null ) {
+					exceptionHandler.handleLoadingException(e, this);
+				}
+			}
+		}
+
+		protected abstract void doLoad(T entity, PreparedStatement stmt, long index) throws SQLException;
+
+		@Override
+		public void createCheckpoint() {
+			if ( options.getTransactionMode() == TransactionCheckpoints && transaction != null
+					&& !transaction.isCompleted() ) {
+				Object checkpoint = transaction.createSavepoint();
+				if ( transactionCheckpoint != null ) {
+					transaction.releaseSavepoint(transactionCheckpoint);
+				}
+				transactionCheckpoint = checkpoint;
+			}
+		}
+
+		@Override
+		public void commit() {
+			if ( batchTransaction != null ) {
+				log.debug("Committing bulk load {} batch transaction @ row {}", options.getName(),
+						numLoaded);
+				txManager.commit(batchTransaction);
+				batchTransaction = null;
+			} else if ( transaction != null && !transaction.isCompleted() ) {
+				log.debug("Committing bulk load {} overall transaction @ row {}", options.getName(),
+						numLoaded);
+				txManager.commit(transaction);
+			}
+			try {
+				close();
+				stmt = null;
+				con = null;
+			} catch ( SQLException e ) {
+				throw new TransactionSystemException("Unable to close load statement", e);
+			}
+		}
+
+		@Override
+		public void rollback() {
+			if ( transactionCheckpoint != null && transaction != null ) {
+				transaction.rollbackToSavepoint(transactionCheckpoint);
+				transaction.releaseSavepoint(transactionCheckpoint);
+				transactionCheckpoint = null;
+			} else if ( batchTransaction != null && !batchTransaction.isCompleted() ) {
+				batchTransaction.setRollbackOnly();
+				txManager.rollback(batchTransaction);
+				batchTransaction = null;
+			} else if ( transaction != null && !transaction.isCompleted() ) {
+				txManager.rollback(transaction);
+			}
+		}
+
+		@Override
+		public void close() throws SQLException {
+			if ( stmt != null && !stmt.isClosed() ) {
+				try {
+					stmt.close();
+				} catch ( SQLException e ) {
+					log.warn("Error closing bulk loading statement", e);
+				}
+			}
+			if ( con != null && !con.isClosed() ) {
+				try {
+					if ( batchTransaction != null && !batchTransaction.isCompleted() ) {
+						txManager.rollback(batchTransaction);
+					} else if ( transaction != null && !transaction.isCompleted() ) {
+						txManager.rollback(transaction);
+					}
+				} finally {
+					DataSourceUtils.releaseConnection(con, dataSource);
+				}
+			}
+		}
+
+	}
+
+	private PreparedStatement createJdbcCall(Connection con) throws SQLException {
+		PreparedStatement stmt;
+		if ( jdbcCall.startsWith("{call") ) {
+			stmt = con.prepareCall(jdbcCall);
+		} else {
+			stmt = con.prepareStatement(jdbcCall);
+		}
+		return stmt;
+	}
+
+	/**
+	 * Get the transaction manager.
+	 * 
+	 * @return the manager
+	 */
+	public PlatformTransactionManager getTransactionManager() {
+		return txManager;
+	}
+
+	/**
+	 * Set the transaction manager.
+	 * 
+	 * @param txManager
+	 *        the manager to set
+	 */
+	public void setTransactionManager(PlatformTransactionManager txManager) {
+		this.txManager = txManager;
+	}
+
+	/**
+	 * Get the JDBC data source.
+	 * 
+	 * @return the dataSource
+	 */
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	/**
+	 * Set the JDBC data source.
+	 * 
+	 * @param dataSource
+	 *        the dataSource to set
+	 */
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	/**
+	 * Get the JDBC statement to use for bulk loading.
+	 * 
+	 * @return the JDBC statement
+	 */
+	public String getJdbcCall() {
+		return jdbcCall;
+	}
+
+	/**
+	 * Set the JDBC statement to use for bulk loading.
+	 * 
+	 * @param jdbcCall
+	 *        the JDBC statement to set
+	 */
+	public void setJdbcCall(String jdbcCall) {
+		this.jdbcCall = jdbcCall;
+	}
+
+}
