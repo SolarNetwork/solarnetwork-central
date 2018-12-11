d020b84a3e346e9354c9521b3185682b1d24ea24
Matt Magoffin <matt@solarnetwork.net>
2018-12-11 20:57:26 +1300

    NET-169: add a supporting SQL view for querying on tokens with an array of valid node IDs associated with that token.

---
+++ solarnet-db-setup/postgres/updates/NET-169-solarflux.sql
@@ -0,0 +1,22 @@
+/**
+ * View of all valid node IDs for a given token, as an array.
+ *
+ * This will filter out any node IDs not present on the token policy `nodeIds` array.
+ * Additionally, archived nodes are filtered out.
+ *
+ * Typical query is:
+ *
+ *     SELECT node_id FROM solaruser.user_auth_token_node_ids
+ *     WHERE auth_token = 'token-id'
+ */
+CREATE OR REPLACE VIEW solaruser.user_auth_token_node_ids AS
+SELECT t.auth_token,
+ 	t.user_id,
+	t.jpolicy,
+	array_agg(un.node_id) AS node_ids
+FROM solaruser.user_auth_token t
+JOIN solaruser.user_node un ON un.user_id = t.user_id
+WHERE un.archived = FALSE 
+	AND t.status = 'Active'::solaruser.user_auth_token_status 
+	AND ((t.jpolicy -> 'nodeIds') IS NULL OR (t.jpolicy -> 'nodeIds') @> un.node_id::text::jsonb)
+GROUP BY t.auth_token, t.user_id;
