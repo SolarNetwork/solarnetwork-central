2901cfdd6b5710c32e86934c71dc8dfe53a0af21
Matt Magoffin <matt@solarnetwork.net>
2019-02-08 15:37:46 +1300

    Swap function order for consistency of order with other similar queries.

--- solarnet-db-setup/postgres/postgres-init-generic-datum.sql
+++ solarnet-db-setup/postgres/postgres-init-generic-datum.sql
@@ -1105,6 +1105,155 @@ $$;
  *
  * @param nodes 		the node IDs to find
  * @param sources 		the source IDs to find
+ * @param ts_min		the timestamp of the start of the time range (inclusive)
+ * @param ts_max		the timestamp of the end of the time range (inclusive)
+ */
+CREATE OR REPLACE FUNCTION solardatum.calculate_datum_diff_over(
+	nodes bigint[], sources text[], ts_min timestamptz, ts_max timestamptz)
+RETURNS TABLE(
+  ts_start timestamp with time zone,
+  ts_end timestamp with time zone,
+  time_zone text,
+  node_id bigint,
+  source_id character varying(64),
+  jdata_a jsonb
+) LANGUAGE sql STABLE AS $$
+	-- find records closest to, but not after, min date
+	-- also considering reset records, using their STARTING sample value
+	WITH latest_before_start AS (
+		SELECT DISTINCT ON (d.node_id, d.source_id) d.*
+		FROM (
+			SELECT d.ts, d.node_id, d.source_id, d.jdata_a
+			FROM  solardatum.find_latest_before(nodes, sources, ts_min) dates
+			INNER JOIN solardatum.da_datum d ON d.ts = dates.ts AND d.node_id = dates.node_id AND d.source_id = dates.source_id
+			UNION
+			SELECT DISTINCT ON (node_id, source_id) ts, node_id, source_id, jdata_as AS jdata_a
+			FROM solardatum.da_datum_aux
+			WHERE atype = 'Reset'::solardatum.da_datum_aux_type
+				AND node_id = ANY(nodes)
+				AND source_id = ANY(sources)
+				AND ts < ts_min
+			ORDER BY node_id, source_id, ts DESC
+		) d
+		ORDER BY d.node_id, d.source_id, d.ts DESC
+	)
+	-- in case no data before min date, find closest to min date or after
+	-- also considering reset records, using their STARTING sample value
+	, earliest_after_start AS (
+		SELECT DISTINCT ON (d.node_id, d.source_id) d.*
+		FROM (
+			(
+				SELECT d.ts, d.node_id, d.source_id, d.jdata_a
+				FROM solardatum.find_earliest_after(nodes, sources, ts_min) dates
+				INNER JOIN solardatum.da_datum d ON d.ts = dates.ts AND d.node_id = dates.node_id AND d.source_id = dates.source_id
+			)
+			UNION
+			(
+				SELECT DISTINCT ON (node_id, source_id) ts, node_id, source_id, jdata_as AS jdata_a
+				FROM solardatum.da_datum_aux
+				WHERE atype = 'Reset'::solardatum.da_datum_aux_type
+					AND node_id = ANY(nodes)
+					AND source_id = ANY(sources)
+					AND ts >= ts_min
+				ORDER BY node_id, source_id, ts
+			)
+		) d
+		ORDER BY d.node_id, d.source_id, d.ts
+	)
+	-- find records closest to, but not after max date (could be same as latest_before_start or earliest_after_start)
+	-- also considering reset records, using their FINAL sample value
+	, latest_before_end AS (
+		SELECT DISTINCT ON (d.node_id, d.source_id) d.*
+		FROM (
+			(
+				SELECT d.ts, d.node_id, d.source_id, d.jdata_a
+				FROM solardatum.find_latest_before(nodes, sources, ts_max) dates
+				INNER JOIN solardatum.da_datum d ON d.ts = dates.ts AND d.node_id = dates.node_id AND d.source_id = dates.source_id
+			)
+			UNION
+			(
+				SELECT DISTINCT ON (node_id, source_id) ts, node_id, source_id, jdata_af AS jdata_a
+				FROM solardatum.da_datum_aux
+				WHERE atype = 'Reset'::solardatum.da_datum_aux_type
+					AND node_id = ANY(nodes)
+					AND source_id = ANY(sources)
+					AND ts < ts_max
+				ORDER BY node_id, source_id, ts DESC
+			)
+		) d
+		ORDER BY d.node_id, d.source_id, d.ts DESC
+	)
+	-- narrow data to [start, final] pairs of rows by node,source by choosing
+	-- latest_before_start in preference to earliest_after_start
+	, d AS (
+		SELECT * FROM (
+			SELECT DISTINCT ON (d.node_id, d.source_id) d.*
+			FROM (
+				SELECT * FROM latest_before_start
+				UNION
+				SELECT * FROM earliest_after_start
+			) d
+			ORDER BY d.node_id, d.source_id, d.ts
+		) earliest
+		UNION 
+		SELECT * FROM latest_before_end
+	)
+	-- begin search for reset records WITHIN [start, final] date ranges via table of found [start, final] dates
+	, ranges AS (
+		SELECT node_id
+			, source_id
+			, min(ts) AS sdate
+			, max(ts) AS edate
+		FROM d
+		GROUP BY node_id, source_id
+	)
+	-- find all reset records per node, source within [start, final] date ranges, producing pairs
+	-- of rows for each matching record, of [FINAL, STARTING] data
+	, resets AS (
+		SELECT aux.ts - unnest(ARRAY['1 millisecond','0'])::interval AS ts
+			, aux.node_id
+			, aux.source_id
+			, unnest(ARRAY[aux.jdata_af, aux.jdata_as]) AS jdata_a
+		FROM ranges
+		INNER JOIN solardatum.da_datum_aux aux ON aux.node_id = ranges.node_id AND aux.source_id = ranges.source_id
+			AND aux.ts > ranges.sdate AND aux.ts < ranges.edate
+		WHERE atype = 'Reset'::solardatum.da_datum_aux_type
+	)
+	-- combine [start, final] pairs with reset pairs
+	, combined AS (
+		SELECT * FROM d
+		UNION
+		SELECT * FROM resets
+	)
+	-- calculate difference by node,source, of {start[, resetFinal1, resetStart1, ...], final}
+	SELECT min(d.ts) AS ts_start,
+		max(d.ts) AS ts_end,
+		min(nlt.time_zone) AS time_zone,
+		d.node_id,
+		d.source_id,
+		solarcommon.jsonb_diffsum_object(d.jdata_a ORDER BY d.ts) AS jdata_a
+	FROM combined d
+	INNER JOIN solarnet.node_local_time nlt ON nlt.node_id = d.node_id
+	GROUP BY d.node_id, d.source_id
+	ORDER BY d.node_id, d.source_id
+$$;
+
+
+/**
+ * Calculate the difference between the accumulating properties of datum over a time range.
+ *
+ * This returns one row per node ID and source ID combination found. The returned `ts_start` and `ts_end` columns will
+ * the timestamps of the found starting/ending datum records. The `jdata_a` column will be computed as the difference
+ * between the starting/ending rows, using the `solarcommon.jsonb_diff_object()` aggregate function.
+ *
+ * The `solardatum.da_datum_aux` table will be considered, for `Reset` type rows.
+ * 
+ * This function makes use of `solardatum.find_latest_before()` and `solardatum.find_earliest_after`
+ * and thus has the same restrictions as those, and as a consequence will consider all data before
+ * the given `ts_max` as possible data.
+ *
+ * @param nodes 		the node IDs to find
+ * @param sources 		the source IDs to find
  * @param ts_min		the timestamp of the start of the time range (inclusive), in node local time
  * @param ts_max		the timestamp of the end of the time range (inclusive), in node local time
  */
@@ -1257,155 +1406,6 @@ RETURNS TABLE(
 		d.source_id,
 		solarcommon.jsonb_diffsum_object(d.jdata_a ORDER BY d.ts) AS jdata_a
 	FROM combined d
-	GROUP BY d.node_id, d.source_id
-	ORDER BY d.node_id, d.source_id
-$$;
-
-
-/**
- * Calculate the difference between the accumulating properties of datum over a time range.
- *
- * This returns one row per node ID and source ID combination found. The returned `ts_start` and `ts_end` columns will
- * the timestamps of the found starting/ending datum records. The `jdata_a` column will be computed as the difference
- * between the starting/ending rows, using the `solarcommon.jsonb_diff_object()` aggregate function.
- *
- * The `solardatum.da_datum_aux` table will be considered, for `Reset` type rows.
- * 
- * This function makes use of `solardatum.find_latest_before()` and `solardatum.find_earliest_after`
- * and thus has the same restrictions as those, and as a consequence will consider all data before
- * the given `ts_max` as possible data.
- *
- * @param nodes 		the node IDs to find
- * @param sources 		the source IDs to find
- * @param ts_min		the timestamp of the start of the time range (inclusive)
- * @param ts_max		the timestamp of the end of the time range (inclusive)
- */
-CREATE OR REPLACE FUNCTION solardatum.calculate_datum_diff_over(
-	nodes bigint[], sources text[], ts_min timestamptz, ts_max timestamptz)
-RETURNS TABLE(
-  ts_start timestamp with time zone,
-  ts_end timestamp with time zone,
-  time_zone text,
-  node_id bigint,
-  source_id character varying(64),
-  jdata_a jsonb
-) LANGUAGE sql STABLE AS $$
-	-- find records closest to, but not after, min date
-	-- also considering reset records, using their STARTING sample value
-	WITH latest_before_start AS (
-		SELECT DISTINCT ON (d.node_id, d.source_id) d.*
-		FROM (
-			SELECT d.ts, d.node_id, d.source_id, d.jdata_a
-			FROM  solardatum.find_latest_before(nodes, sources, ts_min) dates
-			INNER JOIN solardatum.da_datum d ON d.ts = dates.ts AND d.node_id = dates.node_id AND d.source_id = dates.source_id
-			UNION
-			SELECT DISTINCT ON (node_id, source_id) ts, node_id, source_id, jdata_as AS jdata_a
-			FROM solardatum.da_datum_aux
-			WHERE atype = 'Reset'::solardatum.da_datum_aux_type
-				AND node_id = ANY(nodes)
-				AND source_id = ANY(sources)
-				AND ts < ts_min
-			ORDER BY node_id, source_id, ts DESC
-		) d
-		ORDER BY d.node_id, d.source_id, d.ts DESC
-	)
-	-- in case no data before min date, find closest to min date or after
-	-- also considering reset records, using their STARTING sample value
-	, earliest_after_start AS (
-		SELECT DISTINCT ON (d.node_id, d.source_id) d.*
-		FROM (
-			(
-				SELECT d.ts, d.node_id, d.source_id, d.jdata_a
-				FROM solardatum.find_earliest_after(nodes, sources, ts_min) dates
-				INNER JOIN solardatum.da_datum d ON d.ts = dates.ts AND d.node_id = dates.node_id AND d.source_id = dates.source_id
-			)
-			UNION
-			(
-				SELECT DISTINCT ON (node_id, source_id) ts, node_id, source_id, jdata_as AS jdata_a
-				FROM solardatum.da_datum_aux
-				WHERE atype = 'Reset'::solardatum.da_datum_aux_type
-					AND node_id = ANY(nodes)
-					AND source_id = ANY(sources)
-					AND ts >= ts_min
-				ORDER BY node_id, source_id, ts
-			)
-		) d
-		ORDER BY d.node_id, d.source_id, d.ts
-	)
-	-- find records closest to, but not after max date (could be same as latest_before_start or earliest_after_start)
-	-- also considering reset records, using their FINAL sample value
-	, latest_before_end AS (
-		SELECT DISTINCT ON (d.node_id, d.source_id) d.*
-		FROM (
-			(
-				SELECT d.ts, d.node_id, d.source_id, d.jdata_a
-				FROM solardatum.find_latest_before(nodes, sources, ts_max) dates
-				INNER JOIN solardatum.da_datum d ON d.ts = dates.ts AND d.node_id = dates.node_id AND d.source_id = dates.source_id
-			)
-			UNION
-			(
-				SELECT DISTINCT ON (node_id, source_id) ts, node_id, source_id, jdata_af AS jdata_a
-				FROM solardatum.da_datum_aux
-				WHERE atype = 'Reset'::solardatum.da_datum_aux_type
-					AND node_id = ANY(nodes)
-					AND source_id = ANY(sources)
-					AND ts < ts_max
-				ORDER BY node_id, source_id, ts DESC
-			)
-		) d
-		ORDER BY d.node_id, d.source_id, d.ts DESC
-	)
-	-- narrow data to [start, final] pairs of rows by node,source by choosing
-	-- latest_before_start in preference to earliest_after_start
-	, d AS (
-		SELECT * FROM (
-			SELECT DISTINCT ON (d.node_id, d.source_id) d.*
-			FROM (
-				SELECT * FROM latest_before_start
-				UNION
-				SELECT * FROM earliest_after_start
-			) d
-			ORDER BY d.node_id, d.source_id, d.ts
-		) earliest
-		UNION 
-		SELECT * FROM latest_before_end
-	)
-	-- begin search for reset records WITHIN [start, final] date ranges via table of found [start, final] dates
-	, ranges AS (
-		SELECT node_id
-			, source_id
-			, min(ts) AS sdate
-			, max(ts) AS edate
-		FROM d
-		GROUP BY node_id, source_id
-	)
-	-- find all reset records per node, source within [start, final] date ranges, producing pairs
-	-- of rows for each matching record, of [FINAL, STARTING] data
-	, resets AS (
-		SELECT aux.ts - unnest(ARRAY['1 millisecond','0'])::interval AS ts
-			, aux.node_id
-			, aux.source_id
-			, unnest(ARRAY[aux.jdata_af, aux.jdata_as]) AS jdata_a
-		FROM ranges
-		INNER JOIN solardatum.da_datum_aux aux ON aux.node_id = ranges.node_id AND aux.source_id = ranges.source_id
-			AND aux.ts > ranges.sdate AND aux.ts < ranges.edate
-		WHERE atype = 'Reset'::solardatum.da_datum_aux_type
-	)
-	-- combine [start, final] pairs with reset pairs
-	, combined AS (
-		SELECT * FROM d
-		UNION
-		SELECT * FROM resets
-	)
-	-- calculate difference by node,source, of {start[, resetFinal1, resetStart1, ...], final}
-	SELECT min(d.ts) AS ts_start,
-		max(d.ts) AS ts_end,
-		min(nlt.time_zone) AS time_zone,
-		d.node_id,
-		d.source_id,
-		solarcommon.jsonb_diffsum_object(d.jdata_a ORDER BY d.ts) AS jdata_a
-	FROM combined d
-	INNER JOIN solarnet.node_local_time nlt ON nlt.node_id = d.node_id
 	GROUP BY d.node_id, d.source_id
 	ORDER BY d.node_id, d.source_id
 $$;
