e8bfd8fa9b7e4ab5ff523a4cf59b3c0cc30a77d2
Matt Magoffin <matt@solarnetwork.net>
2019-02-11 07:00:13 +1300

    Simplify da_datum and da_loc_datum stale aggregate trigger functions.

---
+++ solarnet-db-setup/postgres/updates/NET-183-readings-agg.sql
@@ -0,0 +1,179 @@
+-- update to simplify by eliminating exceptions in favor of ON CONFLICT
+CREATE OR REPLACE FUNCTION solardatum.trigger_agg_stale_datum()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+	neighbor solardatum.da_datum;
+BEGIN
+	IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+		-- curr hour
+		INSERT INTO solaragg.agg_stale_datum (ts_start, node_id, source_id, agg_kind)
+		VALUES (date_trunc('hour', NEW.ts), NEW.node_id, NEW.source_id, 'h')
+		ON CONFLICT (agg_kind, node_id, ts_start, source_id) DO NOTHING;
+
+		-- prev hour; if the previous record for this source falls on the previous hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_datum d
+		WHERE d.ts < NEW.ts
+			AND d.ts > NEW.ts - interval '1 hour'
+			AND d.node_id = NEW.node_id
+			AND d.source_id = NEW.source_id
+		ORDER BY d.ts DESC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts < date_trunc('hour', NEW.ts) THEN
+			INSERT INTO solaragg.agg_stale_datum (ts_start, node_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.node_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, node_id, ts_start, source_id) DO NOTHING;
+		END IF;
+
+		-- next hour; if the next record for this source falls on the next hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_datum d
+		WHERE d.ts > NEW.ts
+			AND d.ts < NEW.ts + interval '1 hour'
+			AND d.node_id = NEW.node_id
+			AND d.source_id = NEW.source_id
+		ORDER BY d.ts ASC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts > date_trunc('hour', NEW.ts) THEN
+			INSERT INTO solaragg.agg_stale_datum (ts_start, node_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.node_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, node_id, ts_start, source_id) DO NOTHING;
+		END IF;
+	END IF;
+
+	IF TG_OP = 'DELETE' OR (TG_OP = 'UPDATE' AND (OLD.source_id <> NEW.source_id OR OLD.node_id <> NEW.node_id)) THEN
+		-- curr hour
+		INSERT INTO solaragg.agg_stale_datum (ts_start, node_id, source_id, agg_kind)
+		VALUES (date_trunc('hour', OLD.ts), OLD.node_id, OLD.source_id, 'h')
+		ON CONFLICT (agg_kind, node_id, ts_start, source_id) DO NOTHING;
+
+		-- prev hour; if the previous record for this source falls on the previous hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_datum d
+		WHERE d.ts < OLD.ts
+			AND d.ts > OLD.ts - interval '1 hour'
+			AND d.node_id = OLD.node_id
+			AND d.source_id = OLD.source_id
+		ORDER BY d.ts DESC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts < date_trunc('hour', OLD.ts) THEN
+			INSERT INTO solaragg.agg_stale_datum (ts_start, node_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.node_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, node_id, ts_start, source_id) DO NOTHING;
+		END IF;
+
+		-- next hour; if the next record for this source falls on the next hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_datum d
+		WHERE d.ts > OLD.ts
+			AND d.ts < OLD.ts + interval '1 hour'
+			AND d.node_id = OLD.node_id
+			AND d.source_id = OLD.source_id
+		ORDER BY d.ts ASC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts > date_trunc('hour', OLD.ts) THEN
+			INSERT INTO solaragg.agg_stale_datum (ts_start, node_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.node_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, node_id, ts_start, source_id) DO NOTHING;
+		END IF;
+	END IF;
+
+	CASE TG_OP
+		WHEN 'INSERT', 'UPDATE' THEN
+			RETURN NEW;
+		ELSE
+			RETURN OLD;
+	END CASE;
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+-- update to simplify by eliminating exceptions in favor of ON CONFLICT
+CREATE OR REPLACE FUNCTION solardatum.trigger_agg_stale_loc_datum()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+	neighbor solardatum.da_loc_datum;
+BEGIN
+	IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+		-- curr hour
+		INSERT INTO solaragg.agg_stale_loc_datum (ts_start, loc_id, source_id, agg_kind)
+		VALUES (date_trunc('hour', NEW.ts), NEW.loc_id, NEW.source_id, 'h')
+		ON CONFLICT (agg_kind, loc_id, ts_start, source_id) DO NOTHING;
+
+		-- prev hour; if the previous record for this source falls on the previous hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_loc_datum d
+		WHERE d.ts < NEW.ts
+			AND d.ts > NEW.ts - interval '1 hour'
+			AND d.loc_id = NEW.loc_id
+			AND d.source_id = NEW.source_id
+		ORDER BY d.ts DESC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts < date_trunc('hour', NEW.ts) THEN
+			INSERT INTO solaragg.agg_stale_loc_datum (ts_start, loc_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.loc_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, loc_id, ts_start, source_id) DO NOTHING;
+		END IF;
+
+		-- next hour; if the next record for this source falls on the next hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_loc_datum d
+		WHERE d.ts > NEW.ts
+			AND d.ts < NEW.ts + interval '1 hour'
+			AND d.loc_id = NEW.loc_id
+			AND d.source_id = NEW.source_id
+		ORDER BY d.ts ASC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts > date_trunc('hour', NEW.ts) THEN
+			INSERT INTO solaragg.agg_stale_loc_datum (ts_start, loc_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.loc_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, loc_id, ts_start, source_id) DO NOTHING;
+		END IF;
+	END IF;
+
+	IF TG_OP = 'DELETE' OR (TG_OP = 'UPDATE' AND (OLD.source_id <> NEW.source_id OR OLD.loc_id <> NEW.loc_id)) THEN
+		-- curr hour
+		INSERT INTO solaragg.agg_stale_loc_datum (ts_start, loc_id, source_id, agg_kind)
+		VALUES (date_trunc('hour', OLD.ts), OLD.loc_id, OLD.source_id, 'h')
+		ON CONFLICT (agg_kind, loc_id, ts_start, source_id) DO NOTHING;
+
+		-- prev hour; if the previous record for this source falls on the previous hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_loc_datum d
+		WHERE d.ts < OLD.ts
+			AND d.ts > OLD.ts - interval '1 hour'
+			AND d.loc_id = OLD.loc_id
+			AND d.source_id = OLD.source_id
+		ORDER BY d.ts DESC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts < date_trunc('hour', OLD.ts) THEN
+			INSERT INTO solaragg.agg_stale_loc_datum (ts_start, loc_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.loc_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, loc_id, ts_start, source_id) DO NOTHING;
+		END IF;
+
+		-- next hour; if the next record for this source falls on the next hour; we have to mark that hour as stale as well
+		SELECT * FROM solardatum.da_loc_datum d
+		WHERE d.ts > OLD.ts
+			AND d.ts < OLD.ts + interval '1 hour'
+			AND d.loc_id = OLD.loc_id
+			AND d.source_id = OLD.source_id
+		ORDER BY d.ts ASC
+		LIMIT 1
+		INTO neighbor;
+		IF FOUND AND neighbor.ts > date_trunc('hour', OLD.ts) THEN
+			INSERT INTO solaragg.agg_stale_loc_datum (ts_start, loc_id, source_id, agg_kind)
+			VALUES (date_trunc('hour', neighbor.ts), neighbor.loc_id, neighbor.source_id, 'h')
+			ON CONFLICT (agg_kind, loc_id, ts_start, source_id) DO NOTHING;
+		END IF;
+	END IF;
+
+	CASE TG_OP
+		WHEN 'INSERT', 'UPDATE' THEN
+			RETURN NEW;
+		ELSE
+			RETURN OLD;
+	END CASE;
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE;
