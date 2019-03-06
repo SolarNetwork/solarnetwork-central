ff39c997a45bc3a6b53dfa50d78d9fd2f60bbe3d
Matt Magoffin <matt@solarnetwork.net>
2019-03-06 18:13:07 +1300

    Add missing view required by solarauth.

--- solarnet-db-setup/postgres/postgres-init-users.sql
+++ solarnet-db-setup/postgres/postgres-init-users.sql
@@ -172,7 +172,7 @@ CREATE OR REPLACE VIEW solaruser.user_auth_token_login AS
 /**
  * View of granted roles for tokens.
  */
-CREATE VIEW solaruser.user_auth_token_role AS
+CREATE OR REPLACE VIEW solaruser.user_auth_token_role AS
 	SELECT
 		t.auth_token AS username,
 		'ROLE_'::text || upper(t.token_type::character varying::text) AS authority
@@ -183,6 +183,33 @@ CREATE VIEW solaruser.user_auth_token_role AS
 		r.role_name AS authority
 	FROM solaruser.user_auth_token t
 	JOIN solaruser.user_role r ON r.user_id = t.user_id AND t.token_type = 'User'::solaruser.user_auth_token_type;
+
+/**
+ * View of all valid node IDs, as an array, for a given token.
+ *
+ * This will filter out any node IDs not present on the token policy `nodeIds` array.
+ * Additionally, archived nodes are filtered out.
+ *
+ * Typical query is:
+ *
+ *     SELECT node_id FROM solaruser.user_auth_token_node_ids
+ *     WHERE auth_token = 'token-id'
+ */
+CREATE OR REPLACE VIEW solaruser.user_auth_token_node_ids AS
+	SELECT t.auth_token,
+		t.user_id,
+		t.token_type,
+		t.jpolicy,
+		array_agg(un.node_id) AS node_ids
+	FROM solaruser.user_auth_token t
+	JOIN solaruser.user_node un ON un.user_id = t.user_id
+	WHERE un.archived = FALSE 
+		AND t.status = 'Active'::solaruser.user_auth_token_status
+		AND (
+			(t.jpolicy->'nodeIds') IS NULL 
+			OR (t.jpolicy->'nodeIds') @> un.node_id::text::jsonb
+		)
+	GROUP BY t.auth_token, t.user_id;
 
 /**
  * View of all valid node IDs for a given token.
