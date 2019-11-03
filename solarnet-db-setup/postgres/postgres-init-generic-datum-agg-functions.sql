c91aeecbb03129375eb07c9df231130f6412d14e
Matt Magoffin <matt@solarnetwork.net>
2019-11-04 07:21:43 +1300

    NET-219: support most-recent queries for aggregate data using node and source arrays, not just single node with source array.

--- solarnet-db-setup/postgres/postgres-init-generic-datum-agg-functions.sql
+++ solarnet-db-setup/postgres/postgres-init-generic-datum-agg-functions.sql
@@ -1138,104 +1138,68 @@ BEGIN
 END;
 $BODY$;
 
-CREATE OR REPLACE FUNCTION solaragg.find_most_recent_hourly(
-	node bigint,
-	sources text[] DEFAULT NULL)
-  RETURNS SETOF solaragg.agg_datum_hourly_data AS
-$BODY$
-BEGIN
-	IF sources IS NULL OR array_length(sources, 1) < 1 THEN
-		RETURN QUERY
-		WITH maxes AS (
-			SELECT max(d.ts_start) as ts_start, d.source_id, node as node_id FROM solaragg.agg_datum_hourly d
-			INNER JOIN (SELECT solardatum.find_available_sources(node) AS source_id) AS s ON s.source_id = d.source_id
-			WHERE d. node_id = node
-			GROUP BY d.source_id
-		)
-		SELECT d.* FROM solaragg.agg_datum_hourly_data d
-		INNER JOIN maxes ON maxes.node_id = d.node_id AND maxes.source_id = d.source_id AND maxes.ts_start = d.ts_start
-		ORDER BY d.source_id ASC;
-	ELSE
-		RETURN QUERY
-		WITH maxes AS (
-			SELECT max(d.ts_start) as ts_start, d.source_id, node as node_id FROM solaragg.agg_datum_hourly d
-			INNER JOIN (SELECT unnest(sources) AS source_id) AS s ON s.source_id = d.source_id
-			WHERE d. node_id = node
-			GROUP BY d.source_id
-		)
-		SELECT d.* FROM solaragg.agg_datum_hourly_data d
-		INNER JOIN maxes ON maxes.node_id = d.node_id AND maxes.source_id = d.source_id AND maxes.ts_start = d.ts_start
-		ORDER BY d.source_id ASC;
-	END IF;
-END;$BODY$
-  LANGUAGE plpgsql STABLE
-  ROWS 20;
+/**
+ * FUNCTION solardatum.find_most_recent_hourly(bigint[], text[])
+ * 
+ * Find the highest available hourly data for all source IDs for the given node IDs. This query
+ * relies on the `solardatum.da_datum_range` table.
+ *
+ * @param nodes the node IDs to find
+ * @param sources the source IDs to find
+ */
+CREATE OR REPLACE FUNCTION solaragg.find_most_recent_hourly(nodes bigint[], sources text[])
+RETURNS SETOF solaragg.agg_datum_hourly_data  LANGUAGE sql STABLE ROWS 100 AS
+$$
+	SELECT d.* 
+	FROM solardatum.da_datum_range mr
+	INNER JOIN solaragg.agg_datum_hourly_data d ON d.node_id = mr.node_id AND d.source_id = mr.source_id AND d.ts_start = date_trunc('hour', mr.ts_max)
+	WHERE mr.node_id = ANY(nodes) AND mr.source_id = ANY(sources)
+	ORDER BY d.node_id, d.source_id
+$$;
 
-CREATE OR REPLACE FUNCTION solaragg.find_most_recent_daily(
-	node bigint,
-	sources text[] DEFAULT NULL)
-  RETURNS SETOF solaragg.agg_datum_daily_data AS
-$BODY$
-BEGIN
-	IF sources IS NULL OR array_length(sources, 1) < 1 THEN
-		RETURN QUERY
-		WITH maxes AS (
-			SELECT max(d.ts_start) as ts_start, d.source_id, node as node_id FROM solaragg.agg_datum_daily d
-			INNER JOIN (SELECT solardatum.find_available_sources(node) AS source_id) AS s ON s.source_id = d.source_id
-			WHERE d. node_id = node
-			GROUP BY d.source_id
-		)
-		SELECT d.* FROM solaragg.agg_datum_daily_data d
-		INNER JOIN maxes ON maxes.node_id = d.node_id AND maxes.source_id = d.source_id AND maxes.ts_start = d.ts_start
-		ORDER BY d.source_id ASC;
-	ELSE
-		RETURN QUERY
-		WITH maxes AS (
-			SELECT max(d.ts_start) as ts_start, d.source_id, node as node_id FROM solaragg.agg_datum_daily d
-			INNER JOIN (SELECT unnest(sources) AS source_id) AS s ON s.source_id = d.source_id
-			WHERE d. node_id = node
-			GROUP BY d.source_id
-		)
-		SELECT d.* FROM solaragg.agg_datum_daily_data d
-		INNER JOIN maxes ON maxes.node_id = d.node_id AND maxes.source_id = d.source_id AND maxes.ts_start = d.ts_start
-		ORDER BY d.source_id ASC;
-	END IF;
-END;$BODY$
-  LANGUAGE plpgsql STABLE
-  ROWS 20;
 
-CREATE OR REPLACE FUNCTION solaragg.find_most_recent_monthly(
-	node bigint,
-	sources text[] DEFAULT NULL)
-  RETURNS SETOF solaragg.agg_datum_monthly_data AS
-$BODY$
-BEGIN
-	IF sources IS NULL OR array_length(sources, 1) < 1 THEN
-		RETURN QUERY
-		WITH maxes AS (
-			SELECT max(d.ts_start) as ts_start, d.source_id, node as node_id FROM solaragg.agg_datum_monthly d
-			INNER JOIN (SELECT solardatum.find_available_sources(node) AS source_id) AS s ON s.source_id = d.source_id
-			WHERE d. node_id = node
-			GROUP BY d.source_id
-		)
-		SELECT d.* FROM solaragg.agg_datum_monthly_data d
-		INNER JOIN maxes ON maxes.node_id = d.node_id AND maxes.source_id = d.source_id AND maxes.ts_start = d.ts_start
-		ORDER BY d.source_id ASC;
-	ELSE
-		RETURN QUERY
-		WITH maxes AS (
-			SELECT max(d.ts_start) as ts_start, d.source_id, node as node_id FROM solaragg.agg_datum_monthly d
-			INNER JOIN (SELECT unnest(sources) AS source_id) AS s ON s.source_id = d.source_id
-			WHERE d. node_id = node
-			GROUP BY d.source_id
-		)
-		SELECT d.* FROM solaragg.agg_datum_monthly_data d
-		INNER JOIN maxes ON maxes.node_id = d.node_id AND maxes.source_id = d.source_id AND maxes.ts_start = d.ts_start
-		ORDER BY d.source_id ASC;
-	END IF;
-END;$BODY$
-  LANGUAGE plpgsql STABLE
-  ROWS 20;
+/**
+ * FUNCTION solardatum.find_most_recent_daily(bigint[], text[])
+ * 
+ * Find the highest available daily data for all source IDs for the given node IDs. This query
+ * relies on the `solardatum.da_datum_range` table.
+ *
+ * @param nodes the node IDs to find
+ * @param sources the source IDs to find
+ */
+CREATE OR REPLACE FUNCTION solaragg.find_most_recent_daily(nodes bigint[], sources text[])
+RETURNS SETOF solaragg.agg_datum_daily_data  LANGUAGE sql STABLE ROWS 100 AS
+$$
+	SELECT d.* 
+	FROM solardatum.da_datum_range mr
+	INNER JOIN solarnet.node_local_time nlt ON nlt.node_id = mr.node_id
+	INNER JOIN solaragg.agg_datum_daily_data d ON d.node_id = mr.node_id AND d.source_id = mr.source_id 
+		AND d.ts_start = date_trunc('day', mr.ts_max AT TIME ZONE nlt.time_zone) AT TIME ZONE nlt.time_zone
+	WHERE mr.node_id = ANY(nodes) AND mr.source_id = ANY(sources)
+	ORDER BY d.node_id, d.source_id
+$$;
+
+
+/**
+ * FUNCTION solardatum.find_most_recent_monthly(bigint[], text[])
+ * 
+ * Find the highest available monthly data for all source IDs for the given node IDs. This query
+ * relies on the `solardatum.da_datum_range` table.
+ *
+ * @param nodes the node IDs to find
+ * @param sources the source IDs to find
+ */
+CREATE OR REPLACE FUNCTION solaragg.find_most_recent_monthly(nodes bigint[], sources text[])
+RETURNS SETOF solaragg.agg_datum_daily_data  LANGUAGE sql STABLE ROWS 100 AS
+$$
+	SELECT d.* 
+	FROM solardatum.da_datum_range mr
+	INNER JOIN solarnet.node_local_time nlt ON nlt.node_id = mr.node_id
+	INNER JOIN solaragg.agg_datum_monthly_data d ON d.node_id = mr.node_id AND d.source_id = mr.source_id 
+		AND d.ts_start = date_trunc('month', mr.ts_max AT TIME ZONE nlt.time_zone) AT TIME ZONE nlt.time_zone
+	WHERE mr.node_id = ANY(nodes) AND mr.source_id = ANY(sources)
+	ORDER BY d.node_id, d.source_id
+$$;
 
 
 /**
