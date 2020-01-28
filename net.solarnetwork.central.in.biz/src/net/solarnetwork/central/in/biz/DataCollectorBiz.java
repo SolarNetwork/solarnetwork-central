a2e6c0d600485c2524092f4ebbb4828716d7f784
Matt Magoffin <matt@solarnetwork.net>
2020-01-28 14:33:13 +1300

    NET-191: drop more price/weather/hardware legacy datum objects.

--- net.solarnetwork.central.in.biz/src/net/solarnetwork/central/in/biz/DataCollectorBiz.java
+++ net.solarnetwork.central.in.biz/src/net/solarnetwork/central/in/biz/DataCollectorBiz.java
@@ -25,7 +25,6 @@
 package net.solarnetwork.central.in.biz;
 
 import java.util.List;
-import net.solarnetwork.central.datum.domain.Datum;
 import net.solarnetwork.central.datum.domain.GeneralLocationDatum;
 import net.solarnetwork.central.datum.domain.GeneralLocationDatumMetadataFilter;
 import net.solarnetwork.central.datum.domain.GeneralLocationDatumMetadataFilterMatch;
@@ -38,8 +37,6 @@ import net.solarnetwork.central.domain.LocationMatch;
 import net.solarnetwork.central.domain.SolarNodeMetadataFilter;
 import net.solarnetwork.central.domain.SolarNodeMetadataFilterMatch;
 import net.solarnetwork.central.domain.SortDescriptor;
-import net.solarnetwork.central.domain.SourceLocation;
-import net.solarnetwork.central.domain.SourceLocationMatch;
 import net.solarnetwork.domain.GeneralDatumMetadata;
 
 /**
@@ -49,35 +46,10 @@ import net.solarnetwork.domain.GeneralDatumMetadata;
  * Serves as a transactional facade to posting data into central system.
  * </p>
  * 
- * @author matt.magoffin
- * @version 1.5
+ * @author matt
+ * @version 2.0
  */
 public interface DataCollectorBiz {
-
-	/**
-	 * Post a new {@link Datum}.
-	 * 
-	 * @param <D>
-	 *        the Datum type
-	 * @param datum
-	 *        the data to persist
-	 * @return the persisted entity
-	 * @deprecated See {@link #postGeneralNodeDatum(Iterable)}
-	 */
-	@Deprecated
-	<D extends Datum> D postDatum(D datum);
-
-	/**
-	 * Post a collection of datum of {@link Datum} in a single transaction.
-	 * 
-	 * @param datums
-	 *        the collection of datum
-	 * @return the persisted entities, ordered in iterator order of
-	 *         {@code datums}
-	 * @deprecated See {@link #postGeneralNodeDatum(Iterable)}
-	 */
-	@Deprecated
-	List<Datum> postDatum(Iterable<Datum> datums);
 
 	/**
 	 * Post a collection of {@link GeneralNodeDatum} in a single transaction.
@@ -181,59 +153,6 @@ public interface DataCollectorBiz {
 	FilterResults<GeneralLocationDatumMetadataFilterMatch> findGeneralLocationDatumMetadata(
 			GeneralLocationDatumMetadataFilter metadataCriteria, List<SortDescriptor> sortDescriptors,
 			Integer offset, Integer max);
-
-	/**
-	 * Look up PriceLocation objects based on a source name and location name.
-	 * 
-	 * @param criteria
-	 *        the the search criteria
-	 * @return the matching location, or <em>null</em> if not found
-	 */
-	List<SourceLocationMatch> findPriceLocations(SourceLocation criteria);
-
-	/**
-	 * Look up price location objects based on a location search filter and sort
-	 * options.
-	 * 
-	 * @param sortDescriptors
-	 *        the optional sort descriptors
-	 * @param offset
-	 *        an optional result offset
-	 * @param max
-	 *        an optional maximum number of returned results
-	 * @return the results, never <em>null</em>
-	 */
-	FilterResults<SourceLocationMatch> findPriceLocations(SourceLocation criteria,
-			List<SortDescriptor> sortDescriptors, Integer offset, Integer max);
-
-	/**
-	 * Look up WeatherLocation objects based on a source name and location.
-	 * 
-	 * <p>
-	 * At a minimum the {@link SourceLocation#getSource()} must be provided in
-	 * the supplied criteria.
-	 * </p>
-	 * 
-	 * @param criteria
-	 *        the search criteria
-	 * @return the matching locations, or an empty list if none found
-	 */
-	List<SourceLocationMatch> findWeatherLocations(SourceLocation criteria);
-
-	/**
-	 * Look up weather location objects based on a location search filter and
-	 * sort options.
-	 * 
-	 * @param sortDescriptors
-	 *        the optional sort descriptors
-	 * @param offset
-	 *        an optional result offset
-	 * @param max
-	 *        an optional maximum number of returned results
-	 * @return the results, never <em>null</em>
-	 */
-	FilterResults<SourceLocationMatch> findWeatherLocations(SourceLocation criteria,
-			List<SortDescriptor> sortDescriptors, Integer offset, Integer max);
 
 	/**
 	 * Look up location objects based on a location search filter.
