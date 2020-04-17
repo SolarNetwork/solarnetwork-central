d8cfa02956228e4a6343e74d1b5e743f77f5c82f
Matt Magoffin <matt@solarnetwork.net>
2020-04-17 17:30:35 +1200

    NET-248: add "partialAggregate" property to AggregationFilter API, to specify the partial aggregate to use in a query.

--- net.solarnetwork.central.common/src/net/solarnetwork/central/domain/AggregationFilter.java
+++ net.solarnetwork.central.common/src/net/solarnetwork/central/domain/AggregationFilter.java
@@ -30,16 +30,53 @@ import org.joda.time.DateTime;
  * "aggregate" filter results.
  * 
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public interface AggregationFilter extends Filter {
 
 	/**
 	 * Get an aggregation to apply to the query.
 	 * 
-	 * @return the aggregation, or <em>null</em> for default
+	 * @return the aggregation, or {@literal null} for default
 	 */
 	Aggregation getAggregation();
+
+	/**
+	 * Get an aggregation to apply to partial time ranges if the start or end
+	 * dates do not align to exact boundaries defined by the {@code aggregation}
+	 * value.
+	 * 
+	 * <p>
+	 * This filter can be used to request incomplete aggregate results for when
+	 * the provided start or end dates do not align to exact
+	 * {@link #getAggregation()} boundaries. For example if a
+	 * {@link Aggregation#Month} aggregation is requested with a start date of
+	 * {@literal 2020-01-15} and an end date of {@literal 2020-03-15}, this
+	 * property could be set to {@link Aggregation#Day} to request 3 aggregate
+	 * month results that are comprised of these date ranges:
+	 * </p>
+	 * 
+	 * <ol>
+	 * <li>15 Jan - 31 Jan</li>
+	 * <li>1 Feb - 29 Feb</li>
+	 * <li>1 Mar - 14 Mar</li>
+	 * </ol>
+	 * 
+	 * <p>
+	 * If this property is <b>not</b> provided in that same example, then the
+	 * results would be comprised of these date ranges:
+	 * </p>
+	 * 
+	 * <ol>
+	 * <li>1 Jan - 31 Jan</li>
+	 * <li>1 Feb - 29 Feb</li>
+	 * <li>1 Mar - 31 Mar</li>
+	 * </ol>
+	 * 
+	 * @return the partial aggregation, or {@literal null} for none
+	 * @since 1.1
+	 */
+	Aggregation getPartialAggregation();
 
 	/**
 	 * Get a start date.
