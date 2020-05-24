1e3d817030df3660226faf85e46cd6ccd6f9ff7a
Matt Magoffin <matt@solarnetwork.net>
2020-05-25 11:33:14 +1200

    NET-253: add job to call the solaragg.populate_audit_datum_daily_missing() stored procedure so that nodes that aren't posting data still have their storage audit values populated.

---
+++ net.solarnetwork.central.datum.agg/src/net/solarnetwork/central/datum/agg/JdbcCallJob.java
@@ -0,0 +1,116 @@
+/* ==================================================================
+ * JdbcCallJob.java - 25/05/2020 9:26:34 am
+ * 
+ * Copyright 2020 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.datum.agg;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.CallableStatementCallback;
+import org.springframework.jdbc.core.CallableStatementCreator;
+import org.springframework.jdbc.core.JdbcOperations;
+import net.solarnetwork.central.scheduler.JobSupport;
+
+/**
+ * Job to execute a stored procedure that returns a BIGINT result.
+ * 
+ * @author matt
+ * @version 1.0
+ * @since 1.10
+ */
+public class JdbcCallJob extends JobSupport {
+
+	private final JdbcOperations jdbcOps;
+	private String jdbcCall;
+
+	/**
+	 * Construct with properties.
+	 * 
+	 * @param eventAdmin
+	 *        the EventAdmin
+	 * @param jdbcOps
+	 *        the JdbcOperations to use
+	 */
+	public JdbcCallJob(EventAdmin eventAdmin, JdbcOperations jdbcOps) {
+		super(eventAdmin);
+		this.jdbcOps = jdbcOps;
+		setJobGroup("Datum");
+		setMaximumWaitMs(1800000L);
+	}
+
+	@Override
+	protected boolean handleJob(Event job) throws Exception {
+		log.info("Job {} executing JDBC call {}...", getJobId(), getJdbcCall());
+		final Long result = jdbcOps.execute(new CallableStatementCreator() {
+
+			@Override
+			public CallableStatement createCallableStatement(Connection con) throws SQLException {
+				CallableStatement call = con.prepareCall(getJdbcCall());
+				call.registerOutParameter(1, Types.BIGINT);
+				return call;
+			}
+		}, new CallableStatementCallback<Long>() {
+
+			@Override
+			public Long doInCallableStatement(CallableStatement cs)
+					throws SQLException, DataAccessException {
+				cs.execute();
+				return cs.getLong(1);
+			}
+		});
+		log.info("Job {} JDBC call result: {}", getJobId(), result);
+		return true;
+	}
+
+	/**
+	 * Get the JDBC operations.
+	 * 
+	 * @return the operations
+	 */
+	public JdbcOperations getJdbcOps() {
+		return jdbcOps;
+	}
+
+	/**
+	 * Get the JDBC call.
+	 * 
+	 * @return the call
+	 */
+	public String getJdbcCall() {
+		return jdbcCall;
+	}
+
+	/**
+	 * Set the JDBC call.
+	 * 
+	 * @param jdbcCall
+	 *        the call
+	 */
+	public void setJdbcCall(String jdbcCall) {
+		this.jdbcCall = jdbcCall;
+	}
+
+}
--- solarnet-db-setup/postgres/updates/NET-253-acc-audit-gen-bug.sql
+++ solarnet-db-setup/postgres/updates/NET-253-acc-audit-gen-bug.sql
@@ -9,7 +9,8 @@ CREATE OR REPLACE FUNCTION solaragg.find_audit_datum_daily_missing(ts date DEFAU
 	RETURNS TABLE(
 		node_id bigint,
 		source_id character varying(64),
-		ts_start timestamp with time zone
+		ts_start timestamp with time zone,
+		time_zone character varying(64)
 	)
 	LANGUAGE sql STABLE AS
 $$
@@ -25,15 +26,18 @@ $$
 			AND ts_start < ts::timestamptz + interval '24 hours'
 			AND ts_start AT TIME ZONE nlt.time_zone = ts
 	)
-	SELECT m.node_id, m.source_id, ts::timestamp AT TIME ZONE nlt.time_zone
+	SELECT m.node_id, m.source_id, ts::timestamp AT TIME ZONE nlt.time_zone, nlt.time_zone
 	FROM missing m
 	INNER JOIN solarnet.node_local_time nlt ON nlt.node_id = m.node_id
 $$;
 
 /**
  * Call the `solaragg.find_audit_datum_daily_missing(date)` function and insert the results
- * into the `solaragg.aud_datum_daily_stale` table so a record of the found node sources gets
- * generated.
+ * into the `solaragg.aud_datum_daily_stale` table with an `aud_kind = 'm'` so a record of the found
+ * node sources gets generated.
+ *
+ * The `aud_kind = m` value is used because the processor that handles that record also populates
+ * the `solaragg.aud_acc_datum_daily` table.
  *
  * @param ts the date to look for; defaults to the current date
  * @return the number of rows inserted
@@ -45,7 +49,11 @@ DECLARE
 	ins_count bigint := 0;
 BEGIN
 	INSERT INTO solaragg.aud_datum_daily_stale (ts_start, node_id, source_id, aud_kind)
-	SELECT ts_start, node_id, source_id, 'r' AS aud_kind
+	SELECT 
+		date_trunc('month', ts_start at time zone time_zone) at time zone time_zone
+		, node_id
+		, source_id
+		, 'm' AS aud_kind
 	FROM solaragg.find_audit_datum_daily_missing(ts)
 	ON CONFLICT DO NOTHING;
 
