ec2f5934b4dc7150b1f032c150f05eeaf9610bdf
Matt Magoffin <matt@solarnetwork.net>
2021-11-14 17:17:01 +1300

    Allow both HTTP/HTTPS listeners to be configured.

---
+++ solarnet/solarin/src/main/java/net/solarnetwork/central/in/config/TomcatConfig.java
@@ -0,0 +1,55 @@
+/* ==================================================================
+ * TomcatConfig.java - 13/11/2021 4:08:04 PM
+ * 
+ * Copyright 2021 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.in.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for Tomcat when running dual HTTP/HTTPS.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+@Configuration
+@ConditionalOnProperty("server.http.port")
+public class TomcatConfig {
+
+	@Value("${server.http.port}")
+	private int httpPort;
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setPort(httpPort);
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(connector);
+		return tomcat;
+	}
+}
