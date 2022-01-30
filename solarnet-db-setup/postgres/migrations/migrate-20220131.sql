de663c318c151eab92264867ed25cc9cc2455fc6
Matt Magoffin <matt@solarnetwork.net>
2022-01-31 10:27:12 +1300

    Add note.

--- solarnet-db-setup/postgres/migrations/migrate-20220131.sql
+++ solarnet-db-setup/postgres/migrations/migrate-20220131.sql
@@ -1,3 +1,6 @@
--- Run this script from the parent directory, e.g. psql -f migrations/migrate-20210827.sql
+-- Run this script from the parent directory, e.g. psql -f migrations/migrate-20220131.sql
 
-\i updates/NET-286-reading-agg-fix.sql
+-- NOTE: the NET-289-aggs-for-vecs-admin.sql must be run as a DB superuser to install the
+-- aggs_for_vecs extension
+
+\i NET-289-aggs-for-vecs.sql
