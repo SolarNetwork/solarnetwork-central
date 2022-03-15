6e98441154c0184b1db1f8d450ad5e3d21e8580c
Matt Magoffin <matt@solarnetwork.net>
2022-03-15 15:00:37 +1300

    Fix errors in SQL migration scripts.

---
+++ solarnet-db-setup/postgres/migrations/migrate-20220130.sql
@@ -0,0 +1,3 @@
+-- Run this script from the parent directory, e.g. psql -f migrations/migrate-20220130.sql
+
+\i updates/NET-288-add-setting-table.sql
--- solarnet-db-setup/postgres/migrations/migrate-20220224.sql
+++ solarnet-db-setup/postgres/migrations/migrate-20220224.sql
@@ -1,3 +1,3 @@
 -- Run this script from the parent directory, e.g. psql -f migrations/migrate-20220224.sql
 
-\i NET-292-purge-incomplete-instructions.sql
+\i updates/NET-292-purge-incomplete-instructions.sql
