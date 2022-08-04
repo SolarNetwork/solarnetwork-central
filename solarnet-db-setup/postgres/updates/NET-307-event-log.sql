991e67c360567cda2e39b70378f6905b156886b2
Matt Magoffin <matt@solarnetwork.net>
2022-08-05 06:12:11 +1200

    NET-307: remove user event log conf table... rely on system-wide expiration.

--- solarnet-db-setup/postgres/updates/NET-307-event-log.sql
+++ solarnet-db-setup/postgres/updates/NET-307-event-log.sql
@@ -21,25 +21,6 @@ CREATE UNIQUE INDEX user_event_log_pk ON solaruser.user_event_log (user_id, even
 CREATE INDEX user_event_log_tags_idx ON solaruser.user_event_log USING GIN (tags);
 
 /**
- * TABLE solaruser.user_event_log_conf
- *
- * Table for user event log configuration, such as expiration policy.
- */
-CREATE TABLE solaruser.user_event_log_conf (
-	id			BIGINT NOT NULL DEFAULT nextval('solaruser.user_expire_seq'),
-	created		TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	user_id     BIGINT NOT NULL,
-	expire_days	INTEGER NOT NULL,
-	CONSTRAINT user_event_log_conf_pk PRIMARY KEY (id),
-	CONSTRAINT user_event_log_conf_user_fk FOREIGN KEY (user_id)
-		REFERENCES solaruser.user_user (id) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE CASCADE
-);
-
-/* Add index on user_id so we can show all entities for user. */
-CREATE INDEX user_event_log_conf_user_idx ON solaruser.user_event_log_conf (user_id);
-
-/**
  * Extract the timestamp from a v7 UUID.
  *
  * See https://www.ietf.org/archive/id/draft-peabody-dispatch-new-uuid-format-04.html#section-5.2
