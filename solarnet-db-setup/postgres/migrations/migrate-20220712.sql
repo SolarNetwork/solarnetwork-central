225f0aff4184026de08b88eb45d9b8ed45a52b96
Matt Magoffin <matt@solarnetwork.net>
2023-02-22 11:33:00 +1300

    Fix typo in comment.

--- solarnet-db-setup/postgres/migrations/migrate-20220712.sql
+++ solarnet-db-setup/postgres/migrations/migrate-20220712.sql
@@ -1,3 +1,3 @@
--- Run this script from the parent directory, e.g. psql -f migrations/migrate-2022012.sql
+-- Run this script from the parent directory, e.g. psql -f migrations/migrate-20220712.sql
 
 \i updates/NET-303-datum-diff-agg-fix.sql
