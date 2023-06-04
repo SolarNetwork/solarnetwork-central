8aaa970996ca1e4fb231caefe9fd16b43aa4837e
Matt Magoffin <matt@solarnetwork.net>
2023-06-05 09:10:54 +1200

    Split OCPP server from SolarIn into own SolarOCPP app, to allow scaling OCPP connections independently.

---
+++ solarnet/solarocpp/src/main/java/net/solarnetwork/central/in/ocpp/config/MyBatisDaoConfig.java
@@ -0,0 +1,55 @@
+/* ==================================================================
+ * MyBatisDaoConfig.java - 4/10/2021 4:44:19 PM
+ * 
+ * Copyright 2021 SolarNetwork.net Dev Team
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.in.ocpp.config;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import net.solarnetwork.central.dao.mybatis.support.MyBatisExceptionTranslator;
+
+/**
+ * MyBatis configuration for SolarOCPP.
+ * 
+ * @author matt
+ * @version 1.0
+ */
+@Configuration
+public class MyBatisDaoConfig {
+
+	@Autowired
+	public SqlSessionFactory sqlSessionFactory;
+
+	@Autowired
+	public DataSource dataSource;
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate() {
+		return new SqlSessionTemplate(sqlSessionFactory, ExecutorType.SIMPLE,
+				new MyBatisExceptionTranslator(dataSource, true));
+	}
+
+}
