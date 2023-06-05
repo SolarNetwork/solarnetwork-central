c35de1904eb2b0dd148e62202f281d27b595238f
Matt Magoffin <matt@solarnetwork.net>
2023-06-06 10:25:59 +1200

    Add Spring Actuator to facilitate dynamic logging configuration.

--- solarnet/solarocpp/src/main/java/net/solarnetwork/central/in/ocpp/config/WebSecurityConfig.java
+++ solarnet/solarocpp/src/main/java/net/solarnetwork/central/in/ocpp/config/WebSecurityConfig.java
@@ -23,22 +23,33 @@
 package net.solarnetwork.central.in.ocpp.config;
 
 import java.util.Arrays;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.firewall.RequestRejectedHandler;
-import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import net.solarnetwork.central.security.jdbc.JdbcUserDetailsService;
 import net.solarnetwork.central.security.web.HandlerExceptionResolverRequestRejectedHandler;
 
 /**
@@ -50,6 +61,15 @@ import net.solarnetwork.central.security.web.HandlerExceptionResolverRequestReje
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+	/** The system operator authority. */
+	public static final String OPS_AUTHORITY = "ROLE_OPS";
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private HandlerExceptionResolver handlerExceptionResolver;
@@ -77,54 +97,110 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		// @formatter:off
-	    http
-	      // limit this configuration to specific paths
-	      .securityMatchers()
-	        .requestMatchers("/solarocpp/**")
-	        .and()
-
-	        // CSRF not needed for stateless calls
-	      .csrf().disable()
-	      
-	      // make sure CORS honored
-	      .cors().and()
-	      
-	      // can simply return 403 on auth failures
-	      .exceptionHandling()
-	      	//.authenticationEntryPoint(unauthorizedEntryPoint())
-	      	//.accessDeniedPage("/error/403")
-	      	.and()
-	      
-	      // no sessions
-	      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-	      
-	      .authorizeHttpRequests()
-	      	.requestMatchers(HttpMethod.OPTIONS, "/solarocpp/**").permitAll()
-	        .requestMatchers(HttpMethod.GET, 
-	        		"/solarocpp/",
-	        		"/solarocpp/error",
-	        		"/solarocpp/*.html",
-	        		"/solarocpp/css/**",
-	        		"/solarocpp/img/**",
-	        		"/solarocpp/ping").permitAll()
-	        
-	        .anyRequest().denyAll()
-	    ;
-	    // @formatter:on
-		return http.build();
+	public UserDetailsService userDetailsService() {
+		JdbcUserDetailsService service = new JdbcUserDetailsService();
+		service.setDataSource(dataSource);
+		service.setUsersByUsernameQuery(JdbcUserDetailsService.DEFAULT_USERS_BY_USERNAME_SQL);
+		service.setAuthoritiesByUsernameQuery(
+				JdbcUserDetailsService.DEFAULT_AUTHORITIES_BY_USERNAME_SQL);
+		return service;
 	}
 
 	@Bean
-	public StrictHttpFirewall httpFirewall() {
-		StrictHttpFirewall firewall = new StrictHttpFirewall();
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService());
+		provider.setPasswordEncoder(passwordEncoder);
+		return provider;
+	}
 
-		// this following is disabled to allow for the SSL_CLIENT_CERT header value
-		// which is a full PEM encoded certificate with newline characters
-		firewall.setAllowedHeaderValues((header) -> true);
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		return new ProviderManager(authenticationProvider());
+	}
 
-		return firewall;
+	@Bean
+	public AuthenticationEventPublisher authenticationEventPublisher(
+			ApplicationEventPublisher appEventPublisher) {
+		return new DefaultAuthenticationEventPublisher(appEventPublisher);
+	}
+
+	/**
+	 * Security rules for the management API.
+	 */
+	@Configuration
+	@Order(1)
+	public static class ManagementWebSecurityConfig {
+
+		@Order(1)
+		@Bean
+		public SecurityFilterChain filterChainManagement(HttpSecurity http) throws Exception {
+			// @formatter:off
+		    http
+		      // limit this configuration to specific paths
+		      .securityMatchers()
+		        .requestMatchers("/ops/**")
+		        .and()
+	
+		        // CSRF not needed for stateless calls
+		      .csrf().disable()
+		      
+		      // make sure CORS honored
+		      .cors().and()
+		      
+		      // no sessions
+		      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		      
+		      .httpBasic().realmName("SN Operations").and()
+		      
+		      .authorizeHttpRequests()
+		        .anyRequest().hasAnyAuthority(OPS_AUTHORITY)
+		        
+		    ;
+		    // @formatter:on
+			return http.build();
+		}
+	}
+
+	/**
+	 * Last set of security rules, for public resources else deny all others.
+	 */
+	@Configuration
+	@Order(2)
+	public static class PublicWebSecurityConfig {
+
+		@Order(2)
+		@Bean
+		public SecurityFilterChain filterChainPublic(HttpSecurity http) throws Exception {
+			// @formatter:off
+		    http
+		      // limit this configuration to specific paths
+		      .securityMatchers()
+		        .requestMatchers("/solarocpp/**")
+		        .and()
+	
+		        // CSRF not needed for stateless calls
+		      .csrf().disable()
+		      
+		      // make sure CORS honored
+		      .cors().and()
+		      
+		      // no sessions
+		      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		      
+		      .authorizeHttpRequests()
+		      	.requestMatchers(HttpMethod.OPTIONS, "/solarocpp/**").permitAll()
+		        .requestMatchers(HttpMethod.GET, 
+		        		"/solarocpp/",
+		        		"/solarocpp/error",
+		        		"/solarocpp/*.html",
+		        		"/solarocpp/css/**",
+		        		"/solarocpp/img/**",
+		        		"/solarocpp/ping").permitAll()		    
+		    ;
+		    // @formatter:on
+			return http.build();
+		}
 	}
 
 }
