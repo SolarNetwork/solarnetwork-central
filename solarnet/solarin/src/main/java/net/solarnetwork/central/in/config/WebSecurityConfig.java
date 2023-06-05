848d79d79fadda131768172a909b0d2ec0fd531f
Matt Magoffin <matt@solarnetwork.net>
2023-06-06 11:35:02 +1200

    Add Spring Actuator to facilitate dynamic logging configuration.

--- solarnet/solarin/src/main/java/net/solarnetwork/central/in/config/WebSecurityConfig.java
+++ solarnet/solarin/src/main/java/net/solarnetwork/central/in/config/WebSecurityConfig.java
@@ -23,14 +23,24 @@
 package net.solarnetwork.central.in.config;
 
 import java.util.Arrays;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
@@ -42,6 +52,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import net.solarnetwork.central.security.NodeUserDetailsService;
 import net.solarnetwork.central.security.Role;
+import net.solarnetwork.central.security.jdbc.JdbcUserDetailsService;
 import net.solarnetwork.central.security.web.HandlerExceptionResolverRequestRejectedHandler;
 
 /**
@@ -54,7 +65,15 @@ import net.solarnetwork.central.security.web.HandlerExceptionResolverRequestReje
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	private static String[] NODE_AUTHORITIES = new String[] { Role.ROLE_NODE.toString() };
+	private static final String OPS_AUTHORITY = Role.ROLE_OPS.toString();
+
+	private static final String NODE_AUTHORITY = Role.ROLE_NODE.toString();
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private HandlerExceptionResolver handlerExceptionResolver;
@@ -62,11 +81,6 @@ public class WebSecurityConfig {
 	@Bean
 	public RequestRejectedHandler requestRejectedHandler() {
 		return new HandlerExceptionResolverRequestRejectedHandler(handlerExceptionResolver);
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return new NodeUserDetailsService();
 	}
 
 	@Bean
@@ -87,63 +101,6 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		// @formatter:off
-	    http
-	      // limit this configuration to specific paths
-	      .securityMatchers()
-	        .requestMatchers("/solarin/**")
-	        .and()
-
-	        // CSRF not needed for stateless calls
-	      .csrf().disable()
-	      
-	      // make sure CORS honored
-	      .cors().and()
-	      
-	      // can simply return 403 on auth failures
-	      .exceptionHandling()
-	      	//.authenticationEntryPoint(unauthorizedEntryPoint())
-	      	//.accessDeniedPage("/error/403")
-	      	.and()
-	      
-	      // no sessions
-	      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-	      
-	      //.requiresChannel()
-	      //  .antMatchers(HttpMethod.GET, "/**/*Collector.do").requiresSecure()
-	      //  .antMatchers(HttpMethod.GET, "/api/v1/sec/**").requiresSecure()
-	      //  .and()
-	        
-	      .x509()
-	        .userDetailsService(userDetailsService())
-	        .subjectPrincipalRegex("UID=(.*?),")
-	        .and()
-	        
-	      .authorizeHttpRequests()
-	      	.requestMatchers(HttpMethod.OPTIONS, "/solarin/**").permitAll()
-	        .requestMatchers(HttpMethod.GET, 
-	        		"/solarin/",
-	        		"/solarin/error",
-	        		"/solarin/*.html",
-	        		"/solarin/css/**",
-	        		"/solarin/img/**",
-	        		"/solarin/ping",
-	        		"/solarin/api/v1/pub/**",
-	        		"/solarin/identity.do").permitAll()
-	        
-	        .requestMatchers( 
-	        		"/solarin/bulkCollector.do",
-	        		"/solarin/u/bulkCollector.do").hasAnyAuthority(NODE_AUTHORITIES)
-	        .requestMatchers("/solarin/api/v1/sec/**").hasAnyAuthority(NODE_AUTHORITIES)
-	        
-	        .anyRequest().authenticated()
-	    ;
-	    // @formatter:on
-		return http.build();
-	}
-
-	@Bean
 	public StrictHttpFirewall httpFirewall() {
 		StrictHttpFirewall firewall = new StrictHttpFirewall();
 
@@ -152,6 +109,127 @@ public class WebSecurityConfig {
 		firewall.setAllowedHeaderValues((header) -> true);
 
 		return firewall;
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		JdbcUserDetailsService service = new JdbcUserDetailsService();
+		service.setDataSource(dataSource);
+		service.setUsersByUsernameQuery(JdbcUserDetailsService.DEFAULT_USERS_BY_USERNAME_SQL);
+		service.setAuthoritiesByUsernameQuery(
+				JdbcUserDetailsService.DEFAULT_AUTHORITIES_BY_USERNAME_SQL);
+		return service;
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService());
+		provider.setPasswordEncoder(passwordEncoder);
+		return provider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		return new ProviderManager(authenticationProvider());
+	}
+
+	@Bean
+	public AuthenticationEventPublisher authenticationEventPublisher(
+			ApplicationEventPublisher appEventPublisher) {
+		return new DefaultAuthenticationEventPublisher(appEventPublisher);
+	}
+
+	/**
+	 * Security rules for the management API.
+	 */
+	@Configuration
+	@Order(1)
+	public static class ManagementWebSecurityConfig {
+
+		@Order(1)
+		@Bean
+		public SecurityFilterChain filterChainManagement(HttpSecurity http) throws Exception {
+			// @formatter:off
+		    http
+		      // limit this configuration to specific paths
+		      .securityMatchers()
+		        .requestMatchers("/ops/**")
+		        .and()
+	
+		        // CSRF not needed for stateless calls
+		      .csrf().disable()
+		      
+		      // make sure CORS honored
+		      .cors().and()
+		      
+		      // no sessions
+		      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		      
+		      .httpBasic().realmName("SN Operations").and()
+		      
+		      .authorizeHttpRequests()
+		        .anyRequest().hasAnyAuthority(OPS_AUTHORITY)
+		        
+		    ;
+		    // @formatter:on
+			return http.build();
+		}
+	}
+
+	/**
+	 * Security rules for the management API.
+	 */
+	@Configuration
+	@Order(2)
+	public static class AppWebSecurityConfig {
+
+		@Order(2)
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			// @formatter:off
+		    http
+		      // limit this configuration to specific paths
+		      .securityMatchers()
+		        .requestMatchers("/solarin/**")
+		        .and()
+	
+		        // CSRF not needed for stateless calls
+		      .csrf().disable()
+		      
+		      // make sure CORS honored
+		      .cors().and()
+		      	      
+		      // no sessions
+		      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		      
+		      .x509()
+		        .userDetailsService(new NodeUserDetailsService())
+		        .subjectPrincipalRegex("UID=(.*?),")
+		        .and()
+		        
+		      .authorizeHttpRequests()
+		      	.requestMatchers(HttpMethod.OPTIONS, "/solarin/**").permitAll()
+		        .requestMatchers(HttpMethod.GET, 
+		        		"/solarin/",
+		        		"/solarin/error",
+		        		"/solarin/*.html",
+		        		"/solarin/css/**",
+		        		"/solarin/img/**",
+		        		"/solarin/ping",
+		        		"/solarin/api/v1/pub/**",
+		        		"/solarin/identity.do").permitAll()
+		        
+		        .requestMatchers( 
+		        		"/solarin/bulkCollector.do",
+		        		"/solarin/u/bulkCollector.do").hasAnyAuthority(NODE_AUTHORITY)
+		        .requestMatchers("/solarin/api/v1/sec/**").hasAnyAuthority(NODE_AUTHORITY)
+		        
+		        .anyRequest().authenticated()
+		    ;
+		    // @formatter:on
+			return http.build();
+		}
 	}
 
 }
