ac936ef266cbaab94f41473584ae94006b8f24eb
Matt Magoffin <matt@solarnetwork.net>
2023-06-09 10:13:47 +1200

    Support larger number of stack trace elements in each thread dump.

--- solarnet/mqtt-conn-test/src/main/java/net/solarnetwork/central/mqttconntest/impl/MqttConnectionDebugger.java
+++ solarnet/mqtt-conn-test/src/main/java/net/solarnetwork/central/mqttconntest/impl/MqttConnectionDebugger.java
@@ -22,7 +22,9 @@
 
 package net.solarnetwork.central.mqttconntest.impl;
 
+import java.lang.management.LockInfo;
 import java.lang.management.ManagementFactory;
+import java.lang.management.MonitorInfo;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.util.Arrays;
@@ -79,10 +81,78 @@ public class MqttConnectionDebugger extends BaseMqttConnectionObserver implement
 			ThreadMXBean bean = ManagementFactory.getThreadMXBean();
 			ThreadInfo[] infos = bean.dumpAllThreads(true, true);
 			log.debug("THREAD DUMP:\n{}",
-					Arrays.stream(infos).map(Object::toString).collect(Collectors.joining()));
+					Arrays.stream(infos).map(e -> stackDump(e, 50)).collect(Collectors.joining()));
 		} catch ( Exception e ) {
 			log.error("Error logging thread dump: {}", e.toString());
 		}
+	}
+
+	private static String stackDump(ThreadInfo info, final int maxFrames) {
+		StringBuilder sb = new StringBuilder("\"" + info.getThreadName() + "\""
+				+ (info.isDaemon() ? " daemon" : "") + " prio=" + info.getPriority() + " Id="
+				+ info.getThreadId() + " " + info.getThreadState());
+		if ( info.getLockName() != null ) {
+			sb.append(" on " + info.getLockName());
+		}
+		if ( info.getLockOwnerName() != null ) {
+			sb.append(" owned by \"" + info.getLockOwnerName() + "\" Id=" + info.getLockOwnerId());
+		}
+		if ( info.isSuspended() ) {
+			sb.append(" (suspended)");
+		}
+		if ( info.isInNative() ) {
+			sb.append(" (in native)");
+		}
+		sb.append('\n');
+		final StackTraceElement[] stackTrace = info.getStackTrace();
+		int i = 0;
+		for ( ; i < stackTrace.length && i < maxFrames; i++ ) {
+			StackTraceElement ste = stackTrace[i];
+			sb.append("\tat " + ste.toString());
+			sb.append('\n');
+			if ( i == 0 && info.getLockInfo() != null ) {
+				Thread.State ts = info.getThreadState();
+				switch (ts) {
+					case BLOCKED:
+						sb.append("\t-  blocked on " + info.getLockInfo());
+						sb.append('\n');
+						break;
+					case WAITING:
+						sb.append("\t-  waiting on " + info.getLockInfo());
+						sb.append('\n');
+						break;
+					case TIMED_WAITING:
+						sb.append("\t-  waiting on " + info.getLockInfo());
+						sb.append('\n');
+						break;
+					default:
+				}
+			}
+
+			for ( MonitorInfo mi : info.getLockedMonitors() ) {
+				if ( mi.getLockedStackDepth() == i ) {
+					sb.append("\t-  locked " + mi);
+					sb.append('\n');
+				}
+			}
+		}
+		if ( i < stackTrace.length ) {
+			sb.append("\t...");
+			sb.append('\n');
+		}
+
+		LockInfo[] locks = info.getLockedSynchronizers();
+		if ( locks.length > 0 ) {
+			sb.append("\n\tNumber of locked synchronizers = " + locks.length);
+			sb.append('\n');
+			for ( LockInfo li : locks ) {
+				sb.append("\t- " + li);
+				sb.append('\n');
+			}
+		}
+		sb.append('\n');
+		return sb.toString();
+
 	}
 
 	@Override
