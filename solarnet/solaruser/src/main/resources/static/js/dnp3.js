7023269011a2b42025bde837e860f000e3295d5c
Matt Magoffin <matt@solarnetwork.net>
2023-08-17 17:39:28 +1200

    NET-347: work on DNP3 servers UI.

--- solarnet/solaruser/src/main/resources/static/js/dnp3.js
+++ solarnet/solaruser/src/main/resources/static/js/dnp3.js
@@ -41,24 +41,25 @@ $(document).ready(function() {
 
 		function updateProgressAmount(bar, barAmount, percentComplete) {
 			var value = (percentComplete * 100).toFixed(0);
-			console.log('Progress now ' +percentComplete);
-			if ( bar ) {
-				bar.attr('aria-valuenow', value).css('width', value+'%');
+			console.log('Progress now ' + percentComplete);
+			if (bar) {
+				bar.attr('aria-valuenow', value).css('width', value + '%');
 			}
-			if ( barAmount ) {
+			if (barAmount) {
 				barAmount.text(value);
 			}
 		}
+
 
 		/* ============================
 		   Globals
 		   ============================ */
 		const i18n = SolarReg.i18nData(this);
-		
+
 		const systems = Object.freeze({
 			ca: createSystem($('#dnp3-cas-container'), 'ca'),
+			s: createSystem($('#dnp3-servers-container'), 's'),
 		});
-
 
 
 		/* ============================
@@ -78,14 +79,14 @@ $(document).ready(function() {
 		function renderTrustedIssuerConfigs(configs, preserve) {
 			/** @type {Dnp3System} */
 			const sys = systems['ca'];
-			if ( !sys ) {
+			if (!sys) {
 				return;
 			}
 			configs = Array.isArray(configs) ? configs : [];
-			if ( !preserve ) {
+			if (!preserve) {
 				sys.configsMap.clear();
 			}
-			
+
 			var items = configs.map(function(config) {
 				var model = createTrustedIssuerModel(config);
 				sys.configsMap.set(config.id, model);
@@ -95,7 +96,7 @@ $(document).ready(function() {
 			SolarReg.Templates.populateTemplateItems(sys.container, items, preserve);
 			SolarReg.saveServiceConfigurations(configs, preserve, sys.configs, sys.container);
 		}
-		
+
 		function createTrustedIssuerModel(config) {
 			config.id = config.subjectDn;
 			config.name = config.subjectDn;
@@ -115,38 +116,38 @@ $(document).ready(function() {
 
 		// ***** Import form
 		$('#dnp3-ca-import-modal')
-		.on('shown.bs.modal', SolarReg.Settings.focusEditServiceForm)
-		.on('submit', function(event) {
-			var uploadProgressBar = $('.upload .progress-bar', event.target);
-			var uploadProgressBarAmount = $('.amount', uploadProgressBar);
-			var modal = $(event.target);
-			
-			modal.find('.before').addClass('hidden');
-			modal.find('.upload').removeClass('hidden');
-			
-			SolarReg.Settings.handlePostEditServiceForm(event, function(req, res) {
-				if ( Array.isArray(res) ) {
-					renderTrustedIssuerConfigs(res, true);
-				}
-			}, function serializeCaImportForm(form) {
-				var formData = new FormData(form);
-				return formData;
-			}, {
-				upload: function(event) {
-					if ( event.lengthComputable ) {  
-						updateProgressAmount(uploadProgressBar, uploadProgressBarAmount, event.loaded / event.total);
+			.on('shown.bs.modal', SolarReg.Settings.focusEditServiceForm)
+			.on('submit', function(event) {
+				var uploadProgressBar = $('.upload .progress-bar', event.target);
+				var uploadProgressBarAmount = $('.amount', uploadProgressBar);
+				var modal = $(event.target);
+
+				modal.find('.before').addClass('hidden');
+				modal.find('.upload').removeClass('hidden');
+
+				SolarReg.Settings.handlePostEditServiceForm(event, function(req, res) {
+					if (Array.isArray(res)) {
+						renderTrustedIssuerConfigs(res, true);
 					}
-				}
+				}, function serializeCaImportForm(form) {
+					var formData = new FormData(form);
+					return formData;
+				}, {
+					upload: function(event) {
+						if (event.lengthComputable) {
+							updateProgressAmount(uploadProgressBar, uploadProgressBarAmount, event.loaded / event.total);
+						}
+					}
+				});
+				return false;
+			})
+			.on('hidden.bs.modal', function() {
+				var modal = $(this);
+				modal.find('.before').removeClass('hidden');
+				modal.find('.upload').addClass('hidden');
+				updateProgressAmount(modal.find('.upload .progress-bar'), modal.find('.upload .progress-bar .amount'), 0);
+				SolarReg.Settings.resetEditServiceForm(this);
 			});
-			return false;
-		})
-		.on('hidden.bs.modal', function() {
-			var modal = $(this);
-			modal.find('.before').removeClass('hidden');
-			modal.find('.upload').addClass('hidden');
-			updateProgressAmount(modal.find('.upload .progress-bar'), modal.find('.upload .progress-bar .amount'), 0);
-			SolarReg.Settings.resetEditServiceForm(this);
-		});
 
 		$('#dnp3-ca-edit-modal').on('show.bs.modal', function handleModalShow() {
 			var modal = $(this)
@@ -155,62 +156,244 @@ $(document).ready(function() {
 			SolarReg.Settings.handleSettingToggleButtonChange(modal.find('button[name=enabled]'), enabled);
 			SolarReg.Settings.prepareEditServiceForm(modal, [], []);
 		})
-		.on('submit', function handleCaModalFormSubmit(event) {
-			const config = SolarReg.Templates.findContextItem(this);
-			if ( !config.systemType ) {
-				return;
-			}
-
-			SolarReg.Settings.handlePostEditServiceForm(event, function onSuccess(req, res) {
-				res.id = res.configId;
-				res.systemType = config.systemType;
-				renderTrustedIssuerConfigs([res], true);
-			}, function serializeDataConfigForm(form) {
-				var data = SolarReg.Settings.encodeServiceItemForm(form, true);
-
-				if ( !data.userId ) {
-					// use actor user ID, i.e. for new entity
-					data.userId = form.elements['userId'].dataset.userId;
+			.on('submit', function handleCaModalFormSubmit(event) {
+				const config = SolarReg.Templates.findContextItem(this);
+				if (!config.systemType) {
+					return;
 				}
 
-				return data;
-			}, {
-				urlId: true
-			});
-			return false;
-		})
-		.on('hidden.bs.modal', function handleModalHidden() {
-			const config = SolarReg.Templates.findContextItem(this),
-				type = config.systemType,
+				SolarReg.Settings.handlePostEditServiceForm(event, function onSuccess(req, res) {
+					res.id = res.configId;
+					res.systemType = config.systemType;
+					renderTrustedIssuerConfigs([res], true);
+				}, function serializeDataConfigForm(form) {
+					var data = SolarReg.Settings.encodeServiceItemForm(form, true);
 
-				/** @type {Dnp3System} */
-				sys = systems[type];
-			if ( !sys ) {
+					if (!data.userId) {
+						// use actor user ID, i.e. for new entity
+						data.userId = form.elements['userId'].dataset.userId;
+					}
+
+					return data;
+				}, {
+					urlId: true
+				});
+				return false;
+			})
+			.on('hidden.bs.modal', function handleModalHidden() {
+				const config = SolarReg.Templates.findContextItem(this),
+					type = config.systemType,
+
+					/** @type {Dnp3System} */
+					sys = systems[type];
+				if (!sys) {
+					return;
+				}
+				const container = sys.container.find('.list-container');
+				SolarReg.Settings.resetEditServiceForm(this, container, (id, deleted) => {
+					SolarReg.deleteServiceConfiguration(deleted ? id : null, sys.configs, sys.container);
+					if (deleted) {
+						sys.configsMap.delete(id);
+					}
+				});
+			})
+			.find('button.toggle').each(function() {
+				SolarReg.Settings.setupSettingToggleButton($(this), false);
+			});
+
+		/* ============================
+		   Servers
+		   ============================ */
+
+		function renderServerConfigs(configs, preserve) {
+			/** @type {Dnp3System} */
+			const sys = systems['s'];
+			if (!sys) {
 				return;
 			}
-			const container = sys.container.find('.list-container');
-			SolarReg.Settings.resetEditServiceForm(this, container, (id, deleted) => {
-				SolarReg.deleteServiceConfiguration(deleted ? id : null, sys.configs, sys.container);
-				if ( deleted ) {
-					sys.configsMap.delete(id);
-				}
+			configs = Array.isArray(configs) ? configs : [];
+			if (!preserve) {
+				sys.configsMap.clear();
+			}
+
+			var items = configs.map(function(config) {
+				var model = createServerModel(config);
+				sys.configsMap.set(config.id, model);
+				return model;
 			});
-		})
-		.find('button.toggle').each(function() {
-			SolarReg.Settings.setupSettingToggleButton($(this), false);
+
+			var idx = 0;
+			SolarReg.Templates.populateTemplateItems(sys.container, items, preserve, function serverTemplateCallback(item, el) {
+				idx += 1;
+				el.find('div.panel-heading').attr('id', 'server-heading-' + idx);
+				el.find('.panel-title a').attr('href', '#server-body-' + idx).attr('aria-controls', 'server-body-' + idx);
+				el.find('div.panel-collapse ').attr('id', 'server-body-' + idx).attr('aria-labelledby', 'server-heading-' + idx);
+			});
+			SolarReg.saveServiceConfigurations(configs, preserve, sys.configs, sys.container);
+		}
+
+		function createServerModel(config) {
+			config.id = config.serverId;
+			config.systemType = 's';
+			var model = SolarReg.Settings.serviceConfigurationItem(config, []);
+			model.id = config.id;
+			model.systemType = config.systemType;
+			model.createdDisplay = moment(config.created).format('D MMM YYYY');
+			model.modifiedDisplay = moment(config.modified).format('D MMM YYYY');
+			model.enabled = config.enabled;
+			return model;
+		}
+
+		function handleServerDataPointsExport(item) {
+			if (!(item && item.id)) {
+				return;
+			}
+			let url = document.location.href + '/servers/' + item.id + '/csv';
+			document.location = url;
+		}
+
+		function handleServerDataPointsImport(item) {
+			if (!(item && item.id)) {
+				return;
+			}
+			let editModal = $('#dnp3-server-data-points-import-modal');
+			SolarReg.Templates.setContextItem(editModal, item);
+			editModal.modal('show');
+		}
+
+		systems.s.container.on('click', function handleServersClick(event) {
+			console.log('click on servers %o', event);
+			const target = $(event.target).closest('.action');
+			if (!target.length) {
+				return;
+			}
+			const item = SolarReg.Templates.findContextItem(target);
+			if (target.hasClass('csv-import')) {
+				handleServerDataPointsImport(item);
+			} else if (target.hasClass('csv-export')) {
+				handleServerDataPointsExport(item);
+			}
 		});
+
+		systems.s.container.find('.list-container').on('click', function(event) {
+			// edit server
+			SolarReg.Settings.handleEditServiceItemAction(event, [], []);
+		});
+
+		// ***** Add server form
+		$('#dnp3-server-add-modal')
+			.on('shown.bs.modal', SolarReg.Settings.focusEditServiceForm)
+			.on('submit', function(event) {
+				SolarReg.Settings.handlePostEditServiceForm(event, function(req, res) {
+					if (Array.isArray(res)) {
+						renderServerConfigs(res, true);
+					}
+				});
+				return false;
+			})
+			.on('hidden.bs.modal', function() {
+				SolarReg.Settings.resetEditServiceForm(this);
+			});
+
+		// ***** Edit server
+		$('#dnp3-server-edit-modal').on('show.bs.modal', function handleModalShow() {
+			var modal = $(this)
+				, config = SolarReg.Templates.findContextItem(this)
+				, enabled = (config && config.enabled === true ? true : false);
+			SolarReg.Settings.handleSettingToggleButtonChange(modal.find('button[name=enabled]'), enabled);
+			SolarReg.Settings.prepareEditServiceForm(modal, [], []);
+		})
+			.on('submit', function handleServerModalFormSubmit(event) {
+				const config = SolarReg.Templates.findContextItem(this);
+				if (!config.systemType) {
+					return;
+				}
+
+				SolarReg.Settings.handlePostEditServiceForm(event, function onSuccess(req, res) {
+					res.id = res.configId;
+					res.systemType = config.systemType;
+					renderServerConfigs([res], true);
+				}, undefined, {
+					urlId: true
+				});
+				return false;
+			})
+			.on('hidden.bs.modal', function handleModalHidden() {
+				const config = SolarReg.Templates.findContextItem(this),
+					type = config.systemType,
+
+					/** @type {Dnp3System} */
+					sys = systems[type];
+				if (!sys) {
+					return;
+				}
+				const container = sys.container.find('.list-container');
+				SolarReg.Settings.resetEditServiceForm(this, container, (id, deleted) => {
+					SolarReg.deleteServiceConfiguration(deleted ? id : null, sys.configs, sys.container);
+					if (deleted) {
+						sys.configsMap.delete(id);
+					}
+				});
+			})
+			.find('button.toggle').each(function() {
+				SolarReg.Settings.setupSettingToggleButton($(this), false);
+			});
+			
+		// ***** Import data points CSV
+		$('#dnp3-server-data-points-import-modal')
+			.on('show.bs.modal', function() {
+				const modal = $(this);
+				/** @type HTMLFormElement */
+				const form = this;
+				const config = SolarReg.Templates.findContextItem(this);
+				form.elements.name.value = config.name;
+				form.action = form.dataset.action + '/' + config.id +'/csv';
+			})
+			.on('shown.bs.modal', SolarReg.Settings.focusEditServiceForm)
+			.on('submit', function(event) {
+				var uploadProgressBar = $('.upload .progress-bar', event.target);
+				var uploadProgressBarAmount = $('.amount', uploadProgressBar);
+				var modal = $(event.target);
+
+				modal.find('.before').addClass('hidden');
+				modal.find('.upload').removeClass('hidden');
+
+				SolarReg.Settings.handlePostEditServiceForm(event, function(req, res) {
+					// TODO
+					console.log('TODO: render imported data points: %o', res);
+				}, function serializeServerDataPointsImportForm(form) {
+					var formData = new FormData(form);
+					return formData;
+				}, {
+					upload: function(event) {
+						if (event.lengthComputable) {
+							updateProgressAmount(uploadProgressBar, uploadProgressBarAmount, event.loaded / event.total);
+						}
+					}
+				});
+				return false;
+			})
+			.on('hidden.bs.modal', function() {
+				var modal = $(this);
+				modal.find('.before').removeClass('hidden');
+				modal.find('.upload').addClass('hidden');
+				updateProgressAmount(modal.find('.upload .progress-bar'), modal.find('.upload .progress-bar .amount'), 0);
+				SolarReg.Settings.resetEditServiceForm(this);
+			});
 
 		/* ============================
 		   Init
 		   ============================ */
 		(function initDnp3Management() {
-			var loadCountdown = 1;
+			var loadCountdown = 2;
 			var caConfs = [];
+			var serverConfs = [];
 
 			function liftoff() {
 				loadCountdown -= 1;
-				if ( loadCountdown === 0 ) {
+				if (loadCountdown === 0) {
 					renderTrustedIssuerConfigs(caConfs);
+					renderServerConfigs(serverConfs);
 					SolarReg.showPageLoaded();
 				}
 			}
@@ -218,8 +401,17 @@ $(document).ready(function() {
 			// list all trusted issuers
 			$.getJSON(SolarReg.solarUserURL('/sec/dnp3/trusted-issuer-certs'), function(json) {
 				console.debug('Got DNP3 trusted issuer certificates: %o', json);
-				if ( json && json.success === true ) {
+				if (json && json.success === true) {
 					caConfs = json.data ? json.data.results : undefined;
+				}
+				liftoff();
+			});
+
+			// list all servers
+			$.getJSON(SolarReg.solarUserURL('/sec/dnp3/servers'), function(json) {
+				console.debug('Got DNP3 servers: %o', json);
+				if (json && json.success === true) {
+					serverConfs = json.data ? json.data.results : undefined;
 				}
 				liftoff();
 			});
