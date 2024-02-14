af4e8a9913e48ca0975ea5357f6c9e01dbd169e7
Matt Magoffin <matt@solarnetwork.net>
2024-02-14 17:31:23 +1300

    NET-362: add evse_id column to connector related tables.

--- solarnet-db-setup/postgres/postgres-init-ocpp.sql
+++ solarnet-db-setup/postgres/postgres-init-ocpp.sql
@@ -60,6 +60,7 @@ CREATE TABLE solarev.ocpp_charge_point (
 
 CREATE TABLE solarev.ocpp_charge_point_conn (
 	cp_id				BIGINT NOT NULL,
+	evse_id				INTEGER NOT NULL DEFAULT 0,
 	conn_id				INTEGER NOT NULL,
 	created				TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	status				SMALLINT NOT NULL DEFAULT 0,
@@ -68,7 +69,7 @@ CREATE TABLE solarev.ocpp_charge_point_conn (
 	info				VARCHAR(50),
 	vendor_id			VARCHAR(255),
 	vendor_error		VARCHAR(50),
-	CONSTRAINT ocpp_charge_point_conn_pk PRIMARY KEY (cp_id, conn_id),
+	CONSTRAINT ocpp_charge_point_conn_pk PRIMARY KEY (cp_id, evse_id, conn_id),
 	CONSTRAINT ocpp_charge_point_conn_charge_point_fk FOREIGN KEY (cp_id)
 		REFERENCES solarev.ocpp_charge_point (id)
 		ON DELETE CASCADE
@@ -162,6 +163,7 @@ CREATE TABLE solarev.ocpp_charge_point_status (
  * created 			the row creation date
  * user_id 			the account owner
  * cp_id			the charge point ID
+ * evse_id			the EVSE ID (>= 0) related to the action, or 0 for the charger
  * conn_id			the connector ID (>= 0) related to the action, or 0 for the charger
  * action			the name of the OCPP action
  * msg_id			the action message ID
@@ -171,11 +173,12 @@ CREATE TABLE solarev.ocpp_charge_point_action_status (
 	created			TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	user_id			BIGINT NOT NULL,
 	cp_id			BIGINT NOT NULL,
+	evse_id			INTEGER NOT NULL,
 	conn_id			INTEGER NOT NULL,
 	action			TEXT NOT NULL,
 	msg_id			TEXT NOT NULL,
 	ts				TIMESTAMP WITH TIME ZONE NOT NULL,
-	CONSTRAINT ocpp_charge_point_action_status_pk PRIMARY KEY (user_id, cp_id, conn_id, action),
+	CONSTRAINT ocpp_charge_point_action_status_pk PRIMARY KEY (user_id, cp_id, evse_id, conn_id, action),
 	CONSTRAINT ocpp_charge_point_action_status_charge_point_fk FOREIGN KEY (cp_id)
 		REFERENCES solarev.ocpp_charge_point (id)
 		ON DELETE CASCADE
---
+++ solarnet-db-setup/postgres/updates/NET-362-ocpp2-evse-connector.sql
@@ -0,0 +1,26 @@
+-- add evse_id to connector table
+
+ALTER TABLE solarev.ocpp_charge_point_conn
+ADD COLUMN evse_id INTEGER NOT NULL DEFAULT 0;
+
+ALTER TABLE solarev.ocpp_charge_point_conn
+DROP CONSTRAINT ocpp_charge_point_conn_pk;
+
+ALTER TABLE solarev.ocpp_charge_point_conn
+ADD CONSTRAINT ocpp_charge_point_conn_pk PRIMARY KEY (cp_id, evse_id, conn_id);
+
+-- add evse_id to connector status table
+
+ALTER TABLE solarev.ocpp_charge_point_action_status
+ADD COLUMN evse_id INTEGER NOT NULL DEFAULT 0;
+
+ALTER TABLE solarev.ocpp_charge_point_action_status
+DROP CONSTRAINT ocpp_charge_point_action_status_pk;
+
+ALTER TABLE solarev.ocpp_charge_point_action_status
+ADD CONSTRAINT ocpp_charge_point_action_status_pk PRIMARY KEY (user_id, cp_id, evse_id, conn_id, action);
+
+-- add evse_id to charge session table
+
+ALTER TABLE solarev.ocpp_charge_sess
+ADD COLUMN evse_id INTEGER NOT NULL DEFAULT 0;
