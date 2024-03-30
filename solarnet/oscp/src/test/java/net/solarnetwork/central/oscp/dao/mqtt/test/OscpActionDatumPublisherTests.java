5329b0fbe7ae5b533e085a52bef6dab3fa8f7b79
Matt Magoffin <matt@solarnetwork.net>
2024-03-31 09:57:46 +1300

    Fix Java 21 overloads warning from class implementing both Function and Consumer; refactor Consumer to separate class.

--- solarnet/oscp/src/test/java/net/solarnetwork/central/oscp/dao/mqtt/test/OscpActionDatumPublisherTests.java
+++ solarnet/oscp/src/test/java/net/solarnetwork/central/oscp/dao/mqtt/test/OscpActionDatumPublisherTests.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * OscpActionDatumPublisherTests.java - 11/10/2022 3:51:17 pm
- * 
+ *
  * Copyright 2022 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -70,7 +70,7 @@ import oscp.v20.UpdateGroupCapacityForecast;
 
 /**
  * Test cases for the {@link OscpActionDatumPublisher} class.
- * 
+ *
  * @author matt
  * @version 1.0
  */
@@ -162,7 +162,7 @@ public class OscpActionDatumPublisherTests {
 		givenMqttConnectionPublish();
 
 		// WHEN
-		publisher.accept(pubEvent);
+		publisher.asConsumer().accept(pubEvent);
 
 		// THEN
 		then(conn).should().publish(msgCaptor.capture());
@@ -199,7 +199,7 @@ public class OscpActionDatumPublisherTests {
 		givenMqttConnectionPublish();
 
 		// WHEN
-		publisher.accept(pubEvent);
+		publisher.asConsumer().accept(pubEvent);
 
 		// THEN
 		then(conn).should(times(count)).publish(msgCaptor.capture());
