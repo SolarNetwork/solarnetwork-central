c1ebb924bca32426df12f0df539be9c3a3e95635
Matt Magoffin <matt@solarnetwork.net>
2024-04-09 11:04:57 +1200

    Tweak decimal comparisons in tests to work with Postgres 15's increased decimal precision.

--- solarnet/common-test/src/main/java/net/solarnetwork/central/test/CommonTestUtils.java
+++ solarnet/common-test/src/main/java/net/solarnetwork/central/test/CommonTestUtils.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * CommonTestUtils.java - 6/10/2021 10:14:04 AM
- * 
+ *
  * Copyright 2021 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -27,25 +27,29 @@ import java.math.RoundingMode;
 import java.security.SecureRandom;
 import java.util.UUID;
 import java.util.regex.Pattern;
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import net.solarnetwork.util.ClassUtils;
 
 /**
  * Common test utilities.
- * 
+ *
  * @author matt
- * @version 1.1
+ * @version 1.2
  */
 public final class CommonTestUtils {
 
 	/** A random number generator. */
 	public static final SecureRandom RNG = new SecureRandom();
 
+	/** The default maximum scale. */
+	public static final int DEFAULT_MAX_SCALE = 9;
+
 	/**
 	 * Create a {@link Matcher} for a string that compares to the contents of a
 	 * text resource.
-	 * 
+	 *
 	 * @param resource
 	 *        the name of the resource
 	 * @param clazz
@@ -61,7 +65,7 @@ public final class CommonTestUtils {
 	/**
 	 * Create a {@link Matcher} for a string that compares to the contents of a
 	 * text resource.
-	 * 
+	 *
 	 * @param resource
 	 *        the name of the resource
 	 * @param clazz
@@ -80,7 +84,7 @@ public final class CommonTestUtils {
 
 	/**
 	 * Get a random decimal number.
-	 * 
+	 *
 	 * @return the random decimal number
 	 */
 	public static BigDecimal randomDecimal() {
@@ -89,7 +93,7 @@ public final class CommonTestUtils {
 
 	/**
 	 * Get a random string value.
-	 * 
+	 *
 	 * @return the string
 	 */
 	public static String randomString() {
@@ -98,10 +102,109 @@ public final class CommonTestUtils {
 
 	/**
 	 * Get a random long value.
-	 * 
+	 *
 	 * @return the long
 	 */
 	public static Long randomLong() {
 		return RNG.nextLong();
 	}
+
+	/**
+	 * Compare a decimal array using a maximum scale.
+	 *
+	 * <p>
+	 * The {@link #DEFAULT_MAX_SCALE} maximum scale and
+	 * {@link RoundingMode#DONW} rounding mode will be used.
+	 * </p>
+	 *
+	 * @param expected
+	 *        the expected decimals
+	 * @return the matcher
+	 * @since 1.2
+	 */
+	public static Matcher<BigDecimal[]> decimalArrayMatching(BigDecimal[] expected) {
+		return decimalArrayMatching(expected, DEFAULT_MAX_SCALE, RoundingMode.DOWN);
+	}
+
+	/**
+	 * Compare a decimal array using a maximum scale.
+	 *
+	 * @param expected
+	 *        the expected decimals
+	 * @param maxScale
+	 *        the maximum scale to compare at
+	 * @param mode
+	 *        the rounding mode to use
+	 * @return the matcher
+	 * @since 1.2
+	 */
+	public static Matcher<BigDecimal[]> decimalArrayMatching(BigDecimal[] expected, int maxScale,
+			RoundingMode mode) {
+		return new org.hamcrest.CustomMatcher<BigDecimal[]>("decimal array") {
+
+			@Override
+			public boolean matches(Object actual) {
+				if ( actual == null && expected != null ) {
+					return false;
+				} else if ( actual instanceof BigDecimal[] array ) {
+					if ( array.length != expected.length ) {
+						return false;
+					}
+					for ( int i = 0; i < expected.length; i++ ) {
+						BigDecimal a = array[i];
+						BigDecimal e = expected[i];
+						if ( a == null ) {
+							return false;
+						}
+						if ( e.compareTo(a) == 0 ) {
+							return true;
+						}
+						if ( a.scale() != e.scale() ) {
+							BigDecimal a1 = a.setScale(Math.min(maxScale, e.scale()), mode);
+							BigDecimal e1 = e.setScale(Math.min(maxScale, e.scale()), mode);
+							if ( e1.compareTo(a1) == 0 ) {
+								return true;
+							}
+						}
+					}
+				}
+				return false;
+			}
+
+			@Override
+			public void describeMismatch(Object actual, Description description) {
+				if ( actual == null && expected != null ) {
+					description.appendText("was null");
+				} else if ( actual instanceof BigDecimal[] array ) {
+					if ( array.length != expected.length ) {
+						description.appendText("length was %d ".formatted(expected.length))
+								.appendValue(actual);
+						return;
+					}
+					for ( int i = 0; i < expected.length; i++ ) {
+						BigDecimal a = array[i];
+						BigDecimal e = expected[i];
+						if ( a == null ) {
+							description.appendText("item %d not null".formatted(i));
+							return;
+						}
+						if ( e.compareTo(a) == 0 ) {
+							continue;
+						}
+						BigDecimal a1 = a.setScale(Math.min(maxScale, e.scale()), mode);
+						BigDecimal e1 = e.setScale(Math.min(maxScale, e.scale()), mode);
+						if ( e1.compareTo(a1) == 0 ) {
+							continue;
+						}
+						description.appendText("item %d was ".formatted(i)).appendValue(a1);
+						return;
+					}
+				} else {
+					super.describeMismatch(actual, description);
+				}
+			}
+
+		};
+	}
+
 }
