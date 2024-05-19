ea2022082226f90ab1d16ed4c489a2d73abb3f3d
Matt Magoffin <matt@solarnetwork.net>
2024-05-19 16:01:02 +1200

    Configure virtual threads for SolarDNP3.

--- solarnet/solardnp3/src/main/java/net/solarnetwork/central/dnp3/app/SolarDnp3App.java
+++ solarnet/solardnp3/src/main/java/net/solarnetwork/central/dnp3/app/SolarDnp3App.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * SolarDnp3App.java - 1/08/2023 10:08:20 am
- * 
+ *
  * Copyright 2023 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -33,6 +33,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import net.solarnetwork.central.common.config.SolarNetCommonConfiguration;
 import net.solarnetwork.central.common.dao.config.SolarNetCommonDaoConfiguration;
+import net.solarnetwork.central.common.mixin.config.VirtualThreadTaskConfig;
 import net.solarnetwork.central.datum.config.SolarNetDatumConfiguration;
 import net.solarnetwork.central.dnp3.config.SolarNetDnp3Configuration;
 import net.solarnetwork.central.instructor.config.SolarNetInstructorConfiguration;
@@ -43,9 +44,9 @@ import net.solarnetwork.util.ApplicationContextUtils;
 
 /**
  * Main entry point for the SolarDNP3 application.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 //@formatter:off
 @SpringBootApplication(scanBasePackageClasses = {
@@ -56,13 +57,14 @@ import net.solarnetwork.util.ApplicationContextUtils;
 		SolarDnp3App.class,
 })
 @Import({
+		VirtualThreadTaskConfig.class,
 		SolarNetCommonConfiguration.class,
 		SolarNetCommonDaoConfiguration.class,
 		SolarNetCommonSecurityConfiguration.class,
 		UserDaoConfig.class,
 })
 @ConfigurationPropertiesScan(basePackageClasses = {
-		SolarNetCommonNetProxyConfiguration.class,		
+		SolarNetCommonNetProxyConfiguration.class,
 })
 //@formatter:on
 public class SolarDnp3App {
@@ -71,7 +73,7 @@ public class SolarDnp3App {
 
 	/**
 	 * Command-line entry point to launching server.
-	 * 
+	 *
 	 * @param args
 	 *        command line arguments
 	 */
@@ -81,7 +83,7 @@ public class SolarDnp3App {
 
 	/**
 	 * Get a command line argument processor.
-	 * 
+	 *
 	 * @param ctx
 	 *        The application context.
 	 * @return The command line runner.
