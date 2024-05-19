e3fb74d4391e6d39eb9ef63864f89921e8b5938c
Matt Magoffin <matt@solarnetwork.net>
2024-05-19 16:05:46 +1200

    Configure virtual threads for SolarOSCP-FP.

--- solarnet/oscp-fp/src/main/java/net/solarnetwork/central/oscp/fp/SolarOscpFlexibilityProviderApp.java
+++ solarnet/oscp-fp/src/main/java/net/solarnetwork/central/oscp/fp/SolarOscpFlexibilityProviderApp.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * SolarOscpFlexibilityProviderApp.java - 10/08/2022 5:11:01 pm
- * 
+ *
  * Copyright 2022 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -29,6 +29,7 @@ import net.solarnetwork.central.common.config.SolarNetCommonConfiguration;
 import net.solarnetwork.central.common.dao.config.MainDataSourceConfig;
 import net.solarnetwork.central.common.dao.config.MyBatisCommonConfig;
 import net.solarnetwork.central.common.dao.config.SolarNodeOwnershipDaoConfig;
+import net.solarnetwork.central.common.mixin.config.VirtualThreadTaskConfig;
 import net.solarnetwork.central.datum.config.JdbcDatumEntityDaoConfig;
 import net.solarnetwork.central.instructor.config.SolarNetInstructorConfiguration;
 import net.solarnetwork.central.oscp.config.SolarNetOscpConfiguration;
@@ -36,9 +37,9 @@ import net.solarnetwork.central.web.support.WebServiceGlobalControllerSupport;
 
 /**
  * Main entry point for the SolarOscp Flexibility Provider application.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 //@formatter:off
 @SpringBootApplication(scanBasePackageClasses = {
@@ -47,6 +48,7 @@ import net.solarnetwork.central.web.support.WebServiceGlobalControllerSupport;
 		SolarOscpFlexibilityProviderApp.class,
 })
 @Import({
+	VirtualThreadTaskConfig.class,
 	SolarNetCommonConfiguration.class,
 	MyBatisCommonConfig.class,
 	MainDataSourceConfig.class,
@@ -59,7 +61,7 @@ public class SolarOscpFlexibilityProviderApp {
 
 	/**
 	 * Command-line entry point to launching server.
-	 * 
+	 *
 	 * @param args
 	 *        command line arguments
 	 */
