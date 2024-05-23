14642e2d9e98ea2f7f2dec2ea422fcc534b16513
Matt Magoffin <matt@solarnetwork.net>
2024-05-24 10:47:50 +1200

    NET-376: add proxyBeanMethods = false where possible to improve startup time.

--- solarnet/user-billing/src/main/java/net/solarnetwork/central/user/billing/snf/config/SnfBillingSystemConfig.java
+++ solarnet/user-billing/src/main/java/net/solarnetwork/central/user/billing/snf/config/SnfBillingSystemConfig.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * BillingSystemConfig.java - 1/11/2021 10:51:41 AM
- * 
+ *
  * Copyright 2021 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -47,11 +47,11 @@ import net.solarnetwork.central.user.billing.snf.dao.TaxCodeDao;
 
 /**
  * SNF BillingSystem configuration.
- * 
+ *
  * @author matt
  * @version 1.0
  */
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class SnfBillingSystemConfig {
 
 	@Autowired
@@ -90,8 +90,8 @@ public class SnfBillingSystemConfig {
 	private int deliveryTimeoutSecs = 60;
 
 	@Bean
-	public BillingSystem snfBillingSystem() {
-		SnfBillingSystem system = new SnfBillingSystem(snfInvoicingSystem(), accountDao, invoiceDao);
+	public BillingSystem snfBillingSystem(SnfInvoicingSystem snfInvoicingSystem) {
+		SnfBillingSystem system = new SnfBillingSystem(snfInvoicingSystem, accountDao, invoiceDao);
 		return system;
 	}
 
