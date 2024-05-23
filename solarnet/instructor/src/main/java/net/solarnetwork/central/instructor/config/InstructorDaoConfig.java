d6007b89b940770534bd7fb8213c9608c2cea354
Matt Magoffin <matt@solarnetwork.net>
2024-05-24 10:35:51 +1200

    NET-376: add proxyBeanMethods = false where possible to improve startup time.

--- solarnet/instructor/src/main/java/net/solarnetwork/central/instructor/config/InstructorDaoConfig.java
+++ solarnet/instructor/src/main/java/net/solarnetwork/central/instructor/config/InstructorDaoConfig.java
@@ -35,7 +35,7 @@ import net.solarnetwork.central.instructor.dao.mybatis.MyBatisNodeInstructionDao
  * @author matt
  * @version 1.0
  */
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class InstructorDaoConfig {
 
 	@Autowired
