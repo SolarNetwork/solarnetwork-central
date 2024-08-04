1f4adb3e9a52f4c1a96d78c52bbe1f31dd126548
Matt Magoffin <matt@solarnetwork.net>
2024-08-05 11:12:04 +1200

    Add test for non-UTC time zone.

--- solarnet/datum/src/test/java/net/solarnetwork/central/datum/v2/dao/jdbc/test/DbFindAggHodTests.java
+++ solarnet/datum/src/test/java/net/solarnetwork/central/datum/v2/dao/jdbc/test/DbFindAggHodTests.java
@@ -1,33 +1,34 @@
 /* ==================================================================
  * DbFindAggHodTests.java - 10/12/2020 9:12:05 pm
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
 
 package net.solarnetwork.central.datum.v2.dao.jdbc.test;
 
+import static java.util.Collections.singleton;
 import static java.util.stream.Collectors.joining;
 import static net.solarnetwork.central.datum.v2.dao.jdbc.test.DatumTestUtils.assertAggregateDatum;
 import static net.solarnetwork.domain.datum.DatumProperties.propertiesOf;
 import static net.solarnetwork.domain.datum.DatumPropertiesStatistics.statisticsOf;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import java.math.BigDecimal;
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -36,10 +37,12 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import org.junit.Test;
 import org.springframework.dao.DataAccessException;
@@ -51,22 +54,27 @@ import net.solarnetwork.central.datum.v2.dao.jdbc.AggregateDatumEntityRowMapper;
 import net.solarnetwork.central.datum.v2.dao.jdbc.DatumDbUtils;
 import net.solarnetwork.central.datum.v2.domain.AggregateDatum;
 import net.solarnetwork.central.datum.v2.domain.BasicObjectDatumStreamMetadata;
+import net.solarnetwork.domain.datum.Aggregation;
 import net.solarnetwork.domain.datum.DatumProperties;
 import net.solarnetwork.domain.datum.DatumPropertiesStatistics;
 import net.solarnetwork.domain.datum.ObjectDatumKind;
 import net.solarnetwork.domain.datum.ObjectDatumStreamMetadata;
-import net.solarnetwork.domain.datum.Aggregation;
 
 /**
  * Test cases for the {@literal solardatm.find_agg_dow} database stored
  * procedure.
- * 
+ *
  * @author matt
  * @version 1.0
  */
 public class DbFindAggHodTests extends BaseDatumJdbcTestSupport {
 
 	private List<AggregateDatum> findAggHod(UUID streamId, Instant start, Instant end) {
+		List<Map<String, Object>> meta = jdbcTemplate.queryForList(
+				"SELECT * FROM solardatm.find_metadata_for_stream(?::uuid)", streamId.toString());
+		log.debug("Metadata for stream:\n{}",
+				meta.stream().map(Object::toString).collect(joining("\n")));
+
 		return jdbcTemplate.execute(new ConnectionCallback<List<AggregateDatum>>() {
 
 			@Override
@@ -91,7 +99,7 @@ public class DbFindAggHodTests extends BaseDatumJdbcTestSupport {
 						}
 					}
 				}
-				log.debug("Found agg DOW for stream:\n{}",
+				log.debug("Found agg HOD for stream:\n{}",
 						result.stream().map(Object::toString).collect(joining("\n")));
 				return result;
 			}
@@ -99,12 +107,13 @@ public class DbFindAggHodTests extends BaseDatumJdbcTestSupport {
 	}
 
 	private ObjectDatumStreamMetadata testStreamMetadata() {
-		return testStreamMetadata(1L, "a");
+		return testStreamMetadata(1L, "a", "UTC");
 	}
 
-	private ObjectDatumStreamMetadata testStreamMetadata(Long nodeId, String sourceId) {
-		return new BasicObjectDatumStreamMetadata(UUID.randomUUID(), "UTC", ObjectDatumKind.Node, nodeId,
-				sourceId, new String[] { "x", "y" }, new String[] { "w" }, null);
+	private ObjectDatumStreamMetadata testStreamMetadata(Long nodeId, String sourceId,
+			String timeZoneId) {
+		return new BasicObjectDatumStreamMetadata(UUID.randomUUID(), timeZoneId, ObjectDatumKind.Node,
+				nodeId, sourceId, new String[] { "x", "y" }, new String[] { "w" }, null);
 	}
 
 	@Test
@@ -113,6 +122,57 @@ public class DbFindAggHodTests extends BaseDatumJdbcTestSupport {
 		ObjectDatumStreamMetadata meta = testStreamMetadata();
 
 		ZonedDateTime start = ZonedDateTime.of(2020, 6, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+		List<AggregateDatum> datums = new ArrayList<>(3 * 24);
+		for ( int day = 0; day < 3; day++ ) {
+			for ( int hour = 0; hour < 24; hour++ ) {
+				Instant ts = start.plusDays(day).plusHours(hour).toInstant();
+				DatumProperties props = propertiesOf(new BigDecimal[] { new BigDecimal(day + hour) },
+						new BigDecimal[] { new BigDecimal((day + 1) * (hour + 1)) }, null, null);
+				DatumPropertiesStatistics stats = statisticsOf(
+						new BigDecimal[][] { new BigDecimal[] { new BigDecimal(6),
+								new BigDecimal(day + hour - 1), new BigDecimal(day + hour + 1) } },
+						new BigDecimal[][] { new BigDecimal[] { new BigDecimal(0), new BigDecimal(0),
+								new BigDecimal(0) } });
+
+				datums.add(new AggregateDatumEntity(meta.getStreamId(), ts, Aggregation.Hour, props,
+						stats));
+			}
+		}
+		DatumDbUtils.insertAggregateDatum(log, jdbcTemplate, datums);
+
+		// WHEN
+		List<AggregateDatum> results = findAggHod(meta.getStreamId(), Instant.EPOCH, Instant.now());
+
+		// THEN
+		assertThat("24 hour results for one stream", results, hasSize(24));
+		ZonedDateTime date = ZonedDateTime.of(2001, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+		for ( int i = 0; i < 24; i++ ) {
+			AggregateDatum d = results.get(i);
+			DatumProperties props = propertiesOf(new BigDecimal[] { new BigDecimal(i + 1) },
+					new BigDecimal[] { new BigDecimal((i + 1) * 2) }, null, null);
+			DatumPropertiesStatistics stats = statisticsOf(
+					new BigDecimal[][] { new BigDecimal[] { new BigDecimal(18), new BigDecimal(i - 1),
+							new BigDecimal(i + 3) } },
+					new BigDecimal[][] { new BigDecimal[] { null, new BigDecimal(i + 1),
+							new BigDecimal(3 * (i + 1)) } });
+			assertAggregateDatum("Hour " + i, d, new AggregateDatumEntity(meta.getStreamId(),
+					date.plusHours(i).toInstant(), Aggregation.HourOfDay, props, stats));
+		}
+	}
+
+	@Test
+	public void find_hod_tz() {
+		// GIVEN
+		// a node in Pacific/Honolulu time zone
+		final ZoneId zone = ZoneId.of("Pacific/Honolulu");
+		setupTestLocation(1L, zone.getId());
+		setupTestNode(1L, 1L);
+
+		// datum stream metadata for node
+		ObjectDatumStreamMetadata meta = testStreamMetadata(1L, "a", zone.getId());
+		DatumDbUtils.insertObjectDatumStreamMetadata(log, jdbcTemplate, singleton(meta));
+
+		ZonedDateTime start = ZonedDateTime.of(2020, 6, 1, 0, 0, 0, 0, zone);
 		List<AggregateDatum> datums = new ArrayList<>(3 * 24);
 		for ( int day = 0; day < 3; day++ ) {
 			for ( int hour = 0; hour < 24; hour++ ) {
