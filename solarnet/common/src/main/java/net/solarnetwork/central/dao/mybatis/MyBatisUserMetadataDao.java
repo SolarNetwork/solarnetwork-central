7dc1a38a9af7de96bc1d4e3620606f97b79e417a
Matt Magoffin <matt@solarnetwork.net>
2025-02-03 10:52:08 +1300

    Code quality improvements surfaced by IntelliJ.

--- solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/MyBatisUserMetadataDao.java
+++ solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/MyBatisUserMetadataDao.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * MyBatisUserMetadataDao.java - 11/11/2016 5:42:00 PM
- * 
+ *
  * Copyright 2007-2016 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -35,7 +35,7 @@ import net.solarnetwork.domain.SortDescriptor;
 
 /**
  * MyBatis implementation of {@link UserMetadataDao}.
- * 
+ *
  * @author matt
  * @version 2.2
  * @since 1.8
@@ -58,7 +58,7 @@ public class MyBatisUserMetadataDao extends
 
 	@Override
 	public String jsonMetadataAtPath(Long userId, String path) {
-		Map<String, Object> params = new HashMap<String, Object>(1);
+		Map<String, Object> params = new HashMap<>(1);
 		params.put("userId", requireNonNullArgument(userId, "userId"));
 		params.put("path", requireNonNullArgument(path, "path"));
 		return selectFirst(QUERY_FOR_METADATA_PATH, params);
--- solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/MyBatisVersionedMessageDao.java
+++ solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/MyBatisVersionedMessageDao.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * MyBatisVersionedMessageDao.java - 25/07/2020 10:46:20 AM
- * 
+ *
  * Copyright 2020 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -33,7 +33,7 @@ import net.solarnetwork.domain.KeyValuePair;
 
 /**
  * MyBatis implementation of {@link VersionedMessageDao}.
- * 
+ *
  * @author matt
  * @version 1.0
  * @since 2.6
@@ -53,7 +53,7 @@ public class MyBatisVersionedMessageDao extends BaseMyBatisDao implements Versio
 
 		/**
 		 * Get the query name.
-		 * 
+		 *
 		 * @return the query name
 		 */
 		public String getQueryName() {
@@ -71,13 +71,10 @@ public class MyBatisVersionedMessageDao extends BaseMyBatisDao implements Versio
 		if ( data == null || data.isEmpty() ) {
 			return null;
 		}
-		Properties props = data.stream().reduce(new Properties(), (p, e) -> {
+		return data.stream().reduce(new Properties(), (p, e) -> {
 			p.put(e.getKey(), e.getValue());
 			return p;
-		}, (l, r) -> {
-			return l;
-		});
-		return props;
+		}, (l, r) -> l);
 	}
 
 }
