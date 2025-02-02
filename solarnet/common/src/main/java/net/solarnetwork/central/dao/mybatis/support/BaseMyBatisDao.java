df8ff353d039bc94320adcfbaddc298f23ecdbb1
Matt Magoffin <matt@solarnetwork.net>
2025-02-03 10:46:15 +1300

    Code quality improvements surfaced by IntelliJ.

--- solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/support/BaseMyBatisDao.java
+++ solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/support/BaseMyBatisDao.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * BaseMyBatisDao.java - Nov 10, 2014 1:02:32 PM
- * 
+ *
  * Copyright 2007-2014 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -43,7 +43,7 @@ import net.solarnetwork.domain.SortDescriptor;
 
 /**
  * Base DAO support for MyBatis implementations
- * 
+ *
  * @author matt
  * @version 1.4
  */
@@ -78,7 +78,7 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 	 * Select the first available result from a query. This is similar to
 	 * {@link SqlSession#selectOne(String, Object)} except that the
 	 * {@link BaseMyBatisGenericDao#FIRST_ROW} bounds is passed to the database.
-	 * 
+	 *
 	 * @param statement
 	 *        the name of the SQL statement to execute
 	 * @param parameters
@@ -89,15 +89,15 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 	 */
 	protected final <E> E selectFirst(String statement, Object parameters) {
 		List<E> results = getSqlSession().selectList(statement, parameters, FIRST_ROW);
-		if ( results.size() > 0 ) {
-			return results.get(0);
+		if ( !results.isEmpty() ) {
+			return results.getFirst();
 		}
 		return null;
 	}
 
 	/**
 	 * Select a list with optional support for row bounds.
-	 * 
+	 *
 	 * @param statement
 	 *        the name of the SQL statement to execute
 	 * @param parameters
@@ -112,7 +112,7 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 	 */
 	protected final <E> List<E> selectList(final String statement, Object parameters, Long offset,
 			Integer max) {
-		List<E> rows = null;
+		List<E> rows;
 		if ( max != null && max > 0 ) {
 			rows = getSqlSession().selectList(statement, parameters, new RowBounds(
 					(offset == null || offset.intValue() < 0 ? 0 : offset.intValue()), max));
@@ -124,7 +124,7 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 
 	/**
 	 * Execute a {@code SELECT} query that returns a single long value.
-	 * 
+	 *
 	 * @param statement
 	 *        the name of the SQL statement to execute
 	 * @param parameters
@@ -142,13 +142,13 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 
 	/**
 	 * Execute a count query for a filter.
-	 * 
+	 *
 	 * <p>
 	 * If the query throws an {@link IllegalArgumentException} this method
 	 * assumes that means the query name was not found, and will simply return
 	 * {@literal null}.
 	 * </p>
-	 * 
+	 *
 	 * @param countQueryName
 	 *        the query name
 	 * @param sqlProps
@@ -175,7 +175,7 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 
 	/**
 	 * Perform a filter search using standardized semantics.
-	 * 
+	 *
 	 * @param <M>
 	 *        the match type
 	 * @param <K>
@@ -187,9 +187,9 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 	 * @param filter
 	 *        the filter
 	 * @return the results
-	 * @since 1.3
-	 * @see #selectFiltered(String, Object, List, Integer, Integer, BiConsumer,
+	 * @see #selectFiltered(String, Object, List, Long, Integer, BiConsumer,
 	 *      FilterResultsFactory)
+	 * @since 1.3
 	 */
 	protected <M extends Identity<K>, K, F> FilterResults<M, K> selectFiltered(String query, F filter) {
 		return selectFiltered(query, filter, null, null, null, null, null);
@@ -197,7 +197,7 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 
 	/**
 	 * Perform a filter search using standardized semantics.
-	 * 
+	 *
 	 * @param <M>
 	 *        the match type
 	 * @param <K>
@@ -215,9 +215,9 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 	 * @param max
 	 *        the maximum number of results
 	 * @return the results
-	 * @since 1.3
-	 * @see #selectFiltered(String, Object, List, Integer, Integer, BiConsumer,
+	 * @see #selectFiltered(String, Object, List, Long, Integer, BiConsumer,
 	 *      FilterResultsFactory)
+	 * @since 1.3
 	 */
 	protected <M extends Identity<K>, K, F> FilterResults<M, K> selectFiltered(String query, F filter,
 			List<SortDescriptor> sorts, Long offset, Integer max) {
@@ -226,7 +226,7 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 
 	/**
 	 * Perform a filter search using standardized semantics.
-	 * 
+	 *
 	 * @param <M>
 	 *        the match type
 	 * @param <K>
@@ -246,9 +246,9 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 	 * @param propertyProcessor
 	 *        an optional property process to adjust the SQL properties
 	 * @return the results
-	 * @since 1.3
-	 * @see #selectFiltered(String, Object, List, Integer, Integer, BiConsumer,
+	 * @see #selectFiltered(String, Object, List, Long, Integer, BiConsumer,
 	 *      FilterResultsFactory)
+	 * @since 1.3
 	 */
 	protected <M extends Identity<K>, K, F> FilterResults<M, K> selectFiltered(String query, F filter,
 			List<SortDescriptor> sorts, Long offset, Integer max,
@@ -258,19 +258,19 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 
 	/**
 	 * Perform a filter search using standardized semantics.
-	 * 
+	 *
 	 * <p>
 	 * The following steps are taken:
 	 * </p>
-	 * 
+	 *
 	 * <ol>
 	 * <li>Create a SQL parameters map with a
 	 * {@link BaseMyBatisGenericDaoSupport#FILTER_PROPERTY} key and {@code f}
 	 * value.</li>
 	 * <li>If {@code filter} implements {@link SortCriteria} and provides sort
 	 * descriptors, those will be set as the SQL parameter
-	 * {@link BaseMyBatisGenericDaoSupport#SORT_DESCRIPTORS_PROPERTY}. Otherwise
-	 * the {@code sorts} argument will be set.</li>
+	 * {@link BaseMyBatisGenericDaoSupport#SORT_DESCRIPTORS_PROPERTY}.
+	 * Otherwise, the {@code sorts} argument will be set.</li>
 	 * <li>If {@code propertyProcessor} is not {@literal null} invoke that.</li>
 	 * <li>If {@code filter} implements {@link PaginationCriteria} and provides
 	 * pagination values, those will be used in preference to the {@code offset}
@@ -278,13 +278,13 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 	 * <li>If {@code max} is not {@literal null}, and if filter implements
 	 * {@link OptimizedQueryCriteria} and does not disable a total results
 	 * count, then call {@link #executeCountQuery(String, Map)}</li>
-	 * <li>Call {@link #selectList(String, Object, Integer, Integer)}</li>
+	 * <li>Call {@link #selectList(String, Object, Long, Integer)}</li>
 	 * <li>Create a result object, or if {@code resultsFactory} provided then
 	 * call
 	 * {@link FilterResultsFactory#createFilterResults(Object, Map, Iterable, Long, Long, Integer)}
 	 * and return the result.</li>
 	 * </ol>
-	 * 
+	 *
 	 * @param <M>
 	 *        the match type
 	 * @param <K>
@@ -312,14 +312,14 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 			F filter, List<SortDescriptor> sorts, Long offset, Integer max,
 			BiConsumer<F, Map<String, Object>> propertyProcessor,
 			FilterResultsFactory<M, K, F> resultsFactory) {
-		Map<String, Object> sqlProps = new HashMap<String, Object>(1);
+		Map<String, Object> sqlProps = new HashMap<>(1);
 		sqlProps.put(FILTER_PROPERTY, filter);
 
 		// if filter is SortCriteria and provides sort values, use those over method args
 		if ( filter instanceof SortCriteria && ((SortCriteria) filter).getSorts() != null
 				&& !((SortCriteria) filter).getSorts().isEmpty() ) {
 			sqlProps.put(SORT_DESCRIPTORS_PROPERTY, ((SortCriteria) filter).getSorts());
-		} else if ( sorts != null && sorts.size() > 0 ) {
+		} else if ( sorts != null && !sorts.isEmpty() ) {
 			sqlProps.put(SORT_DESCRIPTORS_PROPERTY, sorts);
 		}
 
@@ -330,10 +330,9 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 		// if filter provides pagination values, don't use MyBatis pagination
 		Integer m = max;
 		Long o = offset;
-		if ( filter instanceof PaginationCriteria ) {
-			final PaginationCriteria pagination = (PaginationCriteria) filter;
-			if ( pagination.getMax() != null && pagination.getMax().intValue() > 0
-					&& pagination.getOffset() != null && pagination.getOffset().intValue() >= 0 ) {
+		if ( filter instanceof PaginationCriteria pagination ) {
+			if ( pagination.getMax() != null && pagination.getMax() > 0 && pagination.getOffset() != null
+					&& pagination.getOffset() >= 0 ) {
 				max = pagination.getMax();
 				m = null;
 				offset = pagination.getOffset();
@@ -344,11 +343,11 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 		// attempt count first, if max NOT null or specified as -1; if filter is instance of OptimizedQueryCriteria
 		// check the withoutTotalResultsCount flag
 		Long totalCount = null;
-		if ( max != null && max.intValue() != -1 && !((filter instanceof OptimizedQueryCriteria)
+		if ( max != null && max != -1 && !((filter instanceof OptimizedQueryCriteria)
 				&& ((OptimizedQueryCriteria) filter).isWithoutTotalResultsCount()) ) {
 			Long n = executeCountQuery(query + "-count", sqlProps);
 			if ( n != null ) {
-				totalCount = n.longValue();
+				totalCount = n;
 			}
 		}
 
@@ -358,8 +357,7 @@ public abstract class BaseMyBatisDao extends SqlSessionDaoSupport {
 			return resultsFactory.createFilterResults(filter, sqlProps, rows, totalCount, offset,
 					rows.size());
 		}
-		return new BasicFilterResults<M, K>(rows, totalCount, offset != null ? offset.intValue() : 0,
-				rows.size());
+		return new BasicFilterResults<>(rows, totalCount, offset != null ? offset : 0, rows.size());
 	}
 
 }
--- solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/support/BaseMyBatisFilterableDao.java
+++ solarnet/common/src/main/java/net/solarnetwork/central/dao/mybatis/support/BaseMyBatisFilterableDao.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * BaseMyBatisFilterableDao.java - Nov 10, 2014 7:26:27 AM
- * 
+ *
  * Copyright 2007-2014 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -26,17 +26,17 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import net.solarnetwork.dao.FilterableDao;
 import net.solarnetwork.central.domain.Filter;
 import net.solarnetwork.central.domain.FilterMatch;
 import net.solarnetwork.dao.BasicFilterResults;
 import net.solarnetwork.dao.Entity;
 import net.solarnetwork.dao.FilterResults;
+import net.solarnetwork.dao.FilterableDao;
 import net.solarnetwork.domain.SortDescriptor;
 
 /**
  * Base MyBatis {@link FilterableDao} implementation.
- * 
+ *
  * @author matt
  * @version 1.4
  */
@@ -50,7 +50,7 @@ public abstract class BaseMyBatisFilterableDao<T extends Entity<PK>, M extends F
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param domainClass
 	 *        the domain class
 	 * @param pkClass
@@ -66,7 +66,7 @@ public abstract class BaseMyBatisFilterableDao<T extends Entity<PK>, M extends F
 
 	/**
 	 * Get the filter query name for a given domain.
-	 * 
+	 *
 	 * @param filterDomain
 	 *        the domain
 	 * @param filter
@@ -80,7 +80,7 @@ public abstract class BaseMyBatisFilterableDao<T extends Entity<PK>, M extends F
 	/**
 	 * Callback to alter the default SQL properties set up by
 	 * {@link #findFiltered(Filter, List, Long, Integer)}.
-	 * 
+	 *
 	 * @param filter
 	 *        the current filter
 	 * @param sqlProps
@@ -95,40 +95,38 @@ public abstract class BaseMyBatisFilterableDao<T extends Entity<PK>, M extends F
 			Integer max) {
 		final String filterDomain = getMemberDomainKey(filterResultClass);
 		final String query = getFilteredQuery(filterDomain, filter);
-		Map<String, Object> sqlProps = new HashMap<String, Object>(1);
+		Map<String, Object> sqlProps = new HashMap<>(1);
 		sqlProps.put(FILTER_PROPERTY, filter);
-		if ( sortDescriptors != null && sortDescriptors.size() > 0 ) {
+		if ( sortDescriptors != null && !sortDescriptors.isEmpty() ) {
 			sqlProps.put(SORT_DESCRIPTORS_PROPERTY, sortDescriptors);
 		}
 		postProcessFilterProperties(filter, sqlProps);
 
 		// attempt count first, if max NOT specified as -1
 		Long totalCount = null;
-		if ( max != null && max.intValue() != -1 ) {
+		if ( max != null && max != -1 ) {
 			Long n = executeFilterCountQuery(query + "-count", filter, sqlProps);
 			if ( n != null ) {
-				totalCount = n.longValue();
+				totalCount = n;
 			}
 		}
 
 		List<M> rows = selectList(query, sqlProps, offset, max);
 
-		BasicFilterResults<M, PK> results = new BasicFilterResults<>(rows,
+		return new BasicFilterResults<>(rows,
 				(totalCount != null ? totalCount : Long.valueOf(rows.size())),
-				offset != null ? offset.longValue() : 0L, rows.size());
-
-		return results;
+				offset != null ? offset : 0L, rows.size());
 	}
 
 	/**
 	 * Execute a count query for a filter.
-	 * 
+	 *
 	 * <p>
 	 * If the query throws an {@link IllegalArgumentException} this method
 	 * assumes that means the query name was not found, and will simply return
 	 * {@literal null}.
 	 * </p>
-	 * 
+	 *
 	 * @param countQueryName
 	 *        the query name
 	 * @param filter
