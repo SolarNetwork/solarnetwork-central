7b7f014c1dd5e2e7c446ca721571eb5733712d01
Matt Magoffin <matt@solarnetwork.net>
2025-02-03 09:15:12 +1300

    Codd quality improvements surfaced by IntelliJ.

--- solarnet/common/src/main/java/net/solarnetwork/central/cloud/aws/biz/AwsVirtualMachineBiz.java
+++ solarnet/common/src/main/java/net/solarnetwork/central/cloud/aws/biz/AwsVirtualMachineBiz.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * AwsVirtualMachineBiz.java - 30/10/2017 7:56:31 PM
- * 
+ *
  * Copyright 2017 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -60,7 +60,7 @@ import software.amazon.awssdk.services.ec2.model.TagDescription;
 
 /**
  * AWS SDK implementation of {@link VirtualMachineBiz}.
- * 
+ *
  * @author matt
  * @version 2.0
  */
@@ -72,7 +72,7 @@ public class AwsVirtualMachineBiz extends BasicIdentifiable implements VirtualMa
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param region
 	 *        the AWS region name
 	 * @param accessKey
@@ -87,7 +87,7 @@ public class AwsVirtualMachineBiz extends BasicIdentifiable implements VirtualMa
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param region
 	 *        the AWS region
 	 * @param credentialsProvider
@@ -170,7 +170,7 @@ public class AwsVirtualMachineBiz extends BasicIdentifiable implements VirtualMa
 
 	@Override
 	public void changeVirtualMachinesState(Set<String> machineIds, VirtualMachineState desiredState) {
-		List<InstanceStateChange> results = null;
+		List<InstanceStateChange> results;
 		if ( desiredState == VirtualMachineState.Running ) {
 			StartInstancesRequest req = StartInstancesRequest.builder().instanceIds(machineIds).build();
 			StartInstancesResponse res = client.startInstances(req);
