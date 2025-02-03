cfe9c4ad5200eab6a89a61d53f52d13262ca1108
Matt Magoffin <matt@solarnetwork.net>
2025-02-03 15:18:57 +1300

    Code quality improvements surfaced by IntelliJ.

--- solarnet/common/src/main/java/net/solarnetwork/central/security/jdbc/JdbcUserDetailsService.java
+++ solarnet/common/src/main/java/net/solarnetwork/central/security/jdbc/JdbcUserDetailsService.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * JdbcUserDetailsService.java - Feb 2, 2010 3:52:04 PM
- * 
+ *
  * Copyright 2007-2010 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -24,13 +24,10 @@ package net.solarnetwork.central.security.jdbc;
 
 import static net.solarnetwork.util.ObjectUtils.requireNonNullArgument;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -51,12 +48,12 @@ import net.solarnetwork.codec.JsonUtils;
 /**
  * Extension of {@link JdbcDaoImpl} that returns {@link AuthenticatedUser}
  * objects.
- * 
+ *
  * <p>
  * The SQL required by this implementation adds the following additional column
  * requirements to the base {@link JdbcDaoImpl} requirements:
  * </p>
- * 
+ *
  * <ol>
  * <li>User ID (Long) - the User primary key</li>
  * <li>Display Name (String) - the User's display name</li>
@@ -66,7 +63,7 @@ import net.solarnetwork.codec.JsonUtils;
  * <li>Token IDs (String) - if Token is <em>true</em>, this is an optional set
  * of IDs</li>
  * </ol>
- * 
+ *
  * @author matt
  * @version 2.1
  */
@@ -96,7 +93,7 @@ public class JdbcUserDetailsService extends JdbcDaoImpl implements UserDetailsSe
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param objectMapper
 	 *        the mapper to use
 	 * @throws IllegalArgumentException
@@ -113,8 +110,7 @@ public class JdbcUserDetailsService extends JdbcDaoImpl implements UserDetailsSe
 	protected UserDetails createUserDetails(String username, UserDetails userFromUserQuery,
 			List<GrantedAuthority> combinedAuthorities) {
 		User user = (User) super.createUserDetails(username, userFromUserQuery, combinedAuthorities);
-		if ( userFromUserQuery instanceof AuthenticatedToken ) {
-			AuthenticatedToken token = (AuthenticatedToken) userFromUserQuery;
+		if ( userFromUserQuery instanceof AuthenticatedToken token ) {
 			return new AuthenticatedToken(user, token.getTokenType(), token.getUserId(),
 					token.getPolicy());
 		}
@@ -126,42 +122,39 @@ public class JdbcUserDetailsService extends JdbcDaoImpl implements UserDetailsSe
 	@Override
 	@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
 	protected List<UserDetails> loadUsersByUsername(String username) {
-		return getJdbcTemplate().query(getUsersByUsernameQuery(), new RowMapper<UserDetails>() {
-
-			@Override
-			public UserDetails mapRow(ResultSet rs, int rowNum) throws SQLException {
-				String username = rs.getString(1);
-				String password = rs.getString(2);
-				boolean enabled = rs.getBoolean(3);
-				Long id = rs.getLong(4);
-				String name = rs.getString(5);
-				boolean authWithToken = rs.getBoolean(6);
-				if ( authWithToken ) {
-					String tokenTypeString = rs.getString(7);
-					SecurityTokenType tokenType = null;
-					if ( tokenTypeString != null ) {
-						try {
-							tokenType = SecurityTokenType.valueOf(tokenTypeString);
-						} catch ( IllegalArgumentException e ) {
-							log.warn("Unknown token type will be ignored: {}", tokenTypeString);
-						}
+		assert getJdbcTemplate() != null;
+		return getJdbcTemplate().query(getUsersByUsernameQuery(), (rs, rowNum) -> {
+			String username1 = rs.getString(1);
+			String password = rs.getString(2);
+			boolean enabled = rs.getBoolean(3);
+			Long id = rs.getLong(4);
+			String name = rs.getString(5);
+			boolean authWithToken = rs.getBoolean(6);
+			if ( authWithToken ) {
+				String tokenTypeString = rs.getString(7);
+				SecurityTokenType tokenType = null;
+				if ( tokenTypeString != null ) {
+					try {
+						tokenType = SecurityTokenType.valueOf(tokenTypeString);
+					} catch ( IllegalArgumentException e ) {
+						log.warn("Unknown token type will be ignored: {}", tokenTypeString);
 					}
-					String policyJson = rs.getString(8);
-					SecurityPolicy policy = null;
-					if ( policyJson != null && !"{}".equals(policyJson) ) {
-						try {
-							policy = objectMapper.readValue(policyJson, BasicSecurityPolicy.class);
-						} catch ( IOException e ) {
-							log.error("Error deserializing [{}] SecurityPolicy from [{}]: {}", username,
-									policyJson, e.getMessage());
-						}
-					}
-					return new AuthenticatedToken(new User(username, password, enabled, true, true, true,
-							AuthorityUtils.NO_AUTHORITIES), tokenType, id, policy);
 				}
-				return new AuthenticatedUser(new User(username, password, enabled, true, true, true,
-						AuthorityUtils.NO_AUTHORITIES), id, name, false);
+				String policyJson = rs.getString(8);
+				SecurityPolicy policy = null;
+				if ( policyJson != null && !"{}".equals(policyJson) ) {
+					try {
+						policy = objectMapper.readValue(policyJson, BasicSecurityPolicy.class);
+					} catch ( IOException e ) {
+						log.error("Error deserializing [{}] SecurityPolicy from [{}]: {}", username1,
+								policyJson, e.getMessage());
+					}
+				}
+				return new AuthenticatedToken(new User(username1, password, enabled, true, true, true,
+						AuthorityUtils.NO_AUTHORITIES), tokenType, id, policy);
 			}
+			return new AuthenticatedUser(new User(username1, password, enabled, true, true, true,
+					AuthorityUtils.NO_AUTHORITIES), id, name, false);
 		}, username);
 	}
 
@@ -175,18 +168,20 @@ public class JdbcUserDetailsService extends JdbcDaoImpl implements UserDetailsSe
 	/**
 	 * Set a list of statically assigned authorities based on a list of role
 	 * names.
-	 * 
+	 *
 	 * <p>
 	 * All users will be automatically granted these authorities.
 	 * </p>
-	 * 
+	 *
 	 * @param roles
 	 *        the role names to grant
 	 */
 	public void setStaticRoles(List<String> roles) {
-		List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>(roles == null ? 0 : roles.size());
-		for ( String role : roles ) {
-			auths.add(new SimpleGrantedAuthority(role));
+		List<GrantedAuthority> auths = new ArrayList<>(roles == null ? 0 : roles.size());
+		if ( roles != null ) {
+			for ( String role : roles ) {
+				auths.add(new SimpleGrantedAuthority(role));
+			}
 		}
 		staticAuthorities = auths;
 	}
