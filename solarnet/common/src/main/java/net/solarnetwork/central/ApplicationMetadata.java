0a0f3b44e305196b423a1a51b86c3b745dc73124
Matt Magoffin <matt@solarnetwork.net>
2025-02-03 17:00:27 +1300

    Code quality improvements surfaced by IntelliJ.

--- solarnet/common/src/main/java/net/solarnetwork/central/ApplicationMetadata.java
+++ solarnet/common/src/main/java/net/solarnetwork/central/ApplicationMetadata.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * ApplicationMetadata.java - 21/02/2022 10:19:16 AM
- * 
+ *
  * Copyright 2022 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -27,7 +27,7 @@ import net.solarnetwork.util.ObjectUtils;
 
 /**
  * General application metadata.
- * 
+ *
  * @author matt
  * @version 1.0
  */
@@ -39,7 +39,7 @@ public class ApplicationMetadata {
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param name
 	 *        the application name, must not be {@literal null}
 	 * @param version
@@ -79,8 +79,8 @@ public class ApplicationMetadata {
 
 	/**
 	 * Get the name.
-	 * 
-	 * @return the name, never {@litearl null}
+	 *
+	 * @return the name, never {@literal null}
 	 */
 	public String getName() {
 		return name;
@@ -88,7 +88,7 @@ public class ApplicationMetadata {
 
 	/**
 	 * Get the version.
-	 * 
+	 *
 	 * @return the version
 	 */
 	public String getVersion() {
@@ -97,7 +97,7 @@ public class ApplicationMetadata {
 
 	/**
 	 * Get the instance ID.
-	 * 
+	 *
 	 * @return the instance ID, never {@literal null}
 	 */
 	public String getInstanceId() {
