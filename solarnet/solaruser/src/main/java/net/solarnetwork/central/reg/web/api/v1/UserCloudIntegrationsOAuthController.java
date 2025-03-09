d1b20bf97c3e17735dad632ee90519d7417d8f18
Matt Magoffin <matt@solarnetwork.net>
2025-03-10 11:29:17 +1300

    NET-445: add API to list integrations for user that require OAuth token configuration.

---
+++ solarnet/solaruser/src/main/java/net/solarnetwork/central/reg/web/api/v1/UserCloudIntegrationsOAuthController.java
@@ -0,0 +1,105 @@
+/* ==================================================================
+ * UserCloudIntegrationsOAuthController.java - 10/03/2025 10:37:59 am
+ *
+ * Copyright 2025 SolarNetwork.net Dev Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * ==================================================================
+ */
+
+package net.solarnetwork.central.reg.web.api.v1;
+
+import static java.util.Collections.emptyList;
+import static net.solarnetwork.central.security.SecurityUtils.getCurrentActorUserId;
+import static net.solarnetwork.domain.Result.success;
+import static net.solarnetwork.util.ObjectUtils.requireNonNullArgument;
+import java.util.stream.StreamSupport;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import net.solarnetwork.central.c2c.biz.CloudIntegrationService;
+import net.solarnetwork.central.c2c.config.SolarNetCloudIntegrationsConfiguration;
+import net.solarnetwork.central.c2c.dao.BasicFilter;
+import net.solarnetwork.central.c2c.domain.CloudIntegrationConfiguration;
+import net.solarnetwork.central.domain.UserLongCompositePK;
+import net.solarnetwork.central.user.c2c.biz.UserCloudIntegrationsBiz;
+import net.solarnetwork.central.web.GlobalExceptionRestController;
+import net.solarnetwork.dao.BasicFilterResults;
+import net.solarnetwork.dao.FilterResults;
+import net.solarnetwork.domain.Result;
+import net.solarnetwork.settings.KeyedSettingSpecifier;
+
+/**
+ * Web service API for cloud integrations OAuth management.
+ *
+ * @author matt
+ * @version 1.0
+ */
+@Profile(SolarNetCloudIntegrationsConfiguration.CLOUD_INTEGRATIONS)
+@GlobalExceptionRestController
+@RestController("v1CloudIntegrationsOAuthController")
+@RequestMapping(value = { "/api/v1/sec/user/c2c/oauth", "/u/sec/c2c/oauth" })
+public class UserCloudIntegrationsOAuthController {
+
+	private final UserCloudIntegrationsBiz userCloudIntegrationsBiz;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param userCloudIntegrationsBiz
+	 *        the user cloud integrations service
+	 * @throws IllegalArgumentException
+	 *         if any argument is {@literal null}
+	 */
+	public UserCloudIntegrationsOAuthController(UserCloudIntegrationsBiz userCloudIntegrationsBiz) {
+		super();
+		this.userCloudIntegrationsBiz = requireNonNullArgument(userCloudIntegrationsBiz,
+				"userCloudIntegrationsBiz");
+	}
+
+	/**
+	 * List the actor's configured integrations that require OAuth access token
+	 * configuration.
+	 *
+	 * @param filter
+	 *        the pagination criteria
+	 * @return the results
+	 */
+	@RequestMapping(value = "/integrations", method = RequestMethod.GET)
+	public Result<FilterResults<CloudIntegrationConfiguration, UserLongCompositePK>> listOauthCloudIntegrationConfigurations(
+			BasicFilter filter) {
+		// force filter to just integrations that require OAuth access token configuration
+		var services = userCloudIntegrationsBiz.availableIntegrationServices();
+		var oauthServiceIdents = StreamSupport.stream(services.spliterator(), false)
+				.filter(s -> s.getSettingSpecifiers().stream()
+						.filter(ss -> ss instanceof KeyedSettingSpecifier<?> k
+								&& CloudIntegrationService.OAUTH_ACCESS_TOKEN_SETTING.equals(k.getKey()))
+						.findAny().isPresent())
+				.map(s -> s.getId()).toArray(String[]::new);
+
+		if ( oauthServiceIdents.length < 1 ) {
+			return success(new BasicFilterResults<>(emptyList()));
+		}
+
+		filter.setServiceIdentifiers(oauthServiceIdents);
+
+		var result = userCloudIntegrationsBiz.listConfigurationsForUser(getCurrentActorUserId(), filter,
+				CloudIntegrationConfiguration.class);
+		return success(result);
+	}
+
+}
