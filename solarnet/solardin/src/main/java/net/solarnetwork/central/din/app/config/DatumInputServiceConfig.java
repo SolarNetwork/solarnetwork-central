7f14e22b36a51a63a86010f7b695bd883ed5eacc
Matt Magoffin <matt@solarnetwork.net>
2025-05-01 15:27:53 +1200

    Use DatumEntityDao directly, instead of AsyncDatumCollector... in future can switch to SqsDatumCollector as needed.

--- solarnet/solardin/src/main/java/net/solarnetwork/central/din/app/config/DatumInputServiceConfig.java
+++ solarnet/solardin/src/main/java/net/solarnetwork/central/din/app/config/DatumInputServiceConfig.java
@@ -23,42 +23,29 @@
 package net.solarnetwork.central.din.app.config;
 
 import static net.solarnetwork.central.din.app.config.SolarFluxMqttConnectionConfig.SOLARFLUX;
-import java.io.Serializable;
 import java.util.Collection;
-import javax.cache.Cache;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 import net.solarnetwork.central.biz.UserEventAppenderBiz;
 import net.solarnetwork.central.dao.SolarNodeOwnershipDao;
 import net.solarnetwork.central.datum.biz.DatumProcessor;
-import net.solarnetwork.central.datum.support.AsyncDatumCollector;
-import net.solarnetwork.central.datum.support.AsyncDatumCollectorSettings;
-import net.solarnetwork.central.datum.v2.dao.DatumEntityDao;
 import net.solarnetwork.central.datum.v2.dao.DatumWriteOnlyDao;
 import net.solarnetwork.central.din.biz.TransformService;
 import net.solarnetwork.central.din.biz.impl.DaoDatumInputEndpointBiz;
 import net.solarnetwork.central.din.dao.EndpointConfigurationDao;
 import net.solarnetwork.central.din.dao.InputDataEntityDao;
 import net.solarnetwork.central.din.dao.TransformConfigurationDao;
-import net.solarnetwork.util.StatTracker;
 
 /**
  * Core service configuration.
  *
  * @author matt
- * @version 1.1
+ * @version 1.2
  */
 @Configuration(proxyBeanMethods = false)
 public class DatumInputServiceConfig implements DatumInputConfiguration {
-
-	@Autowired
-	private PlatformTransactionManager txManager;
 
 	@Autowired
 	private SolarNodeOwnershipDao nodeOwnershipDao;
@@ -70,9 +57,6 @@ public class DatumInputServiceConfig implements DatumInputConfiguration {
 	@Qualifier(CACHING)
 	@Autowired
 	private EndpointConfigurationDao endpointDao;
-
-	@Autowired
-	private DatumEntityDao datumDao;
 
 	@Autowired
 	private InputDataEntityDao inputDataDao;
@@ -88,30 +72,7 @@ public class DatumInputServiceConfig implements DatumInputConfiguration {
 	private UserEventAppenderBiz userEventAppenderBiz;
 
 	@Bean
-	@ConfigurationProperties(prefix = "app.solarin.async-collector")
-	public AsyncDatumCollectorSettings asyncDatumCollectorSettings() {
-		return new AsyncDatumCollectorSettings();
-	}
-
-	@Qualifier(CACHING)
-	@Bean(initMethod = "serviceDidStartup", destroyMethod = "serviceDidShutdown")
-	public AsyncDatumCollector asyncDaoDatumCollector(AsyncDatumCollectorSettings settings,
-			@Qualifier(DATUM_BUFFER) Cache<Serializable, Serializable> buffer) {
-		TransactionTemplate tt = new TransactionTemplate(txManager);
-		StatTracker stats = new StatTracker("AsyncDaoDatum", null,
-				LoggerFactory.getLogger(AsyncDatumCollector.class), settings.getStatFrequency());
-		AsyncDatumCollector collector = new AsyncDatumCollector(buffer, datumDao, tt, stats);
-		collector.setConcurrency(settings.getThreads());
-		collector.setShutdownWaitSecs(settings.getShutdownWaitSecs());
-		collector.setQueueSize(settings.getQueueSize());
-		collector.setQueueRefillThreshold(settings.getQueueRefillThreshold());
-		collector.setDatumCacheRemovalAlertThreshold(settings.getDatumCacheRemovalAlertThreshold());
-		return collector;
-	}
-
-	@Bean
-	public DaoDatumInputEndpointBiz datumInputEndpointBiz(
-			@Qualifier(CACHING) DatumWriteOnlyDao datumDao) {
+	public DaoDatumInputEndpointBiz datumInputEndpointBiz(DatumWriteOnlyDao datumDao) {
 		var biz = new DaoDatumInputEndpointBiz(nodeOwnershipDao, endpointDao, transformDao, datumDao,
 				inputDataDao, transformServices);
 		biz.setFluxPublisher(fluxPublisher);
