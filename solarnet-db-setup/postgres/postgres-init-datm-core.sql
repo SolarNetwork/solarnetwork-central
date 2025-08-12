86ea0f79ef4bdd77b005c64c847d9cb62d74ea6c
Matt Magoffin <matt@solarnetwork.net>
2025-08-13 11:41:45 +1200

    NET-473: copy changes into setup DDL.

--- solarnet-db-setup/postgres/postgres-init-datm-core.sql
+++ solarnet-db-setup/postgres/postgres-init-datm-core.sql
@@ -597,17 +597,28 @@ $$
 	-- Include previous adjacent hour if first datum in hour falls exactly on hour,
 	-- to account for possible accumulation after an hour gap (NET-469). The outer
 	-- query then strips out any matching end date, making it exclusive again.
-	SELECT * FROM (
-		SELECT DISTINCT ON (ts_start)
-			  sid
-			, UNNEST(CASE
-				  	WHEN date_trunc('hour', ts) = min(ts) THEN ARRAY[date_trunc('hour', ts) - INTERVAL 'PT1H', date_trunc('hour', ts)]
-				  	ELSE ARRAY[date_trunc('hour', ts)]
-				  END) AS ts_start
-		FROM solardatm.da_datm
-		WHERE stream_id = sid
-			AND ts >= start_ts
-			AND ts <= end_ts
+	SELECT sid, ts_start
+	FROM (
+		SELECT DISTINCT UNNEST(CASE
+				WHEN date_trunc('hour', ts) = min(ts) THEN ARRAY[date_trunc('hour', ts) - INTERVAL 'PT1H', date_trunc('hour', ts)]
+				ELSE ARRAY[date_trunc('hour', ts)]
+			END) AS ts_start
+		FROM (
+			SELECT ts
+			FROM solardatm.da_datm
+			WHERE stream_id = sid
+				AND ts >= start_ts
+				AND ts <= end_ts
+
+			UNION ALL
+
+			SELECT ts
+			FROM solardatm.da_datm_aux
+			WHERE stream_id = sid
+				AND ts >= start_ts
+				AND ts <= end_ts
+				AND atype = 'Reset'::solardatm.da_datm_aux_type
+		)
 		GROUP BY date_trunc('hour', ts)
 	)
 	WHERE ts_start < end_ts
