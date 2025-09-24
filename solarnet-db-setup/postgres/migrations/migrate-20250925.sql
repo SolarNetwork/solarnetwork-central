442f6c85051ed7faef75ccefbf0e47a9ba02642f
Matt Magoffin <matt@solarnetwork.net>
2025-09-25 08:35:37 +1200

    NET-480: only execute one rake task at a time within datum stream.

--- solarnet-db-setup/postgres/migrations/migrate-20250925.sql
+++ solarnet-db-setup/postgres/migrations/migrate-20250925.sql
@@ -1,3 +1,4 @@
 -- Run this script from the parent directory, e.g. psql -f migrations/migrate-20250925.sql
 
 \i updates/NET-479-delete-optimisation.sql
+\i updates/NET-480-cloud-datum-stream-rake-task-group.sql
