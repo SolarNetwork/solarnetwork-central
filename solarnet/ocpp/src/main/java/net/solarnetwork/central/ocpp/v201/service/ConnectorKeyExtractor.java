fa688b2cd12af23318cd9805104042200890a834
Matt Magoffin <matt@solarnetwork.net>
2026-01-15 05:59:20 +1300

    Remove unnecessary final modifier.

--- solarnet/ocpp/src/main/java/net/solarnetwork/central/ocpp/v201/service/ConnectorKeyExtractor.java
+++ solarnet/ocpp/src/main/java/net/solarnetwork/central/ocpp/v201/service/ConnectorKeyExtractor.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * ConnectorKeyExtractor.java - 18/02/2024 7:28:06 am
- * 
+ *
  * Copyright 2024 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -32,13 +32,13 @@ import ocpp.v201.TransactionEventRequest;
 
 /**
  * Extract a connector key from OCPP v2.0.1 request messages.
- * 
+ *
  * @author matt
  * @version 1.1
  */
 public class ConnectorKeyExtractor implements Function<Object, ChargePointConnectorKey> {
 
-	private static final int intOrZero(Number n) {
+	private static int intOrZero(Number n) {
 		return n != null ? n.intValue() : 0;
 	}
 
