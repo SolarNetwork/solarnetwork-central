1eae062cee385582730e3ea66d24e0b9b6d7b330
Matt Magoffin <matt@solarnetwork.net>
2026-06-03 10:00:10 +1200

    Add migration script for NET-510, NET-512.

---
+++ solarnet-db-setup/postgres/migrations/migrate-20260603.sql
@@ -0,0 +1,4 @@
+-- Run this script from the parent directory, e.g. psql -f migrations/migrate-20260603.sql
+
+\i updates/NET-510-new-aux-types.sql
+\i updates/NET-512-c2c-foreign-key-cascade-fix.sql
