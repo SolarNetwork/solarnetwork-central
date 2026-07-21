2998ba47a5412c19e78a4cf62c50c92508838f7a
Matt Magoffin <matt@solarnetwork.net>
2026-07-21 16:47:18 +1200

    NET-518: add migration DDL and integrate SQL changes into setup DDL.

---
+++ solarnet-db-setup/postgres/migrations/migrate-20260721.sql
@@ -0,0 +1,3 @@
+-- Run this script from the parent directory, e.g. psql -f migrations/migrate-20260721.sql
+
+\i updates/NET-518-c2c-sprop-merge.sql
--- solarnet-db-setup/postgres/postgres-init-common.sql
+++ solarnet-db-setup/postgres/postgres-init-common.sql
@@ -465,3 +465,31 @@ PARTITION OF solarcommon.bucket
 CREATE TABLE solarcommon.bucket_3
 PARTITION OF solarcommon.bucket
 FOR VALUES WITH (MODULUS 3, REMAINDER 2);
+
+
+/**************************************************************************************************
+ * FUNCTION solarcommon.jsonb_recursive_merge(jsonb,jsonb,BOOLEAN)
+ *
+ * Recursively merge JSON objects and arrays (if merge_arrays is TRUE).
+ *
+ * @param A the "source" JSON
+ * @param B the JSON to merge
+ * @param merge_arrays if TRUE then also merge arrays, otherwise replace them with values from B
+ * @return the merged JSON
+ */
+CREATE OR REPLACE FUNCTION solarcommon.jsonb_recursive_merge(A jsonb, B jsonb, merge_arrays BOOLEAN DEFAULT FALSE)
+RETURNS jsonb LANGUAGE SQL AS
+$$
+	SELECT jsonb_object_agg(
+		COALESCE(ka, kb),
+		CASE
+			WHEN va ISNULL THEN vb
+			WHEN vb ISNULL THEN va
+			WHEN merge_arrays AND jsonb_typeof(va) = 'array' AND jsonb_typeof(vb) = 'array' THEN va || vb
+			WHEN jsonb_typeof(va) <> 'object' OR jsonb_typeof(vb) <> 'object' THEN vb
+			ELSE solarcommon.jsonb_recursive_merge(va, vb, merge_arrays)
+		END
+	)
+	FROM jsonb_each(A) ta(ka, va)
+	FULL JOIN jsonb_each(B) tb(kb, vb) ON ka = kb
+$$;
